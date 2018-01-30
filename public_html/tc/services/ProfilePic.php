072e8d197761f36dd45027e7f46ba8b2c58e1e1c
Unknown <tristan.m.orourke@gmail.com>
2018-01-30 13:46:37 -0500

    Revert "Get headers in more backwards-compatible way"
    This reverts commit eb725509e02eed6f08536925abd59ca1dd6b9b2b.

--- public_html/tc/services/ProfilePic.php
+++ public_html/tc/services/ProfilePic.php
@@ -72,12 +72,13 @@ header("Content-Type: application/json; charset=utf-8");
             
             if(strlen($requestParams) > 1){
                 $user_id = Utils::getParameterFromRequest($requestParams,4);
+                $headers = getallheaders();
                                 
                 $profile_pic = new ProfilePic($user_id, 
                         file_get_contents('php://input'), 
                         null,
-                        $_SERVER['CONTENT_TYPE'],
-                        $_SERVER['CONTENT_LENGTH']);
+                        $headers['Content-type'],
+                        $headers['Content-Length']);
                 $result = ProfilePicController::putProfilePic($profile_pic);
                 //$json = json_encode($result, JSON_PRETTY_PRINT);
                 //echo($profile_pic->getImage());
