f749d3aa469ae805fef70b5372a981c4f66e952f
Unknown <tristan.m.orourke@gmail.com>
2018-02-14 17:14:21 -0500

    Made form inputs shorter, labels bolder

--- public_html/index.php
+++ public_html/index.php
@@ -14,42 +14,43 @@
             </div>
             <div class="wb-frmvld wb-init dialogWindowInterior" id="registerFormWrapper">
                 <form name="registerForm" id="registerForm" novalidate="novalidate" method="post" enctype="application/x-www-form-urlencoded">
-                    
-                    <div class="form-group">
-                        <label for="register_email">
-                            <span>Email:</span>
-                            <strong id="register_email_error" class="error hidden">
-                                <span id="register_email_error_msg" class="label label-danger"></span>
-                            </strong>
-                        </label>
-                        <input class="form-control form-textbox" id="register_email" name="register_email" type="text" required=""/>
-                    </div>
-                    <div class="form-group">
-                        <label for="register_email_confirm">
-                            <span>Re-enter email:</span>
-                            <strong id="register_email_confirm_error" class="error hidden">
-                                <span id="register_email_confirm_error_msg" class="label label-danger"></span>
-                            </strong>
-                        </label>
-                        <input class="form-control form-textbox" id="register_email_confirm" name="register_email_confirm" type="text" required=""/>
-                    </div>
-                    <div class="form-group">
-                        <label for="register_password">
-                            <span>Password:</span>
-                            <strong id="register_password_error" class="error hidden">
-                                <span id="register_password_error_msg" class="label label-danger"></span>
-                            </strong>
-                        </label>
-                        <input class="form-control form-textbox" id="register_password" name="register_password" type="password" required=""/>
-                    </div>
-                    <div class="form-group">
-                        <label for="register_password_confirm">
-                            <span>Re-enter password</span>
-                            <strong id="register_password_confirm_error" class="error hidden">
-                                <span id="register_password_confirm_error_msg" class="label label-danger"></span>
-                            </strong>
-                        </label>
-                        <input class="form-control form-textbox" id="register_password_confirm" name="register_password_confirm" type="password" required=""/>
+                    <div class="center-block three-quarter-width">
+                        <div class="form-group">
+                            <label for="register_email">
+                                <span>Email:</span>
+                                <strong id="register_email_error" class="error hidden">
+                                    <span id="register_email_error_msg" class="label label-danger"></span>
+                                </strong>
+                            </label>
+                            <input class="form-control form-textbox" id="register_email" name="register_email" type="text" required=""/>
+                        </div>
+                        <div class="form-group">
+                            <label for="register_email_confirm">
+                                <span>Re-enter email:</span>
+                                <strong id="register_email_confirm_error" class="error hidden">
+                                    <span id="register_email_confirm_error_msg" class="label label-danger"></span>
+                                </strong>
+                            </label>
+                            <input class="form-control form-textbox" id="register_email_confirm" name="register_email_confirm" type="text" required=""/>
+                        </div>
+                        <div class="form-group">
+                            <label for="register_password">
+                                <span>Password:</span>
+                                <strong id="register_password_error" class="error hidden">
+                                    <span id="register_password_error_msg" class="label label-danger"></span>
+                                </strong>
+                            </label>
+                            <input class="form-control form-textbox" id="register_password" name="register_password" type="password" required=""/>
+                        </div>
+                        <div class="form-group">
+                            <label for="register_password_confirm">
+                                <span>Re-enter password:</span>
+                                <strong id="register_password_confirm_error" class="error hidden">
+                                    <span id="register_password_confirm_error_msg" class="label label-danger"></span>
+                                </strong>
+                            </label>
+                            <input class="form-control form-textbox" id="register_password_confirm" name="register_password_confirm" type="password" required=""/>
+                        </div>
                     </div>
                     <div class="hidden">
                         <p><a href="javascript:void(0)" onclick="UserAPI.hideRegisterForm(); return UserAPI.showLogin(this);" class="ui-link" id="switchToLogin" title="Already have an account? Click here to login.">Already have an account? Click here to login</a></p>
@@ -93,30 +94,31 @@
             </div>
             <div class="dialogWindowInterior">
                 <form name="loginForm" id="loginForm" method="post" enctype="application/x-www-form-urlencoded">
-                                        <div class="label label-danger hidden" id="loginErrors"></div>
-                    <div class="form-group">
-                        <label for="login_email">
-                            <span>Email:</span>
-                            <strong id="login_email_error" class="error hidden">
-                                <span id="login_email_error_msg" class="label label-danger"></span>
-                            </strong>
-                        </label>
-                        <div>
-                            <input class="form-control full-width" type="email" name="login_email" id="login_email" required=""/>
+                    <div class="label label-danger hidden" id="loginErrors"></div>
+                    <div class="center-block three-quarter-width">
+                        <div class="form-group">
+                            <label for="login_email">
+                                <span>Email:</span>
+                                <strong id="login_email_error" class="error hidden">
+                                    <span id="login_email_error_msg" class="label label-danger"></span>
+                                </strong>
+                            </label>
+                            <div>
+                                <input class="form-control full-width" type="email" name="login_email" id="login_email" required=""/>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="login_password">
-                            <span>Password:</span>
-                            <strong id="login_password_error" class="error hidden">
-                                <span id="login_password_error_msg" class="label label-danger"></span>
-                            </strong>
-                        </label>
-                        <div>
-                            <input class="form-control full-width" type="password" name="login_password" id="login_password" required=""/>
+                        <div class="form-group">
+                            <label for="login_password">
+                                <span>Password:</span>
+                                <strong id="login_password_error" class="error hidden">
+                                    <span id="login_password_error_msg" class="label label-danger"></span>
+                                </strong>
+                            </label>
+                            <div>
+                                <input class="form-control full-width" type="password" name="login_password" id="login_password" required=""/>
+                            </div>
                         </div>
-                    </div>
-                                        
+                    </div>                
                     <div class="hidden">
                         <div style="margin: 1em 0 0 0;">
                             <a href="javascript:void(0)">Forgot your password? Click here to reset it. (Not working yet.)</a>
