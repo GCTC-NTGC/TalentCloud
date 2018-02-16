26f2329e987862777f121b483c06b31c85e133a3
Grant <grant.d.barnes@gmail.com>
2018-02-16 11:42:38 -0500

    - Edited includes

---
+++ public_html/inc/footer-admin.php
@@ -0,0 +1,9 @@
+<footer>
+    <div></div>
+</footer>
+<!-- BEGIN - Add Contact Overlay and Form-->
+<script src="../js/Messages.js"></script>
+<script>
+    ManagerEventsAPI.onLoadEvents();
+</script>
+<script src="../js/AccessibilityAPI.js"></script>
