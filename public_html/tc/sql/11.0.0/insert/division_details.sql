c3fbeaff0e17df84353deb93433066169ea71d5b
Unknown <tristan.m.orourke@gmail.com>
2018-02-20 16:12:34 -0500

    Final sql update

--- public_html/tc/sql/11.0.0/insert/division_details.sql
+++ public_html/tc/sql/11.0.0/insert/division_details.sql
@@ -1,5 +1,8 @@
 /** dependent on create division and division_details tables**/
 /** dependent on content division**/
+
+TRUNCATE `talentcloud`.`division_details`;
+
 INSERT INTO `talentcloud`.`division_details` (`division_id`, `division_locale_id`, `division_name`) VALUES ('1', '1', 'Office of the Chief Information Officer');
 
 INSERT INTO `talentcloud`.`division_details` (`division_id`, `division_locale_id`, `division_name`) VALUES ('1', '2', 'Bureau du dirigeant principal de l\'information');
