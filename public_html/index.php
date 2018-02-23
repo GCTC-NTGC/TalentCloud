93e1932f522f04f715b92b7c0fc035548a974dd1
Unknown <tristan.m.orourke@gmail.com>
2018-02-23 17:38:19 -0500

    Finished updating applicant profile banner styling

--- public_html/index.php
+++ public_html/index.php
@@ -528,61 +528,63 @@ and open the template in the editor .
         <div class="pageBanner">
             <div id="profileBannerFiller"></div>
         </div>
-        <div class="pageBody container">
-            <div id="profileBasicInfo" class="centered">
-                <div id="profileBasicInfoTopBar">
-                    <div class="socialMediaWrapper"> 
-                        <ul id="profileSocialMediaLinks"> 
-                            <li id="profileTwitterLinkWrapper" class="hidden">
-                                <a href="#" id="profileTwitterLink" target="_blank"><img src="/images/Twitter_icon_white.svg" class="socialMediaLink" alt="Twitter logo"/></a>
-                            </li>
-                            <li id="profileLinkedinLinkWrapper" class="hidden">
-                                <a href="#" id="profileLinkedinLink" target="_blank"><img src="/images/Linkedin_icon_white.svg" class="socialMediaLink" alt="LinkedIn logo"/></a>
-                            </li>    
-                        </ul>
+        <div class="pageBody">
+            <div class="container">
+                <div id="profileBasicInfo" class="centered">
+                    <div id="profileBasicInfoTopBar">
+                        <div class="socialMediaWrapper"> 
+                            <ul id="profileSocialMediaLinks"> 
+                                <li id="profileTwitterLinkWrapper" class="hidden">
+                                    <a href="#" id="profileTwitterLink" target="_blank"><img src="/images/Twitter_icon_white.svg" class="socialMediaLink" alt="Twitter logo"/></a>
+                                </li>
+                                <li id="profileLinkedinLinkWrapper" class="hidden">
+                                    <a href="#" id="profileLinkedinLink" target="_blank"><img src="/images/Linkedin_icon_white.svg" class="socialMediaLink" alt="LinkedIn logo"/></a>
+                                </li>    
+                            </ul>
+                        </div>
+                        <img id="myProfilePic" class="profilePicLarge" src="images/user.png" alt="Profile Pic"/>
+                        <div class="editProfileWrapper">
+                            <a href="javascript:void(0)" id="profileBasicInfoEdit" onclick="JobSeekerAPI.showJobSeekerProfileBasicInfoEdit()">
+                                <img src="/images/edit_profile_pic.svg" alt="Edit Basic Info" class="editImage"/>
+                            </a>
+                        </div>
                     </div>
-                    <img id="myProfilePic" class="profilePicLarge" src="images/user.png" alt="Profile Pic"/>
-                    <div class="editProfileWrapper">
-                        <a href="javascript:void(0)" id="profileBasicInfoEdit" onclick="JobSeekerAPI.showJobSeekerProfileBasicInfoEdit()">
-                            <img src="/images/edit_profile_pic.svg" alt="Edit Basic Info" class="editImage"/>
-                        </a>
+                    <div id="profileNameWrapper">
+                        <div id="profileName">
+                            <span id="profileFirstName"></span> <span id="profileLastName"></span>
+                        </div>
                     </div>
-                </div>
-                <div id="profileNameWrapper">
-                    <div id="profileName">
-                        <span id="profileFirstName"></span> <span id="profileLastName"></span>
+                    <div class="profileTagLineContainer">
+                        <p id="profileTagLine">Default tag line!</p>
+                        <p>
+                        <!--p>
+                            <strong>Available: </strong><span id="profileStartDate"></span>
+                            <br>
+                            <strong>Status: </strong><span id="profileStatus"></span>
+                        </p-->
+                        </p>
                     </div>
+                    <input type="hidden" id="profileId"/>
+                    <input type="hidden" id="profileLastUpdated"/>
                 </div>
-                <div class="profileTagLineContainer">
-                    <p id="profileTagLine">Default tag line!</p>
-                    <p>
-                    <!--p>
-                        <strong>Available: </strong><span id="profileStartDate"></span>
-                        <br>
-                        <strong>Status: </strong><span id="profileStatus"></span>
-                    </p-->
-                    </p>
+                <div id="profileAboutMeWrapper" class="profileSubSection">
+                    <div class="profileSubSectionTitleBar">
+                        <h2>About Me</h2>
+                        <a href="javascript:void(0)" id="profileAboutMeEdit" class="sectionEditBtn" onclick="JobSeekerAPI.showJobSeekerProfileAboutMeEdit()"><img src="/images/btn_edit_dark.png" alt="Edit About Me" class="editImage"/></a>
+                    </div>
+                    <p id="profileAboutMe">This is the about me section.</p>
                 </div>
-                <input type="hidden" id="profileId"/>
-                <input type="hidden" id="profileLastUpdated"/>
+                <!--
+                <div id="profileSkillsWrapper" class="profileSubSection">
+                    <div class="profileSubSectionTitleBar">
+                        <h2>My Skills and Knowledge</h2>
+                        <a href="javascript:void(0)" id="profileSkillsEdit" class="sectionEditBtn"><img src="/images/btn_edit_dark.png" alt="Edit Skills" class="editImage"/></a>
+                    </div>
+                    <ul id="profileSkillsList">
+                    </ul>
+                </div>
+                -->
             </div>
-            <div id="profileAboutMeWrapper" class="profileSubSection">
-                <div class="profileSubSectionTitleBar">
-                    <h2>About Me</h2>
-                    <a href="javascript:void(0)" id="profileAboutMeEdit" class="sectionEditBtn" onclick="JobSeekerAPI.showJobSeekerProfileAboutMeEdit()"><img src="/images/btn_edit_dark.png" alt="Edit About Me" class="editImage"/></a>
-                </div>
-                <p id="profileAboutMe">This is the about me section.</p>
-            </div>
-            <!--
-            <div id="profileSkillsWrapper" class="profileSubSection">
-                <div class="profileSubSectionTitleBar">
-                    <h2>My Skills and Knowledge</h2>
-                    <a href="javascript:void(0)" id="profileSkillsEdit" class="sectionEditBtn"><img src="/images/btn_edit_dark.png" alt="Edit Skills" class="editImage"/></a>
-                </div>
-                <ul id="profileSkillsList">
-                </ul>
-            </div>
-            -->
         </div>
     </section>
 </main>
