0cf6eee8c2c65c3eded181c4c926a84cce88bf6a
Grant <grant.d.barnes@gmail.com>
2018-02-28 15:05:15 -0500

    Added footer to admin side / Canada.ca link label

--- public_html/tc/sql/16.0.0/insert/base_content.sql
+++ public_html/tc/sql/16.0.0/insert/base_content.sql
@@ -4,6 +4,9 @@ INSERT INTO `talentcloud`.`base_content` (`base_content_id`, `base_content_type_
 INSERT INTO `talentcloud`.`base_content` (`base_content_id`, `base_content_type_id`, `base_content_key`, `base_content_value`, `base_content_locale_id`) VALUES ('0000000134', '0000000001', 'gctc', 'GC Talent Cloud', '0000000001');
 INSERT INTO `talentcloud`.`base_content` (`base_content_id`, `base_content_type_id`, `base_content_key`, `base_content_value`, `base_content_locale_id`) VALUES ('0000000135', '0000000001', 'gctc', 'Nuage de talents GC', '0000000002');
 
+INSERT INTO `talentcloud`.`base_content` (`base_content_id`, `base_content_type_id`, `base_content_key`, `base_content_value`, `base_content_locale_id`) VALUES ('0000000136', '0000000001', 'canadaLink', 'Visit Canada.ca', '0000000001');
+INSERT INTO `talentcloud`.`base_content` (`base_content_id`, `base_content_type_id`, `base_content_key`, `base_content_value`, `base_content_locale_id`) VALUES ('0000000137', '0000000001', 'canadaLink', 'Visiter Canada.ca', '0000000002');
+
 
 UPDATE `talentcloud`.`base_content` SET `base_content_value`='Home' WHERE `base_content_id`='0000000018';
 UPDATE `talentcloud`.`base_content` SET `base_content_value`='Accueil' WHERE `base_content_id`='0000000019';
