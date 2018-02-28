c9419f2168487d979487fd604e481a2ef138fb90
Grant <Grant@192.168.0.69>
2018-02-28 01:47:34 -0500

    - Fixed footer positioning / page heights / white backgrounds - Old DB fix for job posters added again

--- public_html/tc/sql/11.0.0/alter/job_poster.sql
+++ public_html/tc/sql/11.0.0/alter/job_poster.sql
@@ -6,8 +6,10 @@
 /**
  * Author:  GBowden
  * Created: Jan 18, 2018
+ * Edited:  Feb 27, 2018 (Grant)
  */
 
 ALTER TABLE `talentcloud`.`job_poster` 
-ADD COLUMN `job_poster_remuneration_min` INT(9) NULL AFTER `job_number`,
+/* changed job_number to job_poster_department_id */
+ADD COLUMN `job_poster_remuneration_min` INT(9) NULL AFTER `job_poster_department_id`,
 ADD COLUMN `job_poster_remuneration_max` INT(9) NULL AFTER `job_poster_remuneration_min`;
