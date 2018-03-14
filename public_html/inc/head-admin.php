90ea5822c09ff2872825df37d1e0d7dfbcf774e0
Grant <Grant@192.168.0.69>
2018-03-14 09:16:20 -0400

    - Work environment questions, sliders, buttons, some styles. - Removed stray </div> in nav-admin

--- public_html/inc/head-admin.php
+++ public_html/inc/head-admin.php
@@ -4,5 +4,5 @@
 <script src="/admin/js/CreateEditProfileAPI.js"></script>
 <script src="/admin/js/ManagerEventsAPI.js"></script>
 <script src="/admin/js/DepartmentAPI.js"></script>
-<script src="/admin/js/DivisionAPI.js" type="text/javascript"></script>
-<script src="/admin/js/BranchAPI.js" type="text/javascript"></script>
+<script src="/admin/js/DivisionAPI.js"></script>
+<script src="/admin/js/BranchAPI.js"></script>
