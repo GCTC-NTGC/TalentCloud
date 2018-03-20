2354ff079574fc01e47422718d2a3f7e18322387
Grant <Grant@192.168.0.69>
2018-03-20 10:15:42 -0400

    Fixed accessibility errors on admin side

--- public_html/inc/nav-admin.php
+++ public_html/inc/nav-admin.php
@@ -41,36 +41,3 @@
         </div>
     </div>
 </nav>
-
-<!--<li class="top-nav--link active" aria-hidden="false" id="homeLink">
-    <a href="/">Home</a>
-</li>
-<li class="top-nav--link">
-    <a href="/about">FAQ</a>
-</li>
-<ul class="navbar-right" role="menubar">
-    <li class="top-nav--link" aria-hidden="false" id="homeLinkListItem">
-        <a href="/admin/" class="active" id="homeLink">Home</a>
-    </li>
-    <li class="top-nav--link hidden" id="browseLinkListItem">
-        <div id="browse">
-            <a href="javascript:void(0)" id="browseLink" onclick="JobPostAPI.showBrowseJobs();">Browse Jobs</a>
-        </div>
-    </li>
-    <li class="top-nav--link" id="profileLinkListItem" aria-hidden="true">
-        <a href="javascript:void(0)" class="hidden" id="profileLink" onclick="CreateEditProfileAPI.showCreateEditProfile();TalentCloudAPI.setNav('profileLinkListItem');">My Profile</a>
-    </li>
-    <li class="top-nav--link" id="jobPostersLinkListItem" aria-hidden="true">
-        <a href="javascript:void(0)" class="hidden" id="jobPostersLink" onclick="CreateJobPosterAPI.showCreateJobPosterForm(this);TalentCloudAPI.setNav('jobPostersLinkListItem');">Job Posters</a>
-    </li>
-    <li class="top-nav--link" id="teamsLinkListItem" aria-hidden="true">
-        <a href="#" class="hidden" id="teamsLink">My Teams</a>
-    </li>
-    <li class="top-nav--link" id="registerLinkListItem">
-        <div id="register">
-            <a href="javascript:void(0)" id="registerLink" onclick="UserAPI.showRegisterForm(this)">Register</a>
-        </div>
-    </li>
-    <li class="top-nav--link">
-        <a href="/about">FAQ</a>
-    </li>-->
