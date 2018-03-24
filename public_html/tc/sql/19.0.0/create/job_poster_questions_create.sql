e2afe5723ec38f201f471201bd72b6dcbc567103
Unknown <tristan.m.orourke@gmail.com>
2018-03-24 14:10:20 -0400

    Made job poster questions localized

--- public_html/tc/sql/19.0.0/create/job_poster_questions_create.sql
+++ public_html/tc/sql/19.0.0/create/job_poster_questions_create.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS `talentcloud`.`job_poster_question`;
 CREATE TABLE `talentcloud`.`job_poster_question` (
-  `id` INT ZEROFILL UNSIGNED NOT NULL AUTO_INCREMENT,
-  `job_poster_id` INT UNSIGNED ZEROFILL NOT NULL,
+  `id` INT(10) ZEROFILL UNSIGNED NOT NULL AUTO_INCREMENT,
+  `job_poster_id` INT(10) UNSIGNED ZEROFILL NOT NULL,
+  `locale_id` INT(10) UNSIGNED ZEROFILL NOT NULL,
   `question` LONGTEXT NOT NULL,
   PRIMARY KEY (`id`));
