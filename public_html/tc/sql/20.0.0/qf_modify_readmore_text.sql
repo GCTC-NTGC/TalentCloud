096d5bd8ddb7aa7333365f610ac09b6fbd808e62
queenie_fung <qf062@192.168.1.66>
2018-04-05 13:22:39 -0600

    Modify read more text to read profile

---
+++ public_html/tc/sql/20.0.0/qf_modify_readmore_text.sql
@@ -0,0 +1,9 @@
+/**
+ * Author:  Queenie Fung
+ * Created: April 5, 2018
+ */
+
+/*Update Read More text in Manager Profile*/
+
+UPDATE `talentcloud`.`base_content` SET `base_content_value`='Read Profile' WHERE `base_content_id`='0000000891';
+UPDATE `talentcloud`.`base_content` SET `base_content_value`='Lire le profil' WHERE `base_content_id`='0000000892';
