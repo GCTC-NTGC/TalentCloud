121af1df8324272780bde9f8b99ab2977ad363de
queenie_fung <qf062@192.168.1.66>
2018-04-05 13:40:22 -0600

    modified read profile text

--- public_html/tc/sql/20.0.0/insert/qf_modify_readmore_text.sql
+++ public_html/tc/sql/20.0.0/insert/qf_modify_readmore_text.sql
@@ -5,5 +5,5 @@
 
 /*Update Read More text in Manager Profile*/
 
-UPDATE `talentcloud`.`base_content` SET `base_content_value`='Read Profile' WHERE `base_content_id`='0000000891';
-UPDATE `talentcloud`.`base_content` SET `base_content_value`='Lire le profil' WHERE `base_content_id`='0000000892';
+UPDATE `talentcloud`.`base_content` SET `base_content_value`='View Full Profile' WHERE `base_content_id`='0000000891';
+UPDATE `talentcloud`.`base_content` SET `base_content_value`='Voir le profil complet' WHERE `base_content_id`='0000000892';
