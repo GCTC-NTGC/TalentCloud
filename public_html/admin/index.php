fecd22f8296a2c5d76080a4da854433aca0e4302
Grant <grant.d.barnes@gmail.com>
2018-04-09 15:38:21 -0400

    Consistant tab spacing on index pages

--- public_html/admin/index.php
+++ public_html/admin/index.php
@@ -8,44 +8,50 @@ and open the template in the editor .
 <head>
     <title>GC Talent Cloud</title>
     <!-- Includes for metadata / scripts -->
-    <?php include '../inc/common/head.php'; ?>
-    <?php include '../inc/manager/head-admin.php'; ?>
+    <?php
+    include '../inc/common/head.php';
+    include '../inc/manager/head-admin.php';
+    ?>
 </head>
 
 <body>
-<!-- Include for Federal Identity Program (black banner) -->
-<?php include '../inc/manager/header-fip.php'; ?>
-<!-- Include for main navigation -->
-<?php include '../inc/manager/header-nav.php'; ?>
+    <!-- Include for Federal Identity Program (black banner) -->
+    <?php include '../inc/manager/header-fip.php'; ?>
+    <!-- Include for main navigation -->
+    <?php include '../inc/manager/header-nav.php'; ?>
 
-<!-- BEGIN - Overlays (all should be children of this div) -->
-<div id="overlays">
-    <!-- BEGIN - Includes for modal dialogs -->
-    <?php include '../inc/manager/modal-registration.php';?>
-    <?php include '../inc/manager/modal-login.php';?>
-    <?php include '../inc/manager/modal-upload-photo.php';?>
-    <?php include '../inc/manager/modal-yes-no.php';?>
-    <?php include '../inc/manager/modal-update.php';?>
-    <!-- END - Modal dialogs -->
-</div>
-<!-- END - Overlays -->
+    <!-- BEGIN - Overlays (all should be children of this div) -->
+    <div id="overlays">
+        <!-- BEGIN - Includes for modal dialogs -->
+        <?php
+        include '../inc/manager/modal-registration.php';
+        include '../inc/manager/modal-login.php';
+        include '../inc/manager/modal-upload-photo.php';
+        include '../inc/manager/modal-yes-no.php';
+        include '../inc/manager/modal-update.php';
+        ?>
+        <!-- END - Modal dialogs -->
+    </div>
+    <!-- END - Overlays -->
 
-<!-- BEGIN - Page Content-->
-<main>
-    <!-- BEGIN - Includes for pages -->
-    <?php include "../inc/manager/page-home-content.php"; ?>
-    <?php include "../inc/manager/page-job-seeker.php"; ?>
-    <?php include "../inc/manager/page-create-job-poster.php"; ?>
-    <?php include "../inc/manager/page-profile-about-me.php"; ?>
-    <?php include "../inc/manager/page-profile-leadership-style.php"; ?>
-    <?php include "../inc/manager/page-profile-work-environment.php"; ?>
-    <?php include "../inc/manager/page-profile-team-culture.php"; ?>
-    <?php include "../inc/manager/page-profile-other.php"; ?>
-    <!-- END - Includes for pages -->
-</main>
-<!-- END - Page Content -->
+    <!-- BEGIN - Page Content-->
+    <main>
+        <!-- BEGIN - Includes for pages -->
+        <?php
+        include "../inc/manager/page-home-content.php";
+        include "../inc/manager/page-job-seeker.php";
+        include "../inc/manager/page-create-job-poster.php";
+        include "../inc/manager/page-profile-about-me.php";
+        include "../inc/manager/page-profile-leadership-style.php";
+        include "../inc/manager/page-profile-work-environment.php";
+        include "../inc/manager/page-profile-team-culture.php";
+        include "../inc/manager/page-profile-other.php";
+        ?>
+        <!-- END - Includes for pages -->
+    </main>
+    <!-- END - Page Content -->
 
-<!-- Include for footer -->
-<?php include '../inc/manager/footer.php'; ?>
+    <!-- Include for footer -->
+    <?php include '../inc/manager/footer.php'; ?>
 </body>
 </html>
