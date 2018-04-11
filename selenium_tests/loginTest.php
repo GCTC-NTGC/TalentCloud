029fe250893555eea2ca55658d744f272d1c6fde
Gray <obyrne.gray@gmail.com>
2018-04-11 14:12:15 -0400

    Added abstract class for Selenium tests to avoid repeating function in each test.

--- selenium_tests/loginTest.php
+++ selenium_tests/loginTest.php
@@ -1,80 +1,19 @@
 <?php
 
-use Facebook\WebDriver\Remote\DesiredCapabilities;
-use Facebook\WebDriver\Remote\RemoteWebDriver;
-use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverExpectedCondition;
-use Facebook\WebDriver\JavaScriptExecutor;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 require_once '../vendor/autoload.php';
-require_once 'Common/Actions.php';
 require_once 'Common/SeleniumConfig.php';
+require_once 'Common/TalentCloudTest.php';
+require_once 'Common/Actions.php';
+
 /**
  * Description of loginTest
  *
  * @author GBowden
  */
-class loginTest extends PHPUnit_Framework_TestCase {
-
-   
-    /**
-     * @var \RemoteWebDriver
-     */
-    protected $webDriver;
-    
-    protected $session;
-    
-    //Selenium standalone jar proxy url - after running start.bat
-    protected $host = "http://localhost:4444/wd/hub";
-    
-    //site url
-    protected $url = SITE_URL;
-
-    public function setUp() {
-        $options = new ChromeOptions();
-        $options->addArguments(array(
-            '--start-maximized',
-            '--ignore-certificate-errors'
-        ));
-        
-        if(BROWSER_TO_TEST == 'chrome'){
-            if(BINARYPATH_CHROME !== ''){
-                $options->setBinary(BINARYPATH_CHROME);
-            }
-        }
-        
-        if(BROWSER_TO_TEST == 'firefox'){
-            if(BINARYPATH_FIREFOX !== ''){
-                $options->setBinary(BINARYPATH_FIREFOX);
-            }
-        }
-        
-        $caps = DesiredCapabilities::chrome();
-        $caps->setCapability(ChromeOptions::CAPABILITY, $options);
-                
-        $this->webDriver = RemoteWebDriver::create($this->host, $caps);
-        
-    }
-
-    public function tearDown() {
-        if($this->webDriver != null){
-            $status = $this->getStatus();
-            if ($status == PHPUnit_Runner_BaseTestRunner::STATUS_ERROR || $status == PHPUnit_Runner_BaseTestRunner::STATUS_FAILURE) {
-                if (SCREENSHOTS_DIR !== ''){
-                $now = new DateTime();
-                $dateStr = $now->format('Y-m-d_H-i-s');
-                    $this->webDriver->takeScreenshot(SCREENSHOTS_DIR.'test_'.$dateStr.'.png');
-                }
-            }
-            $this->webDriver->close();
-        }
-    }
+class loginTest extends talentCloudTest {
 
     public function testLogin() {
         $this->webDriver->get($this->url);
@@ -98,7 +37,7 @@ class loginTest extends PHPUnit_Framework_TestCase {
         
         $user_id = $sessionUserJSON->user_id;
         var_dump($user_id);
-        $this->assertTrue($user_id == 1, "No user Id");
+        $this->assertTrue($user_id > 0, "No user Id");
         
         $authTokenJSONArray = $this->webDriver->executeScript('return UserAPI.getAuthTokenAsJSON();');
         print 'get JWT token from sessionstorage' . PHP_EOL;
