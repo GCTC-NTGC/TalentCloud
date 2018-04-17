ad7fe7fa391f40630c5670432b6579249bab3a03
joshdrink <imjoshdrink@gmail.com>
2018-04-17 10:45:13 -0400

    Admin Profile Changes

--- public_html/inc/manager/page-profile-about-me.php
+++ public_html/inc/manager/page-profile-about-me.php
@@ -18,180 +18,159 @@
         </div>
     </div>
 
+    <br>
+
     <div class="stepGroupForm">
 
-        <h3 id="adminAboutMe">About Me</h3>
+        <h3 id="adminAboutMe" class="manager-edit-profile__title heading--03">About Me</h3>
+
+        <div class="manager-edit-profile__required-copy-wrapper">
+            <span>*</span>
+            <span id="createEditProfile_requiredStep1">Required</span>
+        </div>
 
         <div class="flex-grid top manager-edit-profile__form">
 
             <div class="box med-1of2 form-group">
-                <label for="createEditProfile_bio">
+                <label for="createEditProfile_bio" class="form__label">
                     <span id="createEditProfile_bio_label">A little bit about me</span>
                     <strong id="createEditProfile_bio_error" class="error hidden">
                         <span id="createEditProfile_bio_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <textarea class="form-control full-width" name="createEditProfile_bio" id="createEditProfile_bio"></textarea>
-                </div>
+                <textarea class="form__textarea" name="createEditProfile_bio" id="createEditProfile_bio"></textarea>
             </div>
 
             <div class="box med-1of2 form-group">
-                <label for="createEditProfile_bio_fr">
+                <label for="createEditProfile_bio_fr" class="form__label">
                     <span id="createEditProfile_bio_fr_label">A little bit about me_fr: *</span>
                     <strong id="createEditProfile_bio_fr_error" class="error hidden">
                         <span id="createEditProfile_bio_fr_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <textarea class="form-control full-width" name="createEditProfile_bio_fr" id="createEditProfile_bio_fr"></textarea>
-                </div>
+                <textarea class="form__textarea" name="createEditProfile_bio_fr" id="createEditProfile_bio_fr"></textarea>
             </div>
 
             <div class="box med-1of2 form-group">
-                <label for="createEditProfile_proudOf">
+                <label for="createEditProfile_proudOf" class="form__label">
                     <span id="createEditProfile_proudOf_label">What I'm most proud of in my career</span>
                     <strong id="createEditProfile_proudOf_error" class="error hidden">
                         <span id="createEditProfile_proudOf_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <textarea class="form-control full-width" name="createEditProfile_proudOf" id="createEditProfile_proudOf"></textarea>
-                </div>
+                <textarea class="form__textarea" name="createEditProfile_proudOf" id="createEditProfile_proudOf"></textarea>
             </div>
 
             <div class="box med-1of2 form-group">
-                <label for="createEditProfile_proudOf_fr">
+                <label for="createEditProfile_proudOf_fr" class="form__label">
                     <span id="createEditProfile_proudOf_fr_label">What I'm most proud of in my career</span>
                     <strong id="createEditProfile_proudOf_fr_error" class="error hidden">
                         <span id="createEditProfile_proudOf_fr_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <textarea class="form-control full-width" name="createEditProfile_proudOf_fr" id="createEditProfile_proudOf_fr"></textarea>
-                </div>
+                <textarea class="form__textarea" name="createEditProfile_proudOf_fr" id="createEditProfile_proudOf_fr"></textarea>
             </div>
 
             <div class="box med-1of2 form-group">
-                <label for="createEditProfile_position">
+                <label for="createEditProfile_position" class="form__label">
                     <span id="createEditProfile_position_label">Position</span>
                     <strong id="createEditProfile_position_error" class="error hidden">
                         <span id="createEditProfile_position_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <input type="text" class="form-control full-width" name="createEditProfile_position" id="createEditProfile_position"/>
-                </div>
+                <input type="text" class="form__input--text" name="createEditProfile_position" id="createEditProfile_position"/>
             </div>
 
             <div class="box med-1of2 form-group">
-                <label for="createEditProfile_position_fr">
+                <label for="createEditProfile_position_fr" class="form__label">
                     <span id="createEditProfile_position_fr_label">Position_fr: *</span>
                     <strong id="createEditProfile_position_fr_error" class="error hidden">
                         <span id="createEditProfile_position_fr_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <input class="form-control full-width" type="text" name="createEditProfile_position_fr" id="createEditProfile_position_fr"/>
-                </div>
+                <input class="form__input--text" type="text" name="createEditProfile_position_fr" id="createEditProfile_position_fr"/>
             </div>
 
             <div class="box form-group">
-                <label for="createEditProfile_department">
+                <label for="createEditProfile_department" class="form__label">
                     <span id="createEditProfile_department_label">Department</span>
                     <strong id="createEditProfile_department_error" class="error hidden">
                         <span id="createEditProfile_department_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
+                <div class="form__select-wrapper">
                     <!--<input type="text" class="form-control full-width" name="createEditProfile_department" id="createEditProfile_department"/>-->
-                    <select class="form-control" name="createEditProfile_department" id="createEditProfile_department">
+                    <select class="form__select" name="createEditProfile_department" id="createEditProfile_department">
                         <option value="">--</option>
                     </select>
                 </div>
             </div>
 
             <div class="box med-1of2 form-group">
-                <label for="createEditProfile_branch">
+                <label for="createEditProfile_branch" class="form__label">
                     <span><span id="createEditProfile_branch_label">Branch</span> (English):</span>
                     <strong id="createEditProfile_branch_error" class="error hidden">
                         <span id="createEditProfile_branch_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <input type="text" class="form-control full-width" name="createEditProfile_branch" id="createEditProfile_branch"/>
-                </div>
+                <input type="text" class="form__input--text" name="createEditProfile_branch" id="createEditProfile_branch"/>
             </div>
 
             <div class="box med-1of2 form-group">
-                <label for="createEditProfile_branch_fr">
+                <label for="createEditProfile_branch_fr" class="form__label">
                     <span><span id="createEditProfile_branch_fr_label">Branch</span> (francais):</span>
                     <strong id="createEditProfile_branch_fr_error" class="error hidden">
                         <span id="createEditProfile_branch_fr_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <input type="text" class="form-control full-width" name="createEditProfile_branch_fr" id="createEditProfile_branch_fr"/>
-                </div>
+                <input type="text" class="form__input--text" name="createEditProfile_branch_fr" id="createEditProfile_branch_fr"/>
             </div>
 
             <div class="box med-1of2 form-group">
-                <label for="createEditProfile_division">
+                <label for="createEditProfile_division" class="form__label">
                     <span><span id="createEditProfile_division_label">Division</span> (English):</span>
                     <strong id="createEditProfile_division_error" class="error hidden">
                         <span id="createEditProfile_division_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <input type="text" class="form-control full-width" name="createEditProfile_division" id="createEditProfile_division"/>
-                </div>
+                <input type="text" class="form__input--text" name="createEditProfile_division" id="createEditProfile_division"/>
             </div>
 
             <div class="box med-1of2 form-group">
-                <label for="createEditProfile_division_fr">
+                <label for="createEditProfile_division_fr" class="form__label">
                     <span><span id="createEditProfile_division_fr_label">Division</span> (English):</span>
                     <strong id="createEditProfile_division_fr_error" class="error hidden">
                         <span id="createEditProfile_division_fr_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <input class="form-control full-width" type="text" name="createEditProfile_division_fr" id="createEditProfile_division_fr"/>
-                </div>
+                <input class="form__input--text" type="text" name="createEditProfile_division_fr" id="createEditProfile_division_fr"/>
             </div>
 
             <div class="box form-group">
-                <label for="createEditProfile_twitter">
+                <label for="createEditProfile_twitter" class="form__label">
                     <span id="createEditProfile_twitter_label">Twitter</span>
                     <strong id="createEditProfile_twitter_error" class="error hidden">
                         <span id="createEditProfile_twitter_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <input class="form-control full-width" type="text" name="createEditProfile_twitter" id="createEditProfile_twitter" placeholder="@Username"/>
-                </div>
+                <input class="form__input--text" type="text" name="createEditProfile_twitter" id="createEditProfile_twitter" placeholder="@Username"/>
             </div>
 
             <div class="box form-group">
-                <label for="createEditProfile_linkedin">
+                <label for="createEditProfile_linkedin" class="form__label">
                     <span id="createEditProfile_linkedin_label">LinkedIn</span>
                     <strong id="createEditProfile_linkedin_error" class="error hidden">
                         <span id="createEditProfile_linkedin_error_msg" class="label label-danger"></span>
                     </strong>
                 </label>
-                <div>
-                    <input class="form-control full-width" type="text" name="createEditProfile_linkedin" id="createEditProfile_linkedin" placeholder="www.linkedin.com/in/Username"/>
-                </div>
+                <input class="form__input--text" type="text" name="createEditProfile_linkedin" id="createEditProfile_linkedin" placeholder="www.linkedin.com/in/Username"/>
             </div>
 
         </div>
 
-        <div class="createEditProfileSubmitPane">
-            <div class="formGroup insert">
-                <span>*</span>
-                <span id="createEditProfile_requiredStep1">Required</span>
-            </div>
-            <div class="formGroup">
-                <input type="button" id="createEditProfile_goToStep2_1" value="Save" onclick="CreateEditProfileAPI.validateStep1();"/>
+        <div class="createEditProfileSubmitPane flex-grid top manager-edit-profile__button-form">
+            <div class="box full">
+                <input class="button--yellow" type="button" id="createEditProfile_goToStep2_1" value="Save" onclick="CreateEditProfileAPI.validateStep1();"/>
             </div>
         </div>
 
