39845ac526657207ccd44f28499d231cabd321a3
Grant <grant.d.barnes@gmail.com>
2018-04-18 09:36:58 -0400

    Add CREATE / USE to v20 DB dump

--- public_html/tc/sql/~ create (full DB)/version_20_dump.sql
+++ public_html/tc/sql/~ create (full DB)/version_20_dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `talentcloud` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `talentcloud`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: f.gccollab.ca    Database: talentcloud
