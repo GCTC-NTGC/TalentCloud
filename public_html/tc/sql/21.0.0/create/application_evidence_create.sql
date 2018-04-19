8d1cc08bbc96f1aa303fec438ca4af363f2cade3
Unknown <tristan.m.orourke@gmail.com>
2018-04-19 10:55:31 -0400

    Deleted redundant scripts
    They're collectively duplicated in skill_declaration_create_tables.sql

--- public_html/tc/sql/21.0.0/create/application_evidence_create.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `talentcloud`.`application_evidence` (
-  `job_poster_application_id` INT(10) ZEROFILL UNSIGNED NOT NULL,
-  `evidence_id` INT(10) UNSIGNED ZEROFILL NOT NULL,
-  PRIMARY KEY (`job_poster_application_id`, `evidence_id`));
