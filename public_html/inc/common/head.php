bc1df3cca882b0c7e38642953a727ff74438e117
Grant <grant.d.barnes@gmail.com>
2018-04-23 20:34:25 -0400

    Better Font Awesome link

--- public_html/inc/common/head.php
+++ public_html/inc/common/head.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Font Awesome -->
-<script src="https://use.fontawesome.com/9fe1581a5a.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 <!-- WET / CDS -->
 <link href="/css/theme.min.css" rel="stylesheet" type="text/css"/>
 <link href="/css/theme.css" rel="stylesheet" type="text/css"/>
