f0a9adfa9917ca7cd4e3c7cb810174b3a8bfefcb
Unknown <tristan.m.orourke@gmail.com>
2018-05-07 15:03:41 -0400

    Fixed up evidence preview panel for use as a template

--- public_html/inc/applicant/partial-applicant-evidence-preview.php
+++ public_html/inc/applicant/partial-applicant-evidence-preview.php
@@ -8,29 +8,32 @@
 
                 <span class="applicant-evidence-preview__desktop-menu-title">Essential Criteria</span>
 
-                <button role="tab" aria-selected="true" class="applicant-evidence-preview__desktop-menu-item active" data-evidence-trigger="skill01">
-                    Wireframing
-                </button>
-
-                <button role="tab" aria-selected="false" class="applicant-evidence-preview__desktop-menu-item" data-evidence-trigger="skill02">
-                    User Research
-                </button>
+                <div id="applicationPreviewEssentialEvidenceWrapper"></div>
 
                 <span class="applicant-evidence-preview__desktop-menu-title">Asset Criteria</span>
 
-                <button role="tab" aria-selected="false" class="applicant-evidence-preview__desktop-menu-item" data-evidence-trigger="skill03">
-                    HTML
-                </button>
+                <div id="applicationPreviewEssentialEvidenceWrapper"></div>
+
+                <div class="hidden" id="applicationPrevierwEvidenceMenuItemTemplate">
+                    <button role="tab" aria-selected="true" class="applicant-evidence-preview__desktop-menu-item template" data-evidence-trigger="">
+                    </button>
+                </div>
 
             </div>
 
-            <div class="box lg-3of4 applicant-evidence-preview__form-wrapper">
+            <div class="box lg-3of4 applicant-evidence-preview__form-wrapper">   
+                <?php //This wrapper will be populated with Preview Evidence panels at runtime ?>
+            </div>
 
-                <div role="tabpanel" class="applicant-evidence-preview__accordion-wrapper active" data-evidence-target="skill01" aria-labelledby="">
+            <?php //The following Preview Evidence Panel template can be cloned and used to populate the applicant-evidence-preview__form-wrapper ?>
+            <div id="applicationPreviewEvidencePanelTemplate" class="hidden">
+                <div role="tabpanel" class="applicant-evidence-preview__accordion-wrapper template" data-evidence-target="skill01" aria-labelledby="">
 
                     <div class="applicant-evidence-preview__accordion-trigger active" type="button" tabindex="0" aria-expanded="true">
                         <span class="applicant-evidence-preview__accordion-trigger-title">
-                            Wireframing - Intermediate - 3 Years
+                            <span class="applicant-evidence-preview__criteria-name">Wireframing</span> - 
+                            <span class="applicant-evidence-preview__expertise">Intermediate</span> - 
+                            <span class="applicant-evidence-preview__experience">3-5 Years</span>
                         </span>
                     </div>
 
@@ -56,20 +59,25 @@
                                 Micro-reference
                             </h5>
 
-                            <p class="applicant-evidence-preview__reference-metadata">
-                                <strong>Reference Name </strong>
-                                <span>- Title - </span>
-                                <span>(00/0000) - (00/0000)</span>
-                            </p>
+                            <div class="applicant-evidence-preview__reference-content hidden">
 
-                            <p class="applicant-evidence-preview__reference-status">
-                                <strong>Status: </strong>
-                                <span><i class="fa fa-check-circle"></i> Response Received</span>
-                            </p>
+                                <p class="applicant-evidence-preview__reference-metadata">
+                                    <strong class="applicant-evidence-preview__reference-name">Reference Name</strong> - 
+                                    <span class="applicant-evidence-preview__reference-relationship">Relationship</span> - 
+                                    <span class="applicant-evidence-preview__reference-start-date">(00/0000)</span> - 
+                                    <span class="applicant-evidence-preview__reference-end-date">(00/0000)</span>
+                                </p>
 
-                            <p class="applicant-evidence-preview__reference-copy">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper sodales sem, sit amet rutrum arcu facilisis sed. Mauris consectetur id odio a congue. Ut faucibus tincidunt nulla fermentum sagittis. Vivamus posuere odio non sem maximus, in tristique lacus posuere."</p>
+                                <p class="applicant-evidence-preview__reference-status">
+                                    <strong>Status: </strong>
+                                    <span><i class="fa fa-check-circle"></i> Response Received</span>
+                                </p>
 
-                            <?php // <p class="applicant-evidence-preview__reference-null">No reference was provided.</p> ?>
+                                <p class="applicant-evidence-preview__reference-copy">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper sodales sem, sit amet rutrum arcu facilisis sed. Mauris consectetur id odio a congue. Ut faucibus tincidunt nulla fermentum sagittis. Vivamus posuere odio non sem maximus, in tristique lacus posuere."</p>
+
+                            </div>
+
+                            <p class="applicant-evidence-preview__reference-null">No reference was provided.</p>
 
                         </div>
 
@@ -81,25 +89,24 @@
                                 <i class="fa fa-file"></i>
                                 Skill Sample
                             </h5>
-                            
-                            <?php /*
-                            <p class="applicant-evidence-preview__evidence-metadata">
-                                <strong>Project Name </strong>
-                                <span>- (00/0000)</span>
-                            </p>
 
-                            <p class="applicant-evidence-preview__evidence-status">
-                                <strong>Status: </strong>
-                                <span><i class="fa fa-exclamation-triangle"></i> Awaiting Corroboration</span>
-                            </p>
+                            <div class="applicant-evidence-preview__evidence-content hidden">
 
-                            <p class="applicant-evidence-preview__evidence-copy"><strong>Contribution:</strong> "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper sodales sem, sit amet rutrum arcu facilisis sed. Mauris consectetur id odio a congue. Ut faucibus tincidunt nulla fermentum sagittis. Vivamus posuere odio non sem maximus, in tristique lacus posuere."</p>
+                                <p class="applicant-evidence-preview__evidence-metadata">
+                                    <strong class="applicant-evidence-preview__evidence-name">Project Name</strong> - 
+                                    <span class="applicant-evidence-preview__evidence-date">(00/0000)</span>
+                                </p>
 
-                            <a href="" title="View this evidence." target="_blank" class="applicant-evidence-preview__evidence-link button--blue">View Evidence</a>
-                            
-                             * 
-                             */
-                            ?>
+                                <p class="applicant-evidence-preview__evidence-status">
+                                    <strong>Status: </strong>
+                                    <span><i class="fa fa-exclamation-triangle"></i> Awaiting Corroboration</span>
+                                </p>
+
+                                <p class="applicant-evidence-preview__evidence-copy"><strong>Contribution:</strong> <span class="applicant-evidence-preview__evidence-story">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper sodales sem, sit amet rutrum arcu facilisis sed. Mauris consectetur id odio a congue. Ut faucibus tincidunt nulla fermentum sagittis. Vivamus posuere odio non sem maximus, in tristique lacus posuere.</span></p> 
+
+                                <a href="" title="View this evidence." target="_blank" class="applicant-evidence-preview__evidence-link button--blue">View Evidence</a>
+
+                            </div>
 
                             <p class="applicant-evidence-preview__evidence-null">No evidence was provided.</p>
 
@@ -108,169 +115,6 @@
                     </div>
 
                 </div>
-
-                <?php // DEV-NOTE: The skills below are simply for FED testing. Please reuse the first skill as the markup template. These also act as examples of what the skills look like without the active states on the "completion-wrapper" and "optional-wrapper" elements. ?>
-
-                <div role="tabpanel" class="applicant-evidence-preview__accordion-wrapper" data-evidence-target="skill02" aria-labelledby="">
-
-                    <div class="applicant-evidence-preview__accordion-trigger" type="button" tabindex="0" aria-expanded="true">
-                        <span class="applicant-evidence-preview__accordion-trigger-title">
-                            User Research - Master - 10 Years
-                        </span>
-                    </div>
-
-                    <div class="applicant-evidence-preview__accordion-content">
-
-                        <div class="applicant-evidence-preview__experience-wrapper">
-
-                            <h5 class="applicant-evidence-preview__section-title">
-                                <i class="fa fa-check"></i>
-                                Experience &amp; Knowledge
-                            </h5>
-
-                            <p class="applicant-evidence-preview__experience-copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper sodales sem, sit amet rutrum arcu facilisis sed. Mauris consectetur id odio a congue. Ut faucibus tincidunt nulla fermentum sagittis. Vivamus posuere odio non sem maximus, in tristique lacus posuere.</p>
-
-                        </div>
-
-                        <div class="applicant-evidence-preview__reference-wrapper">
-
-                            <hr class="applicant-evidence-preview__divider">
-
-                            <h5 class="applicant-evidence-preview__section-title">
-                                <i class="fa fa-user"></i>
-                                Micro-reference
-                            </h5>
-
-                            <p class="applicant-evidence-preview__reference-metadata">
-                                <strong>Reference Name </strong>
-                                <span>- Title - </span>
-                                <span>(00/0000) - (00/0000)</span>
-                            </p>
-
-                            <p class="applicant-evidence-preview__reference-status">
-                                <strong>Status: </strong>
-                                <span><i class="fa fa-check-circle"></i> Response Received</span>
-                            </p>
-
-                            <p class="applicant-evidence-preview__reference-copy">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper sodales sem, sit amet rutrum arcu facilisis sed. Mauris consectetur id odio a congue. Ut faucibus tincidunt nulla fermentum sagittis. Vivamus posuere odio non sem maximus, in tristique lacus posuere."</p>
-
-                            <?php // <p class="applicant-evidence-preview__reference-null">No reference was provided.</p> ?>
-
-                        </div>
-
-                        <div class="applicant-evidence-preview__evidence-wrapper">
-
-                            <hr class="applicant-evidence-preview__divider">
-
-                            <h5 class="applicant-evidence-preview__section-title">
-                                <i class="fa fa-file"></i>
-                                Skill Sample
-                            </h5>
-
-                            <p class="applicant-evidence-preview__evidence-metadata">
-                                <strong>Project Name </strong>
-                                <span>- (00/0000)</span>
-                            </p>
-
-                            <p class="applicant-evidence-preview__evidence-status">
-                                <strong>Status: </strong>
-                                <span><i class="fa fa-exclamation-triangle"></i> Awaiting Corroboration</span>
-                            </p>
-
-                            <p class="applicant-evidence-preview__evidence-copy"><strong>Contribution:</strong> "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper sodales sem, sit amet rutrum arcu facilisis sed. Mauris consectetur id odio a congue. Ut faucibus tincidunt nulla fermentum sagittis. Vivamus posuere odio non sem maximus, in tristique lacus posuere."</p>
-
-                            <a href="" title="View this evidence." target="_blank" class="applicant-evidence-preview__evidence-link button--blue">View Evidence</a>
-
-                            <?php // <p class="applicant-evidence-preview__evidence-null">No evidence was provided.</p> ?>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div role="tabpanel" class="applicant-evidence-preview__accordion-wrapper" data-evidence-target="skill03" aria-labelledby="">
-
-                    <div class="applicant-evidence-preview__accordion-trigger" type="button" tabindex="0" aria-expanded="true">
-                        <span class="applicant-evidence-preview__accordion-trigger-title">
-                            HTML - Beginner - 1 Year
-                        </span>
-                    </div>
-
-                    <div class="applicant-evidence-preview__accordion-content">
-
-                        <div class="applicant-evidence-preview__experience-wrapper">
-
-                            <h5 class="applicant-evidence-preview__section-title">
-                                <i class="fa fa-check"></i>
-                                Experience &amp; Knowledge
-                            </h5>
-
-                            <p class="applicant-evidence-preview__experience-copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper sodales sem, sit amet rutrum arcu facilisis sed. Mauris consectetur id odio a congue. Ut faucibus tincidunt nulla fermentum sagittis. Vivamus posuere odio non sem maximus, in tristique lacus posuere.</p>
-
-                        </div>
-
-                        <div class="applicant-evidence-preview__reference-wrapper">
-
-                            <hr class="applicant-evidence-preview__divider">
-
-                            <h5 class="applicant-evidence-preview__section-title">
-                                <i class="fa fa-user"></i>
-                                Micro-reference
-                            </h5>
-
-                            <p class="applicant-evidence-preview__reference-metadata">
-                                <strong>Reference Name </strong>
-                                <span>- Title - </span>
-                                <span>(00/0000) - (00/0000)</span>
-                            </p>
-
-                            <p class="applicant-evidence-preview__reference-status">
-                                <strong>Status: </strong>
-                                <span><i class="fa fa-check-circle"></i> Response Received</span>
-                            </p>
-
-                            <p class="applicant-evidence-preview__reference-copy">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper sodales sem, sit amet rutrum arcu facilisis sed. Mauris consectetur id odio a congue. Ut faucibus tincidunt nulla fermentum sagittis. Vivamus posuere odio non sem maximus, in tristique lacus posuere."</p>
-
-                            <?php // <p class="applicant-evidence-preview__reference-null">No reference was provided.</p> ?>
-
-                        </div>
-
-                        <div class="applicant-evidence-preview__evidence-wrapper">
-
-                            <hr class="applicant-evidence-preview__divider">
-
-                            <h5 class="applicant-evidence-preview__section-title">
-                                <i class="fa fa-file"></i>
-                                Skill Sample
-                            </h5>
-
-                            <?php /*
-                            <p class="applicant-evidence-preview__evidence-metadata">
-                                <strong>Project Name </strong>
-                                <span>- (00/0000)</span>
-                            </p>
-
-                            <p class="applicant-evidence-preview__evidence-status">
-                                <strong>Status: </strong>
-                                <span><i class="fa fa-exclamation-triangle"></i> Awaiting Corroboration</span>
-                            </p>
-
-                            <p class="applicant-evidence-preview__evidence-copy"><strong>Contribution:</strong> "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper sodales sem, sit amet rutrum arcu facilisis sed. Mauris consectetur id odio a congue. Ut faucibus tincidunt nulla fermentum sagittis. Vivamus posuere odio non sem maximus, in tristique lacus posuere."</p>
-
-                            <a href="" title="View this evidence." target="_blank" class="applicant-evidence-preview__evidence-link button--blue">View Evidence</a> 
-                             * 
-                             */
-                            ?>
-
-                            <p class="applicant-evidence-preview__evidence-null">No evidence was provided.</p>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
             </div>
 
         </div>
