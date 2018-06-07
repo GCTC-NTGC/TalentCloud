01f9a44d95db783fb8ed303892cca566eb35a994
Grant <grant.d.barnes@gmail.com>
2018-06-07 14:53:53 -0400

    Removing unused files

--- public_html/wiremock/run.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-exec('java -jar /ebs2/vhosts/f.gccollab.ca/tc.gccollab.ca/wiremock/wiremock-standalone-2.6.0.jar --port 8082 --https-port 8083 --verbose --preserve-host-header > output.txt', $result);
-print_r($result);
-
-?>
