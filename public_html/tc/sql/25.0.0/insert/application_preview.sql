d8b4538cb270618af0b64592ed96941a504f799e
queenie_fung <qf062@192.168.1.66>
2018-06-26 08:53:45 -0600

    fixed typo

--- public_html/tc/sql/25.0.0/insert/application_preview.sql
+++ public_html/tc/sql/25.0.0/insert/application_preview.sql
@@ -4,6 +4,8 @@ VALUES
 
 (1,'applicant-evidence-preview__status=response-received','Response Received',1),
 (1,'applicant-evidence-preview__status=response-received','Réponse reçue',2),
-(1,'applicant-evidence-preview__status=awaiting=authentication','Awaiting Authentication',1),
-(1,'applicant-evidence-preview__status=awaiting=authentication','En attente d’authentification',2)
+(1,'applicant-evidence-preview__status-awaiting-authentication','Awaiting Authentication',1),
+(1,'applicant-evidence-preview__status-awaiting-authentication','En attente d’authentification',2),
+(1,'applicant-evidence-preview__evidence-status-label','Status',1),
+(1,'applicant-evidence-preview__evidence-status-label','État',2),
 ;
