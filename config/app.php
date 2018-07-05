87d9a059e942b2ac964f70d0bec93d56bb2c8eeb
Unknown <tristan.m.orourke@gmail.com>
2018-07-05 19:01:43 -0400

    Added a library for helping debug laravel apps

--- config/app.php
+++ config/app.php
@@ -176,6 +176,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        /*
+         * Debug Service Providers...
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -225,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
