361eb40c7bef86ee7ebaddb2b43834281439c85f
Unknown <tristan.m.orourke@gmail.com>
2018-07-12 19:57:40 -0400

    Made user role a foreign key in user table

---
+++ database/migrations/2018_07_12_145517_add_foreign_keys_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddForeignKeysToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->foreign('user_role_id')->references('id')->on('user_roles')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropForeign('users_user_role_id_foreign');
+		});
+	}
+
+}
