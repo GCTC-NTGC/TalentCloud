9249280603b9b55cc08b77feaa938328cfb07034
Unknown <tristan.m.orourke@gmail.com>
2018-07-16 13:25:51 -0400

    Added a simple page to test/showcase translatable models

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    $test = array("firstVar" => "blah blah");
-    return view('applicant/homepage', ["test" => $test ]);
+use App\Models\ApplicantProfileQuestion;
+
+Route::get('/locale_test', function () {
+    App::setLocale('fr');
+    /* @var $question ApplicantProfileQuestion */
+    $question = ApplicantProfileQuestion::first();
+    //$question localizable properties , like value and description, will be automatically translated with default locale
+    //Non localized properties,like name, are also available
+    //Other locales are available if we specify a translation
+    
+    $test = array("firstVar" => $question->value, 
+        "secondVar" => $question->name, 
+        "thirdVar" => $question->translate('en')->description);
+    return view('applicant/locale_test', ["test" => $test ]);
 });
 
 Route::get('/laravel', function () {
