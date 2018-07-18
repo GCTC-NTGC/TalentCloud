964143ace9fb93a019da012f7ccc2b9a00f8f659
Unknown <tristan.m.orourke@gmail.com>
2018-07-18 16:21:01 -0400

    Added Home page controller. Moved english content into lang file

---
+++ resources/lang/en/common/hero.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+     * --------------------------------------------------------------------------
+     * Hero Image Language Lines
+     * --------------------------------------------------------------------------
+     *
+     * The following language lines are used in the Hero image
+     *
+     */
+
+    "hero_logo_alt" => "The GC Talent Cloud Logo.",
+    "hero_logo" => "/images/logo_tc_colour.png",
+    "hero_tagline" => "People want meaningful work.",
+];
