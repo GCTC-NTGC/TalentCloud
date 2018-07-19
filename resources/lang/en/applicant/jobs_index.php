267143e64424f1085e5e837ae8710d9aa798b197
Unknown <tristan.m.orourke@gmail.com>
2018-07-19 13:46:03 -0400

    Finished matching jobs.index view to model

--- resources/lang/en/applicant/jobs_index.php
+++ resources/lang/en/applicant/jobs_index.php
@@ -14,10 +14,16 @@ return [
         "view_title" => "View the job post for",
         "location_label" => "Location",
         "salary_label" => "Salary",
+        "duration" => [
+            'week' => ':count week.|:count weeks.',
+            'month' => ':count month.|:count months.',
+            'year' => ':count year.|:count year.',
+            'permanent' => 'Permanent.'
+        ],
         "duration_label" => "Duration",
         "remote_label" => "Remote Work",
-        "days_remaining_label" => "days until close.",
-        "applicants_label" => "applicants so far.",
+        "days_remaining_label" => "day until close.|days until close.",
+        "applicants_label" => "applicant so far.|applicants so far.",
         "button_label" => "View Job"
     ]
 ];
