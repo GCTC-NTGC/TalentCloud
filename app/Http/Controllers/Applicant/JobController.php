0562e10a4d3b2c6d703c1ef52246ea39db814c4a
Unknown <tristan.m.orourke@gmail.com>
2018-08-10 15:44:46 -0400

    Removed some comments

--- app/Http/Controllers/Applicant/JobController.php
+++ app/Http/Controllers/Applicant/JobController.php
@@ -16,35 +16,6 @@ class JobController extends Controller
      */
     public function index()
     {
-        /*
-         *
-        "jobs" => [
-            "0" => [
-                "link" => "/browse/jobs/00/",
-                "title" => "Front-end Developer",
-                "department" => "Treasury Board of Canada Secretariat",
-                "city" => "Ottawa",
-                "province" => "Ontario",
-                "salary" => "80,000 - 120,000",
-                "duration" => "1 Year",
-                "remote" => "Allowed",
-                "days_remaining" => "12",
-                "applicants" => "2"
-            ],
-            "1" => [
-                "link" => "/brwose/jobs/01/",
-                "title" => "Consultant",
-                "department" => "Treasury Board of Canada Secretariat",
-                "city" => "Ottawa",
-                "province" => "Ontario",
-                "salary" => "85,000 - 115,000",
-                "duration" => "6 Months",
-                "remote" => "Not Allowed",
-                "days_remaining" => "1",
-                "applicants" => "10"
-            ]
-        ]
-         */
         $now = Carbon::now();
         //Find jobs that are currently open for applications
         $jobs = JobPoster::where('open_date_time', '<=', $now)->where('close_date_time', '>=', $now)->get();
