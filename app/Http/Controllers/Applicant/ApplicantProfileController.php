287934d408dc5127bda93082e47a4c957b265a3f
Unknown <tristan.m.orourke@gmail.com>
2018-08-10 18:22:09 -0400

    Created ApplicantProfileController, and edit route

---
+++ app/Http/Controllers/Applicant/ApplicantProfileController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Applicant;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\Applicant;
+use App\Models\Lookup\ApplicantProfileQuestion;
+use App\Models\ApplicantProfileAnswer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ApplicantProfileController extends Controller
+{
+
+    protected $questionFromNamePrefix = "applicantProfileQuestion_";
+    protected $twitterProfilePrefix = 'https://twitter.com/';
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Applicant  $applicant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Applicant $applicant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the logged-in user's applicant profile
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Request $request)
+    {
+        $user = $request->user();
+        $applicant = $user->applicant;
+        $profileQuestions = ApplicantProfileQuestion::all();
+
+        $profileText = 'job_index' => Lang::get('applicant/job_index');
+
+        $profileQuestionForms = [];
+        foreach ($profileQuestions as $question) {
+            $answerObj = $applicant->applicant_profile_answers->
+                where('applicant_profile_question_id', $question->id);
+            $answer = $answerObj ? $answerObj->answer : null;
+
+            $formValues = ['value' => $question->value,
+                'description' => $question->description,
+                'answer' => $answer,
+                'answer_label' => $profileText->'answer_label',
+                'input_name' => $this->$questionFromNamePrefix . $question->id
+            ];
+            array_push($profileQuestionForms, $formValues);
+        }
+
+        $userProfile = [
+            'name' => $user->name;
+            'tagline' => $applicant->tagline;
+            'photo' => '/images/user.png', //TODO: get real photos
+            'twitter' => [
+                'url' => $twitterProfilePrefix . $applicant->twitter_username,
+                'title' => Lang::get('applicant/applicant_profile.twitter_link_title', $user->name),
+            ],
+            'linkedin' => [
+                'url' => $applicant->linkedin_url,
+                'title' => Lang::get('applicant/applicant_profile.linkedin_link_title', $user->name),
+            ]
+        ]
+
+
+        return view('applicant/profile', [
+            /* Localized strings*/
+            'profile' => $profileText,
+            /* Applicant Profile Questions */
+            'applicant_profile_questions' => $profileQuestionForms,
+            /* User Data */
+            "user" => $userProfile,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Applicant  $applicant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Applicant $applicant)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -919,7 +919,7 @@ Route::get('applications/00/step-04', function () {
 
 Route::get('applications/00/step-05', function () {
     return view('applicant/application_post_05', [
-        
+
     ]);
 })->name('application.post05');
 
@@ -987,72 +987,7 @@ Route::get('browse/managers/123', function () {
 
 /* Profile */
 
-Route::get('profile', function () {
-    return view('applicant/profile', [
-        "profile" => [
-            "save_button_label" => "Save My Answer"
-        ],
-        /* Applicant Profile Questions */
-        "applicant_profile_questions" => [
-            "0" => [
-                "value" => "My career journey so far...",
-                "description" => "This is your chance to share the unique story of how you got to where you are now… and where you want to go from here.",
-                "answer_label" => "Your Answer",
-                "input_name" => "applicantProfileQuestion0",
-                "answer" => null
-            ],
-            "1" => [
-                "value" => "My learning journey so far...",
-                "description" => "Learning never stops, and it comes to all of us in different ways. Whether it comes from formal education or life lessons, knowledge passed on from elders or things you’ve picked up along the way, here’s your chance to share a bit about this side of who you are.",
-                "answer_label" => "Your Answer",
-                "input_name" => "applicantProfileQuestion1",
-                "answer" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut ante aliquet felis finibus luctus. Vivamus justo ante, convallis id justo sed, venenatis ornare magna. Maecenas tempor nunc sit amet mollis venenatis. Proin vitae nunc interdum, porttitor leo a, mollis diam. Sed auctor ultricies massa at aliquam."
-            ],
-            "2" => [
-                "value" => "What I bring to a team...",
-                "description" => "People take note of the rock star and forget they are nothing without the band. Help potential teams and managers see what unique skills, attributes and knowledge you bring to help a team do great work.",
-                "answer_label" => "Your Answer",
-                "input_name" => "applicantProfileQuestion2",
-                "answer" => null
-            ],
-            "3" => [
-                "value" => "I work best when...",
-                "description" => "Introvert? Extrovert? Bit of both? Do you like tight deadlines or do you prefer to have time to process ideas? Do you work well independently or are team products more your thing? Here’s your chance to let a potential manager know what will let you give the team your best.",
-                "answer_label" => "Your Answer",
-                "input_name" => "applicantProfileQuestion3",
-                "answer" => null
-            ],
-            "4" => [
-                "value" => "I learn best when...",
-                "description" => "Do you absorb information best by reading? By doing? Or are you a visual learner? Do you pick things up quickly or do you like to develop deep expertise over time? Joining a new team means learning new things. Help a potential manager understand your learning style so you can get up to speed and contributing quickly.",
-                "answer_label" => "Your Answer",
-                "input_name" => "applicantProfileQuestion4",
-                "answer" => null
-            ],
-            "5" => [
-                "value" => "Types of teams I work well on...",
-                "description" => "Do you absorb information best by reading? By doing? Or are you a visual learner? Do you pick things up quickly or do you like to develop deep expertise over time? Joining a new team means learning new things. Help a potential manager understand your learning style so you can get up to speed and contributing quickly.",
-                "answer_label" => "Your Answer",
-                "input_name" => "applicantProfileQuestion5",
-                "answer" => null
-            ],
-        ],
-        /* User Data */
-        "user" => [
-            "name" => "Jason Greene",
-            "tagline" => "This is Jason's default tagline.",
-            "photo" => "https://talentcloud-nuagedetalents.gccollab.ca/tc/api/v1/profilePic/10?v=7661",
-            "twitter" => [
-                "url" => "https://twitter.com/joshdrink",
-                "title" => "Visit Jason's Twitter profile."
-            ],
-            "linkedin" => [
-                "url" => "https://linkedin.com/joshdrink",
-                "title" => "Visit Jason's Linkedin profile."
-            ]
-        ]
-    ]);
-})->name('profile');
+Route::get('profile', 'Applicant\ApplicantProfileController@edit')->name('profile')->middleware('auth');
 
 /* Auth */
 
