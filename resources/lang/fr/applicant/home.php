e03ab6f203441319b717e744b6632c0d9b644eb7
Gray O'Byrne <obyrne.gray@gmail.com>
2018-08-15 14:36:16 -0400

    Added french translation text.

--- resources/lang/fr/applicant/home.php
+++ resources/lang/fr/applicant/home.php
@@ -9,26 +9,23 @@ return [
      * The following language lines are used in the applicant home page.
      *
      */
-    "about_card_copy_01" => "The jobs are not real (for now).",
-    "about_card_copy_02" => "The platform is experimental.",
-    "about_cta" => "Help us build a new hiring model for the Government of Canada.",
-    "about_copy" => "GC Talent Cloud is making government hiring transparent and connecting hiring managers to people with mad wicked skills, like you.",
-    "how_title" => "How It Works",
-    "how_intro" => "GC Talent Cloud connects you to teams and projects where you can use your unique skills to make a difference in the lives of Canadians.",
-    "how_copy" => "We want GC Talent Cloud to be a drive engine that allows more Canadians to have a chance to work in Government. We want diverse talent to bring new ideas that will shape programs and services across Canada.",
-    "how_cta_copy" => "Interested in chatting about a potential partnership?",
+    "about_card_copy_01" => "Les emplois ne sont pas reels (pour l'instant).",
+    "about_card_copy_02" => "La plateforme est expérimentale.",
+    "about_cta" => "Aidez-nous à élaborer un nouveau modèle d'embauche pour le gouvernement du Canada.",
+    "about_copy" => "Le Nuage de talents du GC s'efforce de rendre le recrutement du gouvernement transparent et met en relation les responsables du recrutement avec des personnes aux compétences insensées, comme vous",
+    "how_title" => "Comment fonctionne-t- il?",
+    "how_intro" => "Le Nuage de talents du GC vous relie à des équipes et à des projets vous permettant d'utiliser vos compétences uniques pour faire une différence dans la vie des Canadiens.",
+    "how_copy" => "Nous voulons voir le Nuage de talents du GC devenir un moteur qui offre la chance de travailler au gouvernement à un plus grand nombre de Canadiens. Nous voulons des talents diversifiés qui apportent des idées neuves et façonneront les programmes et services partout au Canada.",
+    "how_cta_copy" => "Vous voulez discuter d'un éventuel partenariat?",
     "how_cta_link" => "talent.cloud-nuage.de.talents@tbs-sct.gc.ca",
-    "how_cta_title" => "Get in touch with GC Talent Cloud via email.",
-    "how_cta_label" => "Contact Us!",
-    "team_title" => "Our Team",
-    "team_copy" => "We are a small but growing team of public servants passionate about the future of talent in Canada. Learn more about us and make your own contribution to GC Talent Cloud by joining us on one of these channels.",
+    "how_cta_title" => "Communiquez avec nous!",
+    "how_cta_label" => "Contactez-nous!",
+    "team_title" => "Notre équipe",
+    "team_copy" => "Nous formons une petite équipe en pleine croissance de fonctionnaires qui se passionnent pour l'avenir du talent au Canada. Apprenez-en davantage à notre sujet et apportez votre propre contribution au Nuage de talents du GC en vous joignant à nous sur l’un de ces canaux.",
     "team_button_gccollab_link" => "https://gccollab.ca/groups/profile/19750/talent-cloud-nuage-de-talent",
-    "team_button_gccollab_title" => "Visit the team on GCCollab.",
-    "team_button_gccollab_label" => "GC Collab",
+    "team_button_gccollab_title" => "Visitez l'équipe sur GCcollab.",
+    "team_button_gccollab_label" => "GCcollab",
     "team_button_twitter_link" => "https://twitter.com/TalentCloudGC",
-    "team_button_twitter_title" => "Visit the team on Twitter.",
+    "team_button_twitter_title" => "Visitez l'équipe sur Twitter.",
     "team_button_twitter_label" => "Twitter"
 ];
-
-
-
