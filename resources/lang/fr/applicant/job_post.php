3bb3b2cc8aa89e9a2ab44cd3cc3323a911910f83
Gray O'Byrne <obyrne.gray@gmail.com>
2018-09-03 10:49:24 -0400

    French translation

---
+++ resources/lang/fr/applicant/job_post.php
@@ -0,0 +1,114 @@
+<?php
+return [
+    /*
+    * --------------------------------------------------------------------------
+    * Job Poster Language Lines
+    * --------------------------------------------------------------------------
+    *
+    * The following language lines are used on the Job Poster page.
+    *
+    */
+    'title' => 'Parcourir les emplois',
+    'header' => [
+        'days_remaining' => ':count jour jusqu\'à la date de clôture|:count jours jusqu\'à la date de clôture',
+        'applicants_so_far' => ':count candidat jusqu\'à présent|:count candidats jusqu\'à présent',
+        'location_icon_label' => 'Symbole d'emplacement.',
+        'remote_work_icon_label' => 'Symbole de travail à distance.',
+        'reference_id' => 'Numéro de référence #:id',
+        'remote_work_allowed' => [
+            true => 'Travail à distance autorisé',
+            false => 'Travail à distance non autorisé',
+        ],
+        'sidebar_label' => 'À propos de l\'emploi:',
+    ],
+    'basics' => [
+        'sidebar_title' => 'Voir les renseignements généraux pour cet emploi.',
+        'title' => 'Renseignements généraux',
+        'salary_label' => 'Échelle salariale',
+        'duration_label' => 'Durée',
+        'start_label' => 'Date cible de début d\'emploi',
+        'language_label' => 'Exigence linguistique',
+        'security_label' => 'Niveau d\'autorisation de sécurité',
+        'classification_label' => 'Classification gouvernemental',
+        'duration' => [
+            'week' => ':count semaine.|:count semaines',
+            'month' => ':count mois.|:count mois',
+            'year' => ':count un année.|:count un années',
+            'permanent' => 'Permanent'
+        ],
+        'start_date_format' => 'F, Y',
+    ],
+    'manager' => [
+        'photo_title' => 'Photo de profile de :name.',
+        'link_title' => 'Voir le profil de :name.'
+    ],
+    'impact' => [
+        'sidebar_title' => 'Voir la contribution de cet emploi.',
+        'title' => 'Contribution'
+    ],
+    'work' => [
+        'sidebar_title' => 'Voir le travail pour cet emploi.',
+        'title' => 'Votre travail'
+    ],
+    'criteria' => [
+        'sidebar_title' => 'Voir les critères pour cet emploi.',
+        'title' => 'Critères',
+        'essential_title' => 'Nécessaires d\'avoir',
+        'asset_title' => 'Bon d\'avoir'
+    ],
+    'culture' => [
+        'sidebar_title' => 'Voir la culture à cet emploi.',
+        'title' => 'Culture d\'équipe',
+        'manager_title' => 'Votre gestionnaire',
+        'manager_department_bridge' => ' au ',
+        'manager_link_label' => 'Voir le profil de :name.',
+        'work_environment_label' => 'Environnement de travail',
+        'team_narrative_label' => 'Ce qu\'il faut savoir',
+        'team_culture_label' => 'Culture d\'équipe',
+        'team_size_label' => 'Taille de l\'équipe',
+        'gcdirectory_label' => 'Rencontre l\'équipe dans GCdirectory',
+        'team_link_title' => 'View le profile de l\'équipe.',
+        'team_link_label' => 'Profile de l\'équipe',
+        'operating_label' => 'Notre contexte opérationnel',
+        'team_value_label' => 'Nos valeurs',
+        'team_work_label' => 'Notre mode de fonctionnement'
+    ],
+    'work_environment' => [
+        'remote_work_label' => 'Travail à distance autorisé:',
+        'remote_work_allowed' => [
+            true => 'Oui',
+            false => 'Non',
+        ],
+        'telework_label' => 'Télétravail',
+        'telework_allowed' => [
+            'never' => 'Jamais',
+            'rarely' => 'Rarement',
+            'sometimes' => 'Parfois',
+            'often' => 'Habituellement',
+            'always' => 'Presque toujours',
+        ],
+        'time_flexibility_label' => 'Horaire flexible',
+        'time_flexibility_allowed' => [
+            'never' => 'Jamais',
+            'rarely' => 'Rarement',
+            'sometimes' => 'Parfois',
+            'often' => 'Habituellement',
+            'always' => 'Presque toujours',
+        ],
+    ],
+    'know' => [
+        'sidebar_title' => 'Voir les informations supplémentaires pour cet emploi.',
+        'title' => 'Bon à savoir
+'
+    ],
+    'apply' => [
+        'sidebar_title' => 'Voir la section de candidature pour cet emploi.',
+        'title' => 'Postuler maintenant',
+        'accommodation' => 'Veuillez informer l\'équipe du nuage de talents de toute mesure d\'adaptation dont vous pourriez avez besoin au cours du processus en écrivant à talent.cloud-nuage.de.talents@tbs-sct.gc.ca.',
+        'preference' => 'On accordera la préférence aux anciens combattants et aux citoyens canadiens dans cet ordre.',
+        'apply_link_title' => 'Postuler pour cet emploi.',
+        'apply_link_label' => 'Postuler maintenant',
+        'login_link_title' => 'Ouvrir une session et postuler pour cet emploi.',
+        'login_link_label' => 'Ouvrir une session et postuler '
+    ],
+];
