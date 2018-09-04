7bf78a308c24bf7ea12dba5942b3148ab615e7b7
Unknown <tristan.m.orourke@gmail.com>
2018-09-04 18:01:00 -0400

    Restoring nav bar menu functions
    Hides inaccessible menu items, highlights current one

--- app/Http/ViewComposers/MenuComposer.php
+++ app/Http/ViewComposers/MenuComposer.php
@@ -19,17 +19,17 @@ class MenuComposer
     public function compose(View $view)
     {
         if (WhichPortal::isApplicantPortal()) {
-            $menuItems = Lang::get('applicant/menu');
+            $menu = Lang::get('applicant/menu');
 
             //Set active on the proper item
             switch(Route::currentRouteName()) {
                 case 'home':
-                    $menuItems['home']['active'] = true;
+                    $menu['items']['home']['active'] = true;
                     break;
                 case 'jobs.index':
                 case 'jobs.show':
                 case 'managers.show':
-                    $menuItems['jobs']['active'] = true;
+                    $menu['items']['jobs']['active'] = true;
                     break;
                 case 'applications.index':
                 case 'applications.edit':
@@ -38,21 +38,21 @@ class MenuComposer
                 case 'applications.edit.3':
                 case 'applications.edit.4':
                 case 'applications.edit.5':
-                    $menuItems['applications']['active'] = true;
+                    $menu['items']['applications']['active'] = true;
                     break;
                 case 'profile':
                 case 'profile.edit':
                 case 'profile.show':
-                    $menuItems['profile']['active'] = true;
+                    $menu['items']['profile']['active'] = true;
                     break;
                 case 'register':
-                    $menuItems['register']['active'] = true;
+                    $menu['items']['register']['active'] = true;
                     break;
                 case 'login':
-                    $menuItems['login']['active'] = true;
+                    $menu['items']['login']['active'] = true;
                     break;
                 case 'logout':
-                    $menuItems['logout']['active'] = true;
+                    $menu['items']['logout']['active'] = true;
                     break;
                 default:
                     //No menu item will be active
@@ -61,44 +61,44 @@ class MenuComposer
 
             //Check if use is logged in, and remove invalid menu items
             if (Auth::check()) {
-                unset($menuItems['login']);
-                unset($menuItems['register']);
+                unset($menu['items']['login']);
+                unset($menu['items']['register']);
                 //TODO set profile like using user slug
             } else {
-                unset($menuItems['logout']);
-                unset($menuItems['applications']);
-                unset($menuItems['profile']);
+                unset($menu['items']['logout']);
+                unset($menu['items']['applications']);
+                unset($menu['items']['profile']);
             }
         } else if (WhichPortal::isManagerPortal()) {
-            $menuItems = Lang::get('manager/menu');
+            $menu = Lang::get('manager/menu');
 
             //Set active on the proper item
             switch(Route::currentRouteName()) {
                 case 'manager.home':
-                    $menuItems['home']['active'] = true;
+                    $menu['items']['home']['active'] = true;
                     break;
                 case 'manager.jobs.index':
                 case 'manager.jobs.show':
-                    $menuItems['jobs']['active'] = true;
+                    $menu['items']['jobs']['active'] = true;
                     break;
                 case 'manager.jobs.create':
                 case 'manager.jobs.edit':
                 case 'manager.jobs.update':
-                    $menuItems['create_job']['active'] = true;
+                    $menu['items']['create_job']['active'] = true;
                     break;
                 case 'manager.profile':
                 case 'manager.profile.edit':
                 case 'manager.profile.show':
-                    $menuItems['profile']['active'] = true;
+                    $menu['items']['profile']['active'] = true;
                     break;
                 case 'register':
-                    $menuItems['register']['active'] = true;
+                    $menu['items']['register']['active'] = true;
                     break;
                 case 'login':
-                    $menuItems['login']['active'] = true;
+                    $menu['items']['login']['active'] = true;
                     break;
                 case 'logout':
-                    $menuItems['logout']['active'] = true;
+                    $menu['items']['logout']['active'] = true;
                     break;
                 default:
                     //No menu item will be active
@@ -107,14 +107,14 @@ class MenuComposer
 
             //Check if use is logged in, and remove invalid menu items
             if (Auth::check()) {
-                unset($menuItems['login']);
-                unset($menuItems['register']);
+                unset($menu['items']['login']);
+                unset($menu['items']['register']);
                 //TODO set profile like using user slug
             } else {
-                unset($menuItems['logout']);
-                unset($menuItems['jobs']);
-                unset($menuItems['create_job']);
-                unset($menuItems['profile']);
+                unset($menu['items']['logout']);
+                unset($menu['items']['jobs']);
+                unset($menu['items']['create_job']);
+                unset($menu['items']['profile']);
             }
         }
         if (WhichPortal::isManagerPortal()) {
@@ -133,7 +133,7 @@ class MenuComposer
             ];
         }
 
-        $view->with('menu', $menuItems)
+        $view->with('menu', $menu)
             ->with('login_modals', $loginModals);
     }
 }
