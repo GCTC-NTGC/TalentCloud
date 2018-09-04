acc357ce59355fdbac12228e12a14131f966a71e
Unknown <tristan.m.orourke@gmail.com>
2018-09-04 12:11:48 -0400

    Create new Skills tables+models, edit existing SkillDeclaration

---
+++ database/migrations/2018_09_04_143244_create_skills_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('skill_type_id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('skill_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills');
+        Schema::dropIfExists('skill_types');
+    }
+}
