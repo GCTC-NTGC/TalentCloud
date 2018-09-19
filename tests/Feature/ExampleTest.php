f050bd576f188199024bb5c7fc04ce14c8cf6b32
Grant <grant.d.barnes@gmail.com>
2018-09-19 17:05:14 -0400

    edit example test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,8 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        //$response = $this->get('/');
+        $response = $this->get('https://www.fsdjgfkjxhgfkjeygdfjkdzygflkjdhfblxk.com');
 
-        $response->assertStatus(200);
+        $response->assertStatus(404);
     }
 }
