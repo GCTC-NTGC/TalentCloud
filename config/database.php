b71f17cbd56954b21eea88d2903f3a077ee438fd
Grant <grant.d.barnes@gmail.com>
2018-09-21 09:22:10 -0400

    edit heroku config

--- config/database.php
+++ config/database.php
@@ -67,6 +67,17 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'pg-heroku' => [
+            'driver'   => 'pgsql',
+            'host'     => $heroku_db_url['host'],
+            'database' => substr($heroku_db_url['path'], 1),
+            'username' => $heroku_db_url['user'],
+            'password' => $heroku_db_url['pass'],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
