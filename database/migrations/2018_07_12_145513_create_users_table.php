3a0c2fed749278c3fbe80760f6b27dfb0ef55a07
Unknown <tristan.m.orourke@gmail.com>
2018-10-03 17:22:05 -0400

    Restore user role in migrations

--- database/migrations/2018_07_12_145513_create_users_table.php
+++ database/migrations/2018_07_12_145513_create_users_table.php
@@ -1,35 +1,39 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
         Schema::create('users', function (Blueprint $table) {
+		{
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+			$table->integer('user_role_id')->unsigned()->index();
+			$table->string('open_id_sub')->unique()->index();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
         Schema::dropIfExists('users');
     }
+
 }
