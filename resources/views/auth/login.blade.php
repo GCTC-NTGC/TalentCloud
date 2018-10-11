3b6ff7a8b2df64997657a85a9f8080b7e67275aa
Unknown <tristan.m.orourke@gmail.com>
2018-10-11 12:13:43 -0400

    Show login errors

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,6 +38,14 @@
 
                         <div
                             class="login-copy">
+    
+                            @if(!empty($errors->first()))
+                                <div class="row col-lg-12">
+                                    <div class="alert alert-danger">
+                                        <span>{{ $errors->first() }}</span>
+                                    </div>
+                                </div>
+                            @endif
 
                             <div
                                 class="form__input-wrapper--float @if (old('email'))active @endif">
