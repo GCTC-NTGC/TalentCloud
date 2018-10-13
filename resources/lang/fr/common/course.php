90a030b001c1175d3e9c72b1c8f0a72cfe9ad2ff
laurenoncoffee <44123072+laurenoncoffee@users.noreply.github.com>
2018-10-13 15:34:15 -0400

    setup page and partial translation

---
+++ resources/lang/fr/common/course.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    /*
+     * --------------------------------------------------------------------------
+     * French Course Language Lines
+     * --------------------------------------------------------------------------
+     *
+     * The following language lines are used when displaying Course objects
+     *
+     */
+     "new_course_label" => "Nouveau cours ou certificat",
+     "name_label" => "Nom du cours ou de la certification",
+     "institution_label" => "Institution",
+     "status_label" => "Statut",
+     "statuses" => [
+         "certificate_granted" => "Certificat de cours accordé",
+         "credits_towards_degree" => "Crédits vers un diplome (Cours réussi)",
+         "audited" => "Audited",
+         "online_no_proof" => "Cours en ligne (No Proof of Completion)",
+         "online_certificate" => "Cours en ligne (With Certificate/License)",
+         "learning_in_progress" => "Learning in Progress"
+     ],
+     "start_date_label" => "Start Date",
+     "end_date_label" => "End Date",
+     "action_01" => "Delete Diploma/Degree",
+     "action_02" => "Save Diploma/Degree"
+];   
