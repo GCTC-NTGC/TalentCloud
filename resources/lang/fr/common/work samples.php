538a16a5d3b83e2c74f5517cf720c927fef1bccd
laurenoncoffee <44123072+laurenoncoffee@users.noreply.github.com>
2018-10-14 11:43:45 -0400

    french text added

---
+++ resources/lang/fr/common/work samples.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    /*
+    * --------------------------------------------------------------------------
+    * French Work Sample Language Lines
+    * --------------------------------------------------------------------------
+    *
+    * The following language lines are used when displaying Work Samples
+    *
+    */
+    "new_sample_label" => "Nouvel échantillon de travail",
+    "name_label" => "Nom du projet",
+    "type_label" => "Type du projet",
+    "types" => [
+        "word" => "Document en Word",
+        "powerpoint" => "Powerpoint",
+        "video" => "Vidéo",
+        "publication" => "Article publié",
+        "other" => "Autre",
+    ],
+    "link_label" => "La connexion à votre travail",
+    "description_label" => "L'histoire de ce travail",
+    "linked_skills_label" => "Compétences liées",
+    "search_label" => "Rechercher dans mes compétences",
+    "skill_label" => "Sélectionner une compétence",
+    "add_skill_label" => "Ajouter une compétence",
+    "action_01" => "Supprimer cet exemple de travail",
+    "action_02" => "Enregistrer cet exemple de travail"
+];
