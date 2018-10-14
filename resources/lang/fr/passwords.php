08ece4f5b6e3285e818821d4dcabfc173671a817
Gray <obyrne.gray@gmail.com>
2018-10-14 12:00:24 -0400

    Added french translations for auth, password and applicant

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | French Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => "Les mots de passe doivent comporter au moins six caractères et correspondre à la confirmation.",
+    'reset' => "Your password has been reset!",
+    'sent' => "Votre mot de passe a été réinitialisé!",
+    'token' => "Ce jeton de réinitialisation de mot de passe n'est pas valide.",
+    'user' => "Nous ne pouvons trouver un utilisateur avec cette address courriel.",
+];
