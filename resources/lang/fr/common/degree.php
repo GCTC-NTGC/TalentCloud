e1025f4f39febe55a216acfe4c18b5d731283e46
laurenoncoffee <44123072+laurenoncoffee@users.noreply.github.com>
2018-10-14 10:25:58 -0400

    page creation and french edits

--- resources/lang/fr/common/degree.php
+++ resources/lang/fr/common/degree.php
@@ -2,18 +2,18 @@
 return [
     /*
      * --------------------------------------------------------------------------
-     * Degree Language Lines
+     * French Degree Language Lines
      * --------------------------------------------------------------------------
      *
      * The following language lines are used when displaying Degree objects
      *
      */
-     "new_degree_label" => "New Diploma/Degree",
+     "new_degree_label" => "Nouveau diplôme ou diplôme universitaire",
      "type_label" => "Type",
      "types" => [
-         "diploma" => "Diploma",
-         "bachelors" => "Bachelor's Degree",
-         "masters" => "Master's Degree",
+         "diploma" => "diplôme",
+         "bachelors" => "licence",
+         "masters" => "maîtrise",
          "phd" => "PhD"
      ],
      "aoe_label" => "Area of Study",
