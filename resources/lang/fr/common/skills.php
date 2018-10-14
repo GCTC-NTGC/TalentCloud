e04e0522eb5e8789b9e854ab934814ba27fabaa4
laurenoncoffee <44123072+laurenoncoffee@users.noreply.github.com>
2018-10-14 11:17:47 -0400

    french text created

---
+++ resources/lang/fr/common/skills.php
@@ -0,0 +1,51 @@
+<?php
+return [
+    /*
+    * --------------------------------------------------------------------------
+    * French Skills Language Lines
+    * --------------------------------------------------------------------------
+    *
+    * The following language lines are used when displaying Skills
+    *
+    */
+    'new_skill_title' => 'Nouvelle compétence',
+    'name_label' => 'Nom du projet',
+    'type_label' => 'Type du projet',
+    'skill_selection_label' => 'Sélectionner une compétence',
+    'level_label' => 'Mon niveau de compétence',
+    'knowledge_label' => 'Ma connaissance',
+    'skill_status_label' => 'Statut: compétence',
+    'skill_levels' => [
+        'hard' => [
+            'basic' => 'Débutant',
+            'intermediate' => 'Intermédiaire',
+            'advanced' => 'Avancé',
+            'expert' => 'Responsable'
+        ],
+        'soft' => [
+            'basic' => 'Phase de développement précoce',
+            'intermediate' => 'Modérément en évidence',
+            'advanced' => 'Fortement en évidence',
+            'expert' => 'Démonstration à un niveau profond'
+        ],
+    ],
+    'action_01' => 'Supprimer cette compétence',
+    'action_02' => 'Enregistrer cette compétence',
+    'skills' => [
+        'html' => [
+            'name' => 'HTML',
+            'description' => 'lorem ipsum html '
+        ],
+        'css' => [
+            'name' => 'CSS',
+            'description' => 'lorem ipsum css'
+        ],
+        'ux_research' => [
+            'name' => 'UX Research',
+            'description' => 'lorem ipsum ux research'
+        ],
+    ],
+    'status' => [
+        'claimed' => 'Claimed'
+    ]
+];
