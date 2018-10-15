59164672a5f91abf409b944a69be179f4b349575
laurenoncoffee <44123072+laurenoncoffee@users.noreply.github.com>
2018-10-15 14:49:00 -0400

    french additions and swapping out of :attribute

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -10,9 +10,12 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-    'password' => "Les mots de passe doivent comporter au moins six caractères et correspondre à la confirmation.",
-    'reset' => "Your password has been reset!",
-    'sent' => "Votre mot de passe a été réinitialisé!",
+    'password' => "Les mots de passe doivent comporter au moins huit caractères et correspondre à la confirmation.",
+    'reset' => "Votre mot de passe a été réinitialisé.",
+    'sent' => "Nous avons envoyé par courriel votre lien de réinitialisation de mot de passe.",
     'token' => "Ce jeton de réinitialisation de mot de passe n'est pas valide.",
-    'user' => "Nous ne pouvons trouver un utilisateur avec cette address courriel.",
+    'user' => "Nous ne pouvons pas trouver un utilisateur avec cette address courriel.",
+  'password_validation' => [
+        'regex' => 'Le mot de passe doit contenir au moins un caractère appartenant à trois des catégories suivantes: minuscules (az), majuscules (AZ), chiffres (0-9) et symboles non alphanumériques (%, $,!, etc.). ',
+    ]
 ];
