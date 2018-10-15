187de682d96c4f994d48846c34dbbcf94905e089
laurenoncoffee <44123072+laurenoncoffee@users.noreply.github.com>
2018-10-14 20:38:03 -0400

    minor french edits and additional lines

--- resources/lang/fr/applicant/applicant_profile.php
+++ resources/lang/fr/applicant/applicant_profile.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
     /*
     * --------------------------------------------------------------------------
@@ -9,14 +8,32 @@ return [
     * The following language lines are used in the applicant profile.
     *
     */
-    "save_button_label" => "Enregistrer ma réponse",
-    "answer_label" => "Votre réponse",
-    'account_form_legend' => 'Account Information',
-    'user_name_label' => 'Mon nom',
-    'user_email_label' => 'Mon courriel',
-    'password_form_legend' => 'Réinitialiser mon mot de passe',
-    'user_new_password_label' => 'Nouveau mot de passe',
-    'user_confirm_new_password_label' => 'Confirmer le nouveau mot de passe',
-    "twitter_link_title" => "Voir le profil twitter de :name",
-    "linkedin_link_title" => "Voir le profil LinkedIn de :name",  
+    'title' => 'About Me',
+    'about_section' => [
+        'about_title' => 'My Information',
+        'about_gc_disclaimer' => 'Please note that your name can be changed through your GC Account settings. Profile photos will be added as a feature at a later time.',
+        'gc_link' => 'https://account.gccollab.ca/profile/',
+        'gc_link_title' => 'Visit your GC Account in a new tab or window.',
+        'gc_link_label' => 'Visit GC Account',
+        'account_form_legend' => 'Renseignements du compte',
+        'user_name_label' => 'Mon nom',
+        'user_email_label' => 'Mon courriel',
+        'password_form_legend' => 'Réinitialiser mon mot de passe',
+        'user_old_password_label' => 'Ancien mot de passe',
+        'user_new_password_label' => 'Nouveau mot de passe',
+        'user_confirm_new_password_label' => 'Confirmer le nouveau mot de passe',
+        'social_media_form_legend' => 'Des médias sociaux',
+        'twitter_label' => 'Voir le profil Twitter de :name',
+        'linkedin_label' => 'Voir le profil LinkedIn de :name',
+        'other_form_legend' => 'Autres renseignements',
+        'tagline_label' => 'Tagline',
+        'save_button_label' => 'Enregistrer ma réponse',
+        'answer_label' => 'Votre réponse',
+        'twitter_link_title' => 'Visiter le profil Twitter de :name',
+        'linkedin_link_title' => 'Visit le profil LinkedIn de :name',
+        'questions_title' => 'Choses à savoir de moi',
+        'applicant_profile_question_section_title' => 'Votre histoire',
+        'applicant_profile_question_context' => 'Ne pas inclure d\'informations personnelles sensibles.',
+    ],
 ];
+
