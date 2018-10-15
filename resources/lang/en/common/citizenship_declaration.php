14da5b7caea83972c32c79ff057ec76c6936bfa0
laurenoncoffee <44123072+laurenoncoffee@users.noreply.github.com>
2018-10-15 16:41:53 -0400

    changes to work permit open/closed

--- resources/lang/en/common/citizenship_declaration.php
+++ resources/lang/en/common/citizenship_declaration.php
@@ -12,7 +12,7 @@ return [
 
     'citizen' => "Canadian Citizen",
     'permanent_resident' => "Permanent Resident of Canada",
-    'work_permit_open' => "Open - Work Permit",
-    'work_permit_closed' => "Closed - Work Permit",
+    'work_permit_open' => "I have a permit that allows me to work anywhere in Canada",
+    'work_permit_closed' => "I have a permit that only allows me to work for a specific organization",
     'not_entitled' => "I am currently not entitled to work in Canada"
 ];
