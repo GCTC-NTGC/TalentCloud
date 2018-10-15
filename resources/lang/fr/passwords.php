47ad20be3046d7345367bde77ab43d78b101dca9
Unknown <tristan.m.orourke@gmail.com>
2018-10-15 17:57:46 -0400

    Added some validation attribute translations

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -15,7 +15,5 @@ return [
     'sent' => "Nous avons envoyé par courriel votre lien de réinitialisation de mot de passe.",
     'token' => "Ce jeton de réinitialisation de mot de passe n'est pas valide.",
     'user' => "Nous ne pouvons pas trouver un utilisateur avec cette address courriel.",
-  'password_validation' => [
-        'regex' => 'Le mot de passe doit contenir au moins un caractère appartenant à trois des catégories suivantes: minuscules (az), majuscules (AZ), chiffres (0-9) et symboles non alphanumériques (%, $,!, etc.). ',
-    ]
+      
 ];
