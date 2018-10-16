0ccac4a513e9ef497b6f2549fa1c129f3b04c6fa
Unknown <tristan.m.orourke@gmail.com>
2018-10-16 19:34:16 -0400

    Moved Forgot Password form language to lang files

---
+++ resources/lang/fr/common/auth/forgot_password.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+     * --------------------------------------------------------------------------
+     * Forgot Password Language Lines
+     * --------------------------------------------------------------------------
+     *
+     * The following language lines are used on the forgot password form
+     *
+     */
+    
+    'title' => 'Reset Your Mot de Passe',
+    'return' => "Return Home",
+    'email' => 'E-Mail Address',
+    'submit' => 'Send Reset Password Link',
+];
