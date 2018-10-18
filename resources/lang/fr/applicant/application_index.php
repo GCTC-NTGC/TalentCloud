186c8855eb00040a4c21f8e49e7946afa7a13c1e
Gray <obyrne.gray@gmail.com>
2018-10-18 14:45:24 -0400

    Added translation for applicant dashboard

---
+++ resources/lang/fr/applicant/application_index.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    /*
+    * --------------------------------------------------------------------------
+    * French Applicant Index Language Lines
+    * --------------------------------------------------------------------------
+    *
+    * The following language lines are used in the applicant index.
+    *
+    */
+    "title" => "Mes demandes d'emploi",
+    "progress_title" => "Demandes d'emploi en cours",
+    "completed_title" => "Demandes d'emploi soumises",
+    "expired_title" => "Demandes d'emploi expirées",
+    "days_remaining_label" => ":count jours restant",
+    "edit_date_label" => "Dernière modification",
+    "post_label" => "Affiche d'emploi",
+    "post_link_title" => "Voir l'affiche pour cette demandes d'emploi",
+    "post_link_label" => "Voir",
+    "manager_profile_link_title" => "Voir le profil de ce superviseur.",
+    "manager_profile_link_label" => "Voir profil",
+    "draft_link_title" => "Modifier cette demande d'emploi.",
+    "draft_link_label" => "Modifier cette demande d'emploi.",
+    "view_link_title" => "Voir la demande d'emploi.",
+    "view_link_label" => "Voir la demande d'emploi.",
+    "delete_title" => "Supprimer cette demande d'emploi.",
+    "null" => [
+        "title" => "Il y a aucune applications."
+    ],
+    "modals" => [
+        "delete_application" => [
+            "type" => "confirmation",
+            "title" => "Supprimer la demande d'emploi.",
+            "content" => [
+                "00" => "Êtes-vous sûr de vouloir supprimer cette demande d'emploi? Tous votre progrès sera perdus.",
+                "01" =>  "Les expériences et compétences sauvegardées resteront accessibles sur votre profil pour une utilisation ultérieure."
+            ],
+            "id" => "deleteTrash",
+            "action_01" => "Annuler",
+            "action_02" => "Supprimer"
+        ],
+    ]
+];
