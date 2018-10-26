995ebf34b90ab8939b23f2004cd4240567efe15a
Mustafa <43754271+Djama613@users.noreply.github.com>
2018-10-26 14:22:47 -0400

    Bugfix 447
    Edited pages to resolve french english graphic on french reset password pages.

---
+++ forgot_password.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+     * --------------------------------------------------------------------------
+     * Forgot Password Language Lines
+     * --------------------------------------------------------------------------
+     *
+     * The following language lines are used on the forgot password form
+     *
+     */
+
+    'title' => 'Réinitialiser votre mot de passe',
+    'return' => "Retour à la page d'accueil",
+    'email' => 'Courriel',
+    'submit' => 'Envoyer le lien de réinitialisation du mot de passe',
+    'auth_logo' => '/images/logo_tc_colour_fr.png'
+
+];
