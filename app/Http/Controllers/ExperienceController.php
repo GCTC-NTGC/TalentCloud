07c1ef5236735fe4fcf02d8622cdd23e779ab44e
Mustafa <mustafa.a.jama@gmail.com>
2018-11-15 09:27:30 -0500

    message

--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -57,13 +57,47 @@ class ExperienceController extends Controller
      */
     public function update(Request $request, Applicant $applicant)
     {
-        $input = $request->input();
-
-        $degrees = $input['degrees'];
+      
 
         //Delete old degrees that weren't resubmitted
         //Note: this must be done before adding new degrees, so we don't delete
         // them right after adding them
+        
+        $messages = Lang::get(''); // Place correct error messages in the quotations here. 
+        $request->validate([
+            'degrees[:template][:id][area_of_study]' => [
+                'required',
+                
+            ],
+            'degrees[:template][:id][institution]' => [
+                'nullable', // Institution is nullable because applicant might have acquired the skills/knowledge on their own or some other way.
+                'max:255',
+                
+           ],
+            'courses[:template][:id][name]' => [
+                'required',
+            ],
+            'courses[:template][:id][institution]' => [
+                'required', // If someone declares a course we should require them to say where they completed this course.
+            ],
+            'work_experiences[:template][:id][role]' => [
+                'required', // If they decide to fill out Equivalent Experience, a Role should be well defined,
+                
+            ], 
+            'work_experiences[:template][:id][company]' => [
+                'nullable', // Not every role belongs to a Company or Group. Someone could be a Hobbyist and as such might be hesistant to put something else down here. So nullable.
+                
+            ],
+            'work_experiences[:template][:id][description]' => [
+                'required',
+                'min:2500' // Hiring managers will probably like atleast a paragraph's worth of information here. Since you tend to have to back up Equivalent Experience more often than other forms of "Experience".
+            ]
+            
+        ]);
+        
+        $input = $request->input();
+
+        $degrees = $input['degrees'];
         foreach($applicant->degrees as $oldDegree) {
             //Check if no degrees were resubmitted, or if this specific one wasn't
             if (!isset($degrees['old']) ||
