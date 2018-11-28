4021852f7086dd27f6be897147d836c051939241
Unknown <tristan.m.orourke@gmail.com>
2018-11-28 11:28:25 -0500

    Created models and tables for ApplicationReview

---
+++ app/Models/Lookup/ReviewDecision.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Lookup;
+
+use App\Models\BaseModel;
+
+/**
+ * Class ReviewDecision
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Jenssegers\Date\Date $created_at
+ * @property \Jenssegers\Date\Date $updated_at
+ */
+class ReviewDecision extends BaseModel {
+
+    protected $fillable = [];
+
+}
---
+++ app/Models/Lookup/ReviewStatus.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Lookup;
+
+use App\Models\BaseModel;
+
+/**
+ * Class ReviewStatus
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Jenssegers\Date\Date $created_at
+ * @property \Jenssegers\Date\Date $updated_at
+ */
+class ReviewStatus extends BaseModel {
+
+    protected $fillable = [];
+
+}
