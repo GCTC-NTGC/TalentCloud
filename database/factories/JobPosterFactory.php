bb6d2ceb20d1d0706d29ee43b36dba072cf1b115
Grant <grant.d.barnes@gmail.com>
2018-11-29 16:45:38 -0500

    test push

--- database/factories/JobPosterFactory.php
+++ database/factories/JobPosterFactory.php
@@ -2,8 +2,21 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\JobPoster::class, function (Faker $faker) {
+$factory->define(App\Models\JobPoster::class, function (Faker $faker) {
     return [
-        //
+        'job_term_id' => JobTerm::inRandomOrder()->first()->id,
+        'term_qty' => $faker->numberBetween(1, 4),
+        'open_date_time' => $faker->dateTimeBetween('-1 months','now'),
+        'close_date_time' => $faker->dateTimeBetween('now', '1 months'),
+        'start_date_time' => $faker->dateTimeBetween('1 months','2 months'),
+        'department_id' => Department::inRandomOrder()->first()->id,
+        'province_id' => Province::inRandomOrder()->first()->id,
+        'salary_min' => $faker->numberBetween(60000, 80000),
+        'salary_max' => $faker->numberBetween(80000,100000),
+        'noc' => $faker->numberBetween(1, 9999),
+        'classification' => $faker->regexify('[A-Z]{2}-0[1-5]'),
+        'security_clearance_id' => SecurityClearance::inRandomOrder()->first()->id,
+        'language_requirement_id' => LanguageRequirement::inRandomOrder()->first()->id,
+        'published' => true,
     ];
 });
