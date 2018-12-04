08f8238e276b1532d9a43404a2bb685ad53209f8
Mustafa <mustafa.a.jama@gmail.com>
2018-12-04 09:40:06 -0500

    update + instructions for new solutions

--- tests/Unit/EnglishFileExistsTest.php
+++ tests/Unit/EnglishFileExistsTest.php
@@ -37,14 +37,19 @@ class EnglishFileExistsTest extends TestCase
 }
 
 // instead of checking if specific files exists, check if every file in the lang folder returns an array. Do it for french and english at same time no seperate test needed.
+// 
+//  Prefix: Write a function, make it a function that returns an array of everything that you could use to pass into Lang::get. (return auth, manager/applicant, everything under resources/lang/en. For now.) 
+ // Loop through that array and make sure everything is passable into Lang::get and use that for other tests. 
 
-/*
+/*---
+ * 
  * 1) Use Scandir to get everything in the folder and loop through them. (same function as step 7) (its a recursive function, if its a file, turn that into a string that we can pass into Lang::get, if its a folder, we need to call this function again using that folder as a location.
  * 2) If its a file, you need to use the name of the file, and pass it to lang::get. 
  * 3) and then use Assert InternalType array. The Assert is to check what comes back from Lang::get. Its possible Lang::get causes an exception, but will show up in tests (probably) 
  * 4) Resources/Lang/EN or FR is where you start looking.
  * 5) Function needs to take a second argument, what am I going to prefix the filename with(arugment 1 is the directory you are looking in), for the Lang::get funcction, start with empty string. (argument 2 is an empty string. ''), 
  * 6) Check Applicant Profile Controller line 43 for a working example of Lang::get. 
+ * 
  * ---
  * 
  * Put the test in one function, but have the function contain two starting points. One for EN/ one for FR. 
