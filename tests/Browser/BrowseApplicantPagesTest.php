864cbd69fa4dea7ad7871afd17cbc56351873743
Grant <grant.d.barnes@gmail.com>
2018-12-07 11:38:23 -0500

    rename test

--- tests/Browser/BrowseApplicantPagesTest.php
+++ tests/Browser/BrowseApplicantPagesTest.php
@@ -6,7 +6,7 @@ use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class BrowsePagesTest extends DuskTestCase
+class BrowseApplicantPagesTest extends DuskTestCase
 {
     /**
      * A basic browser test example.
