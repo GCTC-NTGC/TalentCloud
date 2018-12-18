9c6ac97eeb058213c2e41d397136ee41d06b4c5c
Grant <grant.d.barnes@gmail.com>
2018-12-17 21:37:42 -0500

    table create order

--- database/migrations/2018_09_04_143244_create_skills_table.php
+++ database/migrations/2018_09_04_143244_create_skills_table.php
@@ -13,15 +13,15 @@ class CreateSkillsTable extends Migration
      */
     public function up()
     {
-        Schema::create('skills', function (Blueprint $table) {
+        Schema::create('skill_types', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('skill_type_id')->unsigned();
             $table->string('name');
             $table->timestamps();
         });
 
-        Schema::create('skill_types', function (Blueprint $table) {
+        Schema::create('skills', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('skill_type_id')->unsigned();
             $table->string('name');
             $table->timestamps();
         });
@@ -41,6 +41,6 @@ class CreateSkillsTable extends Migration
     public function down()
     {
         Schema::dropIfExists('skills');
-        //Schema::dropIfExists('skill_types');
+        Schema::dropIfExists('skill_types');
     }
 }
