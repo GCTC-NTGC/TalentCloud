1bbf74c7d8e2915c2d8920dc03eeeadd2c6787a5
Unknown <tristan.m.orourke@gmail.com>
2018-12-21 16:57:49 -0500

    Fix misnamed migration

--- database/migrations/2018_11_28_022508_populate_skill_types.php
+++ database/migrations/2018_11_28_022508_populate_skill_types.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class PopulateLookups extends Migration
+class PopulateSkillTypes extends Migration
 {
     /**
      * Run the migrations.
