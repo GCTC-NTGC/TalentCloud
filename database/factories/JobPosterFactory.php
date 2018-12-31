f9df601f8b3f9eb70612344c58b13a9d1c068947
Chris Wiseman <wiseman.christopher@gmail.com>
2018-12-31 11:54:56 -0400

    Refactor Job Poster factory to populate all properties and utilize states

--- database/factories/JobPosterFactory.php
+++ database/factories/JobPosterFactory.php
@@ -1,28 +1,66 @@
 <?php
 
-use Faker\Generator as Faker;
-
 use App\Models\Lookup\Department;
+use App\Models\JobPoster;
 use App\Models\Lookup\JobTerm;
 use App\Models\Lookup\LanguageRequirement;
+use App\Models\Manager;
 use App\Models\Lookup\Province;
 use App\Models\Lookup\SecurityClearance;
 
-$factory->define(App\Models\JobPoster::class, function (Faker $faker) {
+$faker_fr = Faker\Factory::create('fr');
+
+$factory->define(JobPoster::class, function (Faker\Generator $faker) use ($faker_fr) {
     return [
         'job_term_id' => JobTerm::inRandomOrder()->first()->id,
         'term_qty' => $faker->numberBetween(1, 4),
-        'open_date_time' => $faker->dateTimeBetween('-1 months','now'),
+        'open_date_time' => $faker->dateTimeBetween('-1 months', 'now'),
         'close_date_time' => $faker->dateTimeBetween('now', '1 months'),
-        'start_date_time' => $faker->dateTimeBetween('1 months','2 months'),
+        'start_date_time' => $faker->dateTimeBetween('1 months', '2 months'),
         'department_id' => Department::inRandomOrder()->first()->id,
         'province_id' => Province::inRandomOrder()->first()->id,
         'salary_min' => $faker->numberBetween(60000, 80000),
-        'salary_max' => $faker->numberBetween(80000,100000),
+        'salary_max' => $faker->numberBetween(80000, 100000),
         'noc' => $faker->numberBetween(1, 9999),
         'classification' => $faker->regexify('[A-Z]{2}-0[1-5]'),
         'security_clearance_id' => SecurityClearance::inRandomOrder()->first()->id,
         'language_requirement_id' => LanguageRequirement::inRandomOrder()->first()->id,
-        'published' => true,
+        'remote_work_allowed' => $faker->boolean(50),
+        'manager_id' => Manager::inRandomOrder()->first()->id,
+        'published' => $faker->boolean(50),
+        'city:en' => $faker->city,
+        'title:en' => $faker->word,
+        'impact:en' => $faker->paragraphs(
+            2,
+            true
+        ),
+        'branch:en' => $faker->word,
+        'division:en' => $faker->word,
+        'education:en' => $faker->sentence(),
+        'city:fr' => $faker_fr->city,
+        'title:fr' => $faker_fr->word,
+        'impact:fr' => $faker_fr->paragraphs(
+            2,
+            true
+        ),
+        'branch:fr' => $faker_fr->word,
+        'division:fr' => $faker_fr->word,
+        'education:fr' => $faker_fr->sentence(),
     ];
 });
+
+$factory->state(JobPoster::class, 'published', [
+    'published' => true
+]);
+
+$factory->state(JobPoster::class, 'unpublished', [
+    'published' => false
+]);
+
+$factory->state(JobPoster::class, 'remote', [
+    'remote_work_allowed' => true
+]);
+
+$factory->state(JobPoster::class, 'local', [
+    'remote_work_allowed' => false
+]);
