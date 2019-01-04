ba14da38d9af48baea122f4437cff6fe2039a46f
Unknown <tristan.m.orourke@gmail.com>
2019-01-03 21:26:08 -0500

    Adding text for new assessment types, in progress

---
+++ resources/lang/en/common/lookup/assessment_type.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    "narrative_assessment" => "Narrative assessment",
+    "scenario_interview" => "Interview involving scenario-based questions",
+    "ux_take_home_exam" => "Take-home exam involving preparation of a UX product or plan based on a scenario or client-role play",
+    "ux_group_test" => "Group test to develop a UX approach to a particular scenario",
+    "ux_written_exam" => "Written exam on UX steps, tools and approaches",
+    "absorbing_information_written_exam" => "Written exam involving absorbing information, providing analysis (can be combined with interview for presentation of results, with follow-up Q & A)",
+    "client_relations_behaviour_interview" => "Interview involving behaviour-based questions related to client-relations, end users, etc.",
+    "formal_credential" => "Certificate or formal credential",
+    "overall_interview" => "Overall interview performance",
+    "coding_test" => "Coding test",
+];
