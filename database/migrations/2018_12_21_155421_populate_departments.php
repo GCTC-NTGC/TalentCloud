d590717df64f2d657c269896c37b5ac1c66495ea
Unknown <tristan.m.orourke@gmail.com>
2019-01-04 14:39:56 -0500

    Make dept ids match live site

--- database/migrations/2018_12_21_155421_populate_departments.php
+++ database/migrations/2018_12_21_155421_populate_departments.php
@@ -20,10 +20,10 @@ class PopulateDepartments extends Migration
             ['id' => 4, 'name' => 'enviroment_and_climate_change'],
             ['id' => 5, 'name' => 'employment_and_social_development'],
             ['id' => 6, 'name' => 'global_affairs'],
-            ['id' => 7, 'name' => 'fisheries_and_oceans'],
-            ['id' => 8, 'name' => 'innovation_science'],
-            ['id' => 9, 'name' => 'public_service_and_procurement'],
-            ['id' => 10, 'name' => 'border_services_agency'],
+            ['id' => 7, 'name' => 'border_services_agency'],
+            ['id' => 8, 'name' => 'fisheries_and_oceans'],
+            ['id' => 9, 'name' => 'innovation_science'],
+            ['id' => 10, 'name' => 'public_service_and_procurement'],
         ]);
 
         DB::table('department_translations')->insert([
@@ -39,14 +39,14 @@ class PopulateDepartments extends Migration
             ['id' => 10, 'department_id' => 5, 'locale' => 'fr', 'value' => 'Emploi et Développement social Canada'],
             ['id' => 11, 'department_id' => 6, 'locale' => 'en', 'value' => 'Global Affairs Canada'],
             ['id' => 12, 'department_id' => 6, 'locale' => 'fr', 'value' => 'Affaires mondiales Canada'],
-            ['id' => 13, 'department_id' => 7, 'locale' => 'en', 'value' => 'Fisheries and Oceans Canada'],
-            ['id' => 14, 'department_id' => 7, 'locale' => 'fr', 'value' => 'Pêches et Océans Canada'],
-            ['id' => 15, 'department_id' => 8, 'locale' => 'en', 'value' => 'Innovation, Science and Economic Development Canada'],
-            ['id' => 16, 'department_id' => 8, 'locale' => 'fr', 'value' => 'Innovation, Sciences et Développement économique Canada'],
-            ['id' => 17, 'department_id' => 9, 'locale' => 'en', 'value' => 'Public Services and Procurement Canada'],
-            ['id' => 18, 'department_id' => 9, 'locale' => 'fr', 'value' => 'Services publics et Approvisionnement Canada'],
-            ['id' => 19, 'department_id' => 10, 'locale' => 'en', 'value' => 'Canada Border Services Agency'],
-            ['id' => 20, 'department_id' => 10, 'locale' => 'fr', 'value' => 'Agence des services frontaliers du Canada'],
+            ['id' => 13, 'department_id' => 7, 'locale' => 'en', 'value' => 'Canada Border Services Agency'],
+            ['id' => 14, 'department_id' => 7, 'locale' => 'fr', 'value' => 'Agence des services frontaliers du Canada'],
+            ['id' => 15, 'department_id' => 8, 'locale' => 'en', 'value' => 'Fisheries and Oceans Canada'],
+            ['id' => 16, 'department_id' => 8, 'locale' => 'fr', 'value' => 'Pêches et Océans Canada'],
+            ['id' => 17, 'department_id' => 9, 'locale' => 'en', 'value' => 'Innovation, Science and Economic Development Canada'],
+            ['id' => 18, 'department_id' => 9, 'locale' => 'fr', 'value' => 'Innovation, Sciences et Développement économique Canada'],
+            ['id' => 19, 'department_id' => 10, 'locale' => 'en', 'value' => 'Public Services and Procurement Canada'],
+            ['id' => 20, 'department_id' => 10, 'locale' => 'fr', 'value' => 'Services publics et Approvisionnement Canada'],
         ]);
     }
 
