302057765c802fdcf037b78c82f1d904296e98d9
Josh Beveridge <joshua.w.beveridge@gmail.com>
2019-01-07 09:23:15 -0500

    Screening Plan Fixes

--- resources/lang/en/manager/screening-plan.php
+++ resources/lang/en/manager/screening-plan.php
@@ -28,7 +28,8 @@ return [
             "assessment_selection_label" => "Select an Assessment",
             "assessment_removal_title" => "Removes the assessment above.",
             "assessment_removal_label" => "Remove",
-            "add_assessment_title" => "Add another assessment to this criteria."
+            "add_assessment_title" => "Add another assessment to this criteria.",
+            "narrative_assessment_label" => "Assessment of Narrative Provided via Talent Cloud"
         ]
     ],
     "delete_plan_modal" => [
