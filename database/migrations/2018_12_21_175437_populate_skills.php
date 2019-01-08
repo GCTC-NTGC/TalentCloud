ecef68c04f9c964ba1756ad45180f68c61081bc4
Grant <grant.d.barnes@gmail.com>
2019-01-08 12:27:07 -0500

    fix table name

--- database/migrations/2018_12_21_175437_populate_skills.php
+++ database/migrations/2018_12_21_175437_populate_skills.php
@@ -89,7 +89,7 @@ class PopulateSkills extends Migration
             ['id' => 73, 'skill_type_id' => 2, 'name' => 'strategy_development'],
         ]);
     }
-    
+
     /**
     * Reverse the migrations.
     *
@@ -97,7 +97,7 @@ class PopulateSkills extends Migration
     */
     public function down()
     {
-        DB::table('province_translations')->whereIn('id',
+        DB::table('skills')->whereIn('id',
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
         11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
         21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
