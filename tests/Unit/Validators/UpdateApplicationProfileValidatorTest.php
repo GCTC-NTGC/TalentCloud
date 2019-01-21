fb841505900b1997f387fc6ecb4cc7f2e192054c
Unknown <tristan.m.orourke@gmail.com>
2019-01-21 15:59:33 -0500

    Create test objects in a setUp function for reusability

--- tests/Unit/Validators/UpdateApplicationProfileValidatorTest.php
+++ tests/Unit/Validators/UpdateApplicationProfileValidatorTest.php
@@ -13,72 +13,83 @@ use App\Services\Validation\Requests\UpdateApplicationProfileValidator;
 
 class UpdateApplicationProfileValidatorTest extends TestCase
 {
+
+    /**
+     * Run parent setup and provide reusable factories.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->password = 'Testing123!';
+        $this->badPassword = 'WrongPassword123!';
+        $this->user = factory(User::class)->create([
+            'password' => Hash::make('Testing123!')
+        ]);
+        $this->applicant = factory(Applicant::class)->create(['user_id' => $this->user->id]);
+
+        $this->otherApplicant = factory(Applicant::class)->create();
+    }
+
     public function testUpdatingBasicProfileIsValid()
     {
-        $applicant = factory(Applicant::class)->create();
         $data = [
-            'profile_name' => $applicant->user->name,
-            'profile_email' => $applicant->user->email,
+            'profile_name' => $this->applicant->user->name,
+            'profile_email' => $this->applicant->user->email,
             'twitter_username' => 'Test_person',
             'linkedin_url' => 'www.linkedin.com/in/test-person',
             'tagline' => 'GO FOR IT',
         ];
-        $validator = new UpdateApplicationProfileValidator($applicant);
+        $validator = new UpdateApplicationProfileValidator($this->applicant);
         $this->assertTrue($validator->isValid($data));
     }
 
     public function testNotValidWhenNameIsEmpty()
     {
-        $applicant = factory(Applicant::class)->create();
         $data = [
             'profile_name' => '',
-            'profile_email' => $applicant->user->email,
+            'profile_email' => $this->applicant->user->email,
             'twitter_username' => 'Test_person',
             'linkedin_url' => 'www.linkedin.com/in/test-person',
             'tagline' => 'GO FOR IT',
         ];
-        $validator = new UpdateApplicationProfileValidator($applicant);
+        $validator = new UpdateApplicationProfileValidator($this->applicant);
         $this->assertFalse($validator->isValid($data));
     }
 
     public function testNotValidWhenNEmailIsEmpty()
     {
-        $applicant = factory(Applicant::class)->create();
         $data = [
-            'profile_name' => $applicant->user->name,
+            'profile_name' => $this->applicant->user->name,
             'profile_email' => '',
             'twitter_username' => 'Test_person',
             'linkedin_url' => 'www.linkedin.com/in/test-person',
             'tagline' => 'GO FOR IT',
         ];
-        $validator = new UpdateApplicationProfileValidator($applicant);
+        $validator = new UpdateApplicationProfileValidator($this->applicant);
         $this->assertFalse($validator->isValid($data));
     }
 
     public function testNotValidWhenNEmailSameAsAnotherUser()
     {
-        $applicant1 = factory(Applicant::class)->create();
-        $applicant2 = factory(Applicant::class)->create();
         $data = [
-            'profile_name' => $applicant1->user->name,
-            'profile_email' => $applicant2->user->email,
+            'profile_name' => $this->applicant->user->name,
+            'profile_email' => $this->otherApplicant->user->email,
             'twitter_username' => 'Test_person',
             'linkedin_url' => 'www.linkedin.com/in/test-person',
             'tagline' => 'GO FOR IT',
         ];
-        $validator = new UpdateApplicationProfileValidator($applicant1);
+        $validator = new UpdateApplicationProfileValidator($this->applicant);
         $this->assertFalse($validator->isValid($data));
     }
 
     public function testUpdatePasswordValidWithOldAndConfirm()
     {
-        $user = factory(User::class)->create([
-            'password' => Hash::make('Testing123!')
-        ]);
-        $applicant = factory(Applicant::class)->create(['user_id' => $user->id]);
         $data = [
-            'profile_name' => $applicant->user->name,
-            'profile_email' => $applicant->user->email,
+            'profile_name' => $this->applicant->user->name,
+            'profile_email' => $this->applicant->user->email,
             'old_password' => "Testing123!",
             'new_password' => 'NewPassword123!',
             'new_password_confirmation' => 'NewPassword123!',
@@ -86,79 +97,66 @@ class UpdateApplicationProfileValidatorTest extends TestCase
             'linkedin_url' => 'www.linkedin.com/in/test-person',
             'tagline' => 'GO FOR IT',
         ];
-        $this->be($applicant->user);
-        $validator = new UpdateApplicationProfileValidator($applicant);
+        $this->be($this->applicant->user);
+        $validator = new UpdateApplicationProfileValidator($this->applicant);
         $this->assertTrue($validator->isValid($data));
     }
 
     public function testUpdatePasswordFailsWithoutConfirm()
     {
-        $user = factory(User::class)->create([
-            'password' => Hash::make('Testing123!')
-        ]);
-        $applicant = factory(Applicant::class)->create(['user_id' => $user->id]);
         $data = [
-            'profile_name' => $applicant->user->name,
-            'profile_email' => $applicant->user->email,
-            'old_password' => 'Testing123!',
+            'profile_name' => $this->applicant->user->name,
+            'profile_email' => $this->applicant->user->email,
+            'old_password' => $this->password,
             'new_password' => 'NewPassword123!',
             'twitter_username' => 'Test_person',
             'linkedin_url' => 'www.linkedin.com/in/test-person',
             'tagline' => 'GO FOR IT',
         ];
-        $this->be($applicant->user);
-        $validator = new UpdateApplicationProfileValidator($applicant);
+        $this->be($this->applicant->user);
+        $validator = new UpdateApplicationProfileValidator($this->applicant);
         $this->assertFalse($validator->isValid($data));
     }
 
     public function testUpdatePasswordFailsWithBadConfirm()
     {
-        $user = factory(User::class)->create([
-            'password' => Hash::make('Testing123!')
-        ]);
-        $applicant = factory(Applicant::class)->create(['user_id' => $user->id]);
         $data = [
-            'profile_name' => $applicant->user->name,
-            'profile_email' => $applicant->user->email,
-            'old_password' => 'Testing123!',
+            'profile_name' => $this->applicant->user->name,
+            'profile_email' => $this->applicant->user->email,
+            'old_password' => $this->password,
             'new_password' => 'NewPassword123!',
             'new_password_confirmation' => 'DifferentPassword123!',
             'twitter_username' => 'Test_person',
             'linkedin_url' => 'www.linkedin.com/in/test-person',
             'tagline' => 'GO FOR IT',
         ];
-        $this->be($applicant->user);
-        $validator = new UpdateApplicationProfileValidator($applicant);
+        $this->be($this->applicant->user);
+        $validator = new UpdateApplicationProfileValidator($this->applicant);
         $this->assertFalse($validator->isValid($data));
     }
 
     public function testUpdatePasswordFailsWithIllegalPassword()
     {
-        $user = factory(User::class)->create([
-            'password' => Hash::make('Testing123!')
-        ]);
-        $applicant = factory(Applicant::class)->create(['user_id' => $user->id]);
         $data = [
-            'profile_name' => $applicant->user->name,
-            'profile_email' => $applicant->user->email,
-            'old_password' => 'Testing123!',
+            'profile_name' => $this->applicant->user->name,
+            'profile_email' => $this->applicant->user->email,
+            'old_password' => $this->password,
             'new_password' => 'NewPassword',
             'new_password_confirmation' => 'NewPassword',
             'twitter_username' => 'Test_person',
             'linkedin_url' => 'www.linkedin.com/in/test-person',
             'tagline' => 'GO FOR IT',
         ];
-        $this->be($applicant->user);
-        $validator = new UpdateApplicationProfileValidator($applicant);
+        $this->be($this->applicant->user);
+        $validator = new UpdateApplicationProfileValidator($this->applicant);
         $this->assertFalse($validator->isValid($data));
     }
 
     public function testUpdatePasswordFailsWithBadOldPassword()
     {
-        $applicant = factory(Applicant::class)->create();
         $data = [
-            'profile_name' => $applicant->user->name,
-            'profile_email' => $applicant->user->email,
+            'profile_name' => $this->applicant->user->name,
+            'profile_email' => $this->applicant->user->email,
             'old_password' => 'NotTheRightPassword123!',
             'new_password' => 'NewPassword123!',
             'new_password_confirmation' => 'NewPassword123!',
@@ -166,8 +164,8 @@ class UpdateApplicationProfileValidatorTest extends TestCase
             'linkedin_url' => 'www.linkedin.com/in/test-person',
             'tagline' => 'GO FOR IT',
         ];
-        $this->be($applicant->user);
-        $validator = new UpdateApplicationProfileValidator($applicant);
+        $this->be($this->applicant->user);
+        $validator = new UpdateApplicationProfileValidator($this->applicant);
         $this->assertFalse($validator->isValid($data));
     }
 }
