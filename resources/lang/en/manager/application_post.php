96044701ead62b59d1aa4860f8c205007e4bb0dc
Xander Miller <xander.miller@gmail.com>
2019-01-24 16:34:21 -0500

    Starts adding localizations to manager/applications/#

---
+++ resources/lang/en/manager/application_post.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    /*
+    * --------------------------------------------------------------------------
+    * Manager Applicant Profile View Language Lines
+    * --------------------------------------------------------------------------
+    * controller: Controllers/ApplicantProfileController.php
+    * view: views/manager/applicant_profile.html.twig
+    * route: /en/manager/applicants/1
+    * The following language lines are used in the Manager Applicant Profile page.
+    *
+    */
+
+    'title' => 'Applicant Profile',
+    'header' => [
+        'twitter_title' => 'Visit :name\'s Twitter profile.',
+        'linkedin_title' => 'Visit :name\'s LinkedIn profile.'
+    ],
+    'content' => [
+        'job_poster' => 'Job Poster',
+        'review' => []
+    ]
+];
