2820c7c00b1e0118e7770aaa7cc1f2ffa6a4d80d
Xander Miller <xander.miller@gmail.com>
2019-01-29 11:06:42 -0500

    Adds more localizations strings to manger job create view

--- resources/lang/en/manager/job_create.php
+++ resources/lang/en/manager/job_create.php
@@ -17,6 +17,41 @@ return [
             'key_tasks' => 'Key Tasks',
             'skills_n_competencies' => 'Skills & Competencies',
             'questions' => 'Questions for the Applicant'
+        ],
+        'form' => [
+            'job_details' => 'Job Details meow',
+            'job_title' => 'Job Title meow',
+            'job_title_english' => 'Job Title (English) meow',
+            'job_title_french' => 'Job Title (Français) meow',
+            'salary_range' => 'Salary Range meow',
+            'minimum_value' => 'Minimum Value meow',
+            'maximum_value' => 'Maximum Value meow',
+            'classifications' => 'Classifications meow',
+            'occupational_group' => 'Occupational Group meow',
+            'noc_code' => 'NOC Code meow',
+            'security_clearance' => 'Security Clearance meow',
+            'language_requirement' => 'Language Requirement meow',
+            'location' => 'Location meow',
+            'remote_work_allowed' => 'Remote Work Allowed meow',
+            'city' => 'City meow',
+            'province' => 'Province meow',
+            'timetable' => 'Timetable meow',
+            'accepting_applications_from' => 'Accepting Applications From meow',
+            'opening_time' => 'Opening Time, UTC meow',
+            'accepting_applications_to' => 'Accepting Applications To meow',
+            'closing_time' => 'Closing Time, UTC meow',
+            'position_start' => 'Position Start Date meow',
+            'position_duration' => 'Position Duration (Months) meow',
+            'departmental_information' => 'Departmental Information meow',
+            'department' => 'Department meow',
+            'branch_english' => 'Branch (English) meow',
+            'branch_french' => 'Branch (Français) meow',
+            'division_english' => 'Division (English) meow',
+            'division_french' => 'Division (Français)',
+
+
+
+
         ]
     ]
 ];
