6e15fdce845d693cccf82df97f7467ae958320eb
Chris Wiseman <wiseman.christopher@gmail.com>
2019-02-05 09:51:15 -0400

    Rename Skill property from name to key

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -9,53 +9,56 @@ namespace App\Models;
 
 use Illuminate\Support\Facades\Lang;
 use \Backpack\CRUD\CrudTrait;
+use Backpack\CRUD\ModelTraits\SpatieTranslatable\HasTranslations;
 
 /**
  * Class Skill
  *
  * @property int $id
+ * @property string $key
  * @property string $name
- * @property int $skill_type_id
- *
- * Accessors:
- * @property string $skill
  * @property string $description
+ * @property int $skill_type_id
  *
  * @property \App\Models\Lookup\SkillType $skill_type
  * @property \Illuminate\Database\Eloquent\Collection $skill_declarations
  */
-class Skill extends BaseModel {
-
+class Skill extends BaseModel
+{
     use CrudTrait;
+    use HasTranslations;
 
+    /**
+     * @var $casts []string
+     * */
     protected $casts = [
         'name' => 'string',
         'skill_type_id' => 'int'
     ];
+    /**
+     * @var $fillable []string
+     * */
     protected $fillable = [
+        'key',
         'name',
+        'description',
         'skill_type_id'
     ];
-
     /**
-     * The accessors to append to the model's array form.
-     */
-    protected $appends = ['skill', 'description'];
+     * @var $translatable []string
+     * */
+    public $translatable = [
+        'name',
+        'description',
+    ];
 
-    public function skill_type() {
+    public function skill_type() // phpcs:ignore
+    {
         return $this->belongsTo(\App\Models\Lookup\SkillType::class);
     }
 
-    public function skill_declarations() {
+    public function skill_declarations() // phpcs:ignore
+    {
         return $this->hasMany(\App\Models\SkillDeclaration::class);
-    }
-
-    // Accessors
-    public function getSkillAttribute() {
-        return Lang::get('common/skills.skills')[$this->name]['name'];
-    }
-
-    public function getDescriptionAttribute() {
-        return Lang::get('common/skills.skills')[$this->name]['description'];
     }
 }
