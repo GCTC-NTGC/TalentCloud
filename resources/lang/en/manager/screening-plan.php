5cae214be2bda97e36521aef5743e6d14eb75a6d
Gray <obyrne.gray@gmail.com>
2019-02-13 09:28:14 -0500

    Changing the term "screening plan" to "assessment plan" to more accurately reflect it's scope

--- resources/lang/en/manager/screening-plan.php
+++ resources/lang/en/manager/screening-plan.php
@@ -8,16 +8,16 @@ return [
     * Controller: Controllers\ScreeningPlanController.php
     * View: views/manager/screening-plan.html.twig
     */
-    "title" => "Screening Plan Builder",
-    "generate_plan_for" => "Generate a screening plan for:",
-    "narrative_warning" => "Please note that all screening plans will include an assessment of the narrative evidence provided by the applicant via Talent Cloud.",
+    "title" => "Assessment Plan Builder",
+    "generate_plan_for" => "Generate an assessment plan for:",
+    "narrative_warning" => "Please note that all assessment plans will include an review of the narrative evidence provided by the applicant via Talent Cloud.",
     "essential_skills" => "Essential Skills",
     "essential_skills_empty" => "No Essential Criteria Exist",
     "asset_skills" => "Asset Skills",
     "asset_skills_empty" => "No Asset Criteria Exist",
-    "build_new_plan" => "Build New Screening Plan",
-    "my_screening_plans" => "My Screening Plans",
-    "screening_plan_here" => "The screening plan that you generate using the button above will appear here.",
+    "build_new_plan" => "Build New Assessment Plan",
+    "my_screening_plans" => "My Assessment Plans",
+    "screening_plan_here" => "The assessment plan that you generate using the button above will appear here.",
     "skill_builder" =>[
         "description" => "Description",
         "assessment_types" => "Assessment Types",
@@ -44,14 +44,14 @@ return [
             "assessment_types" => "Assessment Types",
             "narrative_assessment" => "Assessment of Narrative Provided via Talent Cloud"
         ],
-        "my_screening_plans" => "My Screening Plans",
+        "my_screening_plans" => "My Assessment Plans",
         "assessment_types" => "Assessment Types",
         "narrative_assessment" => "Assessment of Narrative Provided via Talent Cloud"
     ],
     "delete_plan_modal" => [
-        "title" => "Delete this Screening Plan?",
+        "title" => "Delete this Assessment Plan?",
         "content" => [
-            "Are you sure you want to permanently delete this Screening Plan from this Job?",
+            "Are you sure you want to permanently delete this Assessment Plan from this Job?",
         ],
         "action_01" => "Cancel",
         "action_02" => "Delete",
