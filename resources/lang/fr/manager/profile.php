04d401467e11ceee6f6ffe6171d81e7aa03a087e
Gray <obyrne.gray@gmail.com>
2019-02-13 08:19:18 -0500

    Translated manager\profile

--- resources/lang/fr/manager/profile.php
+++ resources/lang/fr/manager/profile.php
@@ -2,96 +2,96 @@
 return [
     /*
     * --------------------------------------------------------------------------
-    * Manager Portal Profile Edit
+    * FR Manager Portal Profile Edit
     * --------------------------------------------------------------------------
     * Route: /manager/profile/#/edit
     * Controller: Controllers\ManagerProfileController.php
     * View: views/manager/profile.html.twig
     */
 
-    'title' => 'My Profile',
+    'title' => 'Mon profile',
     'profile_header' => [
-        'twitter' => 'Visit :name\'s Twitter profile.',
-        'linkedin' => 'Visit :name\'s LinkedIn profile.'
+        'twitter' => 'Voir le profile twitter de :name.',
+        'linkedin' => 'Voir le profile LinkedIn de :name.'
     ],
     'layout' => [
         'sidebar' => [
-            'profile_sections' => 'Profile Sections',
-            'about_me' => 'About Me',
-            'leadership_style' => 'My Leadership Style',
-            'our_work_environment' => 'Our Work Environment',
-            'our_team_culture' => 'Our Team Culture',
-            'save_all_sections' => 'Save All Sections'
+            'profile_sections' => 'Sections du profile',
+            'about_me' => 'À propos de moi',
+            'leadership_style' => 'Mon style de leadership',
+            'our_work_environment' => 'Notre environnement de travail',
+            'our_team_culture' => 'Notre culture d\'équipe',
+            'save_all_sections' => 'Enregistrer toutes les sections'
         ],
         'about' => [
-            'about_me' => 'About Me',
-            'my_name' => 'My Name',
-            'position' => 'Position',
-            'position_english' => 'Position (English)',
-            'position_french' => 'Position (Français)',
-            'departmental_information' => 'Departmental Information',
-            'department' => 'Department',
-            'branch_english' => 'Branch (English)',
-            'branch_french' => 'Branch (Français)',
+            'about_me' => 'À propos de moi',
+            'my_name' => 'Mon prénom et nom',
+            'position' => 'Mon poste',
+            'position_english' => 'Poste (English)',
+            'position_french' => 'Poste (Français)',
+            'departmental_information' => 'Renseignements ministériels',
+            'department' => 'Ministère',
+            'branch_english' => 'Branche (English)',
+            'branch_french' => 'Branche (Français)',
             'division_english' => 'Division (English)',
             'division_french' => 'Division (Français)',
-            'personal_information' => 'Personal Information',
-            'education_english' => 'Education (English)',
-            'education_french' => 'Education (Français)',
-            'years_of_experience' => 'Years of Experience',
-            'career_journey_english' => 'My Career Journey So Far (English)',
-            'career_journey_french' => 'My Career Journey So Far (Français)',
-            'learning_path_english' => 'My Learning Path (English)',
-            'learning_path_french' => 'My Learning Path (Français)',
-            'biography_english' => 'A Bit About Me Outside Work (English)',
-            'biography_french' => 'A Bit About Me Outside Work (Français)',
-            'social_media' => 'Social Media',
-            'twitter_username' => 'Twitter Username',
-            'linkedin_url' => 'LinkedIn Profile URL',
-            'save_about_me' => 'Save About Me'
+            'personal_information' => 'Renseignements personnels',
+            'education_english' => 'Éducation (English)',
+            'education_french' => 'Éducation (Français)',
+            'years_of_experience' => 'Années d\'expérience',
+            'career_journey_english' => 'Mon parcours de carrière jusqu\'à présent (English)',
+            'career_journey_french' => 'Mon parcours de carrière jusqu\'à présent (Français)',
+            'learning_path_english' => 'Mon parcours d\'apprentissage (English)',
+            'learning_path_french' => 'Mon parcours d\'apprentissage (Français)',
+            'biography_english' => 'Un peu à propos de moi en dehors du travail (English)',
+            'biography_french' => 'Un peu à propos de moi en dehors du travail (Français)',
+            'social_media' => 'Médias sociaux',
+            'twitter_username' => 'Mon profil Twitter',
+            'linkedin_url' => 'Mon profil LinkedIn',
+            'save_about_me' => 'Enregistrer À propos de moi'
 
         ],
         'leadership' => [
-            'my_leadership_style' => "My Leadership Style",
-            'my_style_values' => "My Leadership Style and Management Values",
-            'answer_english' => "Answer (English)",
-            'answer_french' => "Answer (Français)",
-            'expect_from_employees' => "What I Expect from My Employees",
-            'approach_learning_development' => "My Approach to Employee Learning and Developmment",
-            'save_leadership_style' => "Save My Leadership Style"
+            'my_leadership_style' => "Mon style de leadership",
+            'my_style_values' => "Mon style de leadership et mes valeurs de gestion",
+            'answer_english' => "Réponse (English)",
+            'answer_french' => "Réponse (Français)",
+            'expect_from_employees' => "Ce que j'attends de mes employés",
+            'approach_learning_development' => "Mon approche à l'apprentissage et au développement de mes employés",
+            'save_leadership_style' => "Enregistrer Mon style de leadership"
         ],
         'environment' => [
-            'work_environment' => "Our Work Environment",
-            'things_to_know' => "Things to Know",
-            'answer_english' => "Answer (English)",
-            'answer_french' => "Answer (Français)",
-            'telework_frequency' => "How Often is Telework Allowed?",
-            'flexible_hour_frequency' => "How Often Are Flexible Hours Allowed?",
-            'environment_photos' => "Work Environment Photos",
-            'upload_image' => "Upload an Image",
-            'alt_text_english' => "Image Alt Text (English)",
-            'alt_text_french' => "Image Alt Text (Français)",
-            'save_work_environment' => "Save Our Work Environment",
+            'work_environment' => "Notre environnement de travail",
+            'things_to_know' => "Ce qu'il faut savoir",
+            'answer_english' => "Réponse (English)",
+            'answer_french' => "Réponse (Français)",
+            'telework_frequency' => "À quelle fréquence le télétravail est-il autorisé?",
+            'flexible_hour_frequency' => "À quelle fréquence les heures flexibles sont-elles autorisées?",
+            'environment_photos' => "Photos de l'environnement de travail",
+            'upload_image' => "Télécharger un photo",
+            'alt_text_english' => "Texte alternatif de l'image (English)",
+            'alt_text_french' => "Texte alternatif de l'image (Français)",
+            'save_work_environment' => "Enregistrer Notre environnement de travail",
             'frequency' => [
-                'never' => 'Never',
-                'rarely' => 'Occasionally',
-                'sometimes' => 'Sometimes',
-                'often' => 'Frequently',
-                'always' => 'Always',
+              'never' => 'Jamais',
+              'rarely' => 'Rarement',
+              'sometimes' => 'Parfois',
+              'often' => 'Habituellement',
+              'always' => 'Presque toujours',
             ]
 
         ],
         'culture' => [
-            'team_culture' => "Our Team Culture",
-            'primary_information' => "Primary Information",
-            'team_size' => "Team Size",
-            'gc_directory_link' => "GC Directory Link",
-            'operating_context' => "Our Operating Context",
-            'answer_english' => "Answer (English)",
-            'answer_french' => "Answer (Français)",
-            'what_we_value' => "What We Value",
-            'how_we_work' => "How We Work",
-            'save_team_culture' => "Save Our Team Culture"
+            'team_culture' => "Notre culture d'équipe",
+            'primary_information' => "Informations primaires",
+            'team_size' => "Taille de l'équipe",
+            'gc_directory_link' => "Lien GCannuaire",
+            'operating_context' => "Notre contexte opérationnel",
+            'answer_english' => "Réponse (English)",
+            'answer_french' => "Réponse (Français)",
+            'what_we_value' => "Nos valeurs",
+            'how_we_work' => "Notre mode de fonctionnement",
+            'save_team_culture' => "Enregistrer Notre culture d'équipe"
         ]
 
     ]
