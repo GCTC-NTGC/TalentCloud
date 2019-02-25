24c368c5bb73957248b54de9391a228d9155d850
Chris Wiseman <wiseman.christopher@gmail.com>
2019-02-25 11:39:36 -0400

    Refactor alert js to handle multiple actions

--- resources/assets/js/alerts.js
+++ resources/assets/js/alerts.js
@@ -1,22 +1,45 @@
 const Swal = require('sweetalert2');
 
 // Manager Index ==========================================
-function handleReview(e) {
-    // Prevent the anchor tag from navigating
+function handleClick(e) {
     e.preventDefault();
-    // Create a 'queue' of alerts, which depend on the Axios
-    // promise resolving.
+    const action = this.getAttribute('data-action');
+
+    let alertConfig = {
+        delete: {
+            title: 'Delete this draft Job Poster?',
+            confirmButtonText: 'Delete',
+            success: {
+                title: 'Deleted',
+                text: 'The draft has been successfully removed.'
+            }
+        },
+        review: {
+            title: 'Send this Job Poster for Review?',
+            confirmButtonText: 'Send to Talent Cloud',
+            success: {
+                title: 'Sent!',
+                text: 'Your Job Poster has been sent to Talent Cloud for review.'
+            }
+        }
+    };
+
+    alertConfig = alertConfig[action];
+
     Swal.queue([{
-        title: 'Send this Job Poster for Review?',
+        title: alertConfig.title,
         text: 'This action cannot be undone.',
         type: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#0A6CBC',
         cancelButtonColor: '#F94D4D',
-        confirmButtonText: 'Send to Talent Cloud',
+        confirmButtonText: alertConfig.confirmButtonText,
         showLoaderOnConfirm: true,
         preConfirm: () => {
-            return axios.post(this.getAttribute('href'))
+            return axios({
+                    method: action == 'delete' ? action : 'post',
+                    url: this.getAttribute('href')
+                })
                 .then(response => {
                     const jobId = this.getAttribute('data-jobid');
                     const jobMarkup = indexWrapper.querySelector(`div[data-jobid="${jobId}"]`);
@@ -24,8 +47,8 @@ function handleReview(e) {
                 })
                 .then(() => {
                     Swal.insertQueueStep({
-                        title: 'Sent!',
-                        text: 'Your Job Poster has been sent to Talent Cloud for review.',
+                        title: alertConfig.success.title,
+                        text: alertConfig.success.text,
                         type: 'success'
                     });
                 })
@@ -42,10 +65,13 @@ function handleReview(e) {
 
 const indexWrapper = document.querySelector('.manager-poster-index');
 // Double check we're on the manager index page, grab all the
-// 'Send for Review' buttons, and register the click event handler
+// 'Send for Review' and 'Delete' buttons, and register the click event handler
 // defined above.
 if (typeof (indexWrapper) != 'undefined' && indexWrapper != null) {
     const reviewButtons = Array.from(indexWrapper.querySelectorAll('a[data-action="review"]'));
-    reviewButtons.forEach(button => button.addEventListener('click', handleReview));
+    reviewButtons.forEach(button => button.addEventListener('click', handleClick));
+
+    const deleteButtons = Array.from(indexWrapper.querySelectorAll('button[data-action="delete"]'));
+    deleteButtons.forEach(button => button.addEventListener('click', handleClick));
 }
 // End Manager Index ======================================
