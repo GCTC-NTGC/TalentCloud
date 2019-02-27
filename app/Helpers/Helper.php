b485bc7eefe01148ac6956d26ffa470eb977c18b
Xander Miller <xander.miller@gmail.com>
2019-02-27 14:03:09 -0500

    first commit of new pull request

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -91,3 +91,44 @@ if (!function_exists('humanizeDateDiff')) {
         return Lang::choice($key, $count);
     }
 }
+
+if (!function_exists('humanizeLastDay')) {
+    /**
+     * Returns the date of the last full day a person has before a deadline time.
+     *
+     * @param Date $datetime DateTime object to measure.
+     * @param Date $origin   DateTime object to use as a reference.
+     *
+     * @return string
+     */
+    function humanizeLastDay(Date $datetime) : string
+    {
+        if (!isset($origin)) {
+            $origin = new Date();
+        }
+        $interval = $datetime->diff($origin);
+
+        $d = $interval->d;
+        $h = $interval->h;
+        $m = $interval->i;
+        $s = $interval->s;
+
+        if ($d > 0) {
+            $unit = 'day';
+            $count = $d;
+        } elseif ($h > 0) {
+            $unit = 'hour';
+            $count = $h;
+        } elseif ($m > 0) {
+            $unit = 'minute';
+            $count = $m;
+        } else {
+            $unit = 'second';
+            $count = $s;
+        }
+
+        $key = "common/time.$unit";
+
+        return Lang::choice($key, $count);
+    }
+}
