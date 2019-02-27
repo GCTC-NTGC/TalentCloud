cd0312a1d373adb92f7e9c0fca889f44722b7219
Grant <grant.d.barnes@gmail.com>
2019-02-27 01:09:25 -0500

    Migration for case insensitive Postgres extension and changing user emails to a citext field

---
+++ database/migrations/2019_02_26_210634_case_insensitive_emails.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CaseInsensitiveEmails extends Migration
+{
+    /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+    public function up()
+    {
+        DB::unprepared(
+            'CREATE EXTENSION IF NOT EXISTS citext',
+            'ALTER TABLE users ALTER COLUMN email TYPE citext'
+        );
+    }
+
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        DB::unprepared(
+            'DROP EXTENSION citext',
+            'ALTER TABLE users ALTER COLUMN email TYPE character varying(191)'
+        );
+    }
+}
