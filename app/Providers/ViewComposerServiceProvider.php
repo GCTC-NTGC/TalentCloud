9b64ec600412f2a18aeccb014ca839606aaa1356
Chris Wiseman <wiseman.christopher@gmail.com>
2019-03-22 16:21:00 -0300

    Register reference composer as a singleton

--- app/Providers/ViewComposerServiceProvider.php
+++ app/Providers/ViewComposerServiceProvider.php
@@ -92,6 +92,7 @@ class ViewComposerServiceProvider extends ServiceProvider
     {
         $this->app->singleton(\App\Http\ViewComposers\CourseComposer::class);
         $this->app->singleton(\App\Http\ViewComposers\DegreeComposer::class);
+        $this->app->singleton(\App\Http\ViewComposers\ReferenceComposer::class);
         $this->app->singleton(\App\Http\ViewComposers\SkillComposer::class);
     }
 }
