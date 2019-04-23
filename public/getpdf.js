f249d3e8d8b3180602e90551cd56d1a6c38a6775
Grant <grant.d.barnes@gmail.com>
2019-04-23 13:11:09 -0400

    comments / remove bookmarklet.js

--- public/getpdf.js
+++ public/getpdf.js
@@ -1,6 +1,9 @@
-window.onload = function () {
+// run on Review Applicants page to export all applications to PDF
+// add bookmark to Chrome with this as the URL:
 
-    focus();
+// javascript:$(document).ready(function() { var links = document.querySelectorAll("div.box.lg-2of11.applicant-links > a:nth-child(1)"); function get_hrefs(links){ links = Array.prototype.slice.call(links); return links.map(function(elem){ return elem.getAttribute("href"); });; } get_hrefs(links); for (var i = 0; i < links.length; i++) { var newWindow = window.open(links[i], '_blank'); var jsCode = newWindow.document.createElement('script'); jsCode.setAttribute('src', links[i].origin + '/getpdf.js'); newWindow.document.body.appendChild(jsCode); console.log(links[i].origin); }; });
+
+window.onload = function () {
 
     $(document).ready(function() {
 
