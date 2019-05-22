00ed368035738db207dc0f2cfeda4d58267a0e93
Xander Miller <xander.miller@gmail.com>
2019-05-22 16:50:55 -0400

    Adds a story for form input component

---
+++ resources/assets/js/stories/Input.stories.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import Input from "../components/Input";
+
+const stories = storiesOf("Input", module)
+  .addDecorator(withInfo)
+  .addDecorator(withKnobs);
+
+stories.add(
+  "Text Input",
+  () => (
+    <Input
+      htmlId={text('ID', 'sample-input')}
+      formName={text('Name', "What a name")}
+      label={text("Label", "Label")}
+      required={boolean("Required", "false")}
+      placeholder={text("Placeholder", "Write what you will")}
+      type={text("type","text")}
+      minLength={number("Minimum Length", "0")}
+      maxLength={number("Maximum Length", "30")}
+      value={text("Default Value", "Hello World")}
+      errorText={text("Error Text", "")}
+      onChange={action('Confirm clicked')}
+    />
+  ),
+  { info: { inline: true } },
+);
