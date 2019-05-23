4de9ab00ad664ab2d5c4aac2228b912eb4e9654b
Xander Miller <xander.miller@gmail.com>
2019-05-23 16:57:10 -0400

    adds snapshot test for input component

--- resources/assets/js/stories/Input.stories.tsx
+++ resources/assets/js/stories/Input.stories.tsx
@@ -28,3 +28,23 @@ stories.add(
   ),
   { info: { inline: true } },
 );
+
+stories.add(
+  "Email Input",
+  () => (
+    <Input
+      htmlId={text('ID', 'sample-email-input')}
+      formName={text('Name', "What a name")}
+      label={text("Label", "Label")}
+      required={boolean("Required", "false")}
+      placeholder={text("Placeholder", "Write what you will")}
+      type={text("type","email")}
+      minLength={number("Minimum Length", "0")}
+      maxLength={number("Maximum Length", "30")}
+      value={text("Default Value", "hello@world.test")}
+      errorText={text("Error Text", "")}
+      onChange={action('Confirm clicked')}
+    />
+  ),
+  { info: { inline: true } },
+);
