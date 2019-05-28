e4aaca17d214678d735233298fdb0276dcf667a2
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-05-28 09:39:12 -0400

    Refactored WordCounter Class component into FuntionComponent

--- resources/assets/js/components/WordCounter/WordCounter.tsx
+++ resources/assets/js/components/WordCounter/WordCounter.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ProgressRing from '../ProgressRing';
+import React, { useState, useEffect } from "react";
+import ProgressRing from "../ProgressRing";
 
-type Message = {
-  max: number,
+/*
+  Question: Should this component have the option to add a textarea component (or input element)?
+  example:
+    prop: textAreaComponent: React.ReactElement
+    ...
+    return (
+      {textAreaComponent && textAreaComponent}
+    )
+*/
+
+interface Message {
+  max: number;
   message: string;
 }
 
@@ -13,94 +22,51 @@ interface WordCounterProps {
   minWords: number;
   warnings: Message[];
   wordLimit: number;
+  numberOfWords: number;
 }
 
-interface WordCounterState {
-  currentNumberOfWords: number;
-  median: number;
-  strokeColor: string;
-  message: string;
-}
+const WordCounter: React.FunctionComponent<WordCounterProps> = ({
+  elementId,
+  numberOfWords,
+  minWords,
+  maxWords,
+  wordLimit,
+  warnings,
+}): React.ReactElement => {
+  const [currentNumberOfWords, setCurrentNumberOfWords] = useState(
+    numberOfWords,
+  );
+  const [message, setMessage] = useState("");
+  const [strokeColor, setStrokeColor] = useState("");
 
-class WordCounter extends React.Component<WordCounterProps, WordCounterState> {
-  public constructor(props: WordCounterProps) {
-    super(props);
-    // const { elementId } = this.props;
-    // const element = document.getElementById(elementId) as HTMLInputElement;
+  const getNumberOfWords = (innerText: string): number => {
+    return innerText.replace(/[ ]{2,}/gi, " ").split(" ").length - 1;
+  };
 
-    const { minWords, maxWords } = this.props;
+  // updates the message
+  const updateMessage = (): void => {
+    let index = 0;
+    const warning = warnings.find(
+      ({ max }, i): boolean => {
+        index = i;
+        return (
+          warnings[i + 1] &&
+          currentNumberOfWords >= max &&
+          currentNumberOfWords < warnings[i + 1].max
+        );
+      },
+    );
 
-    this.state = {
-      currentNumberOfWords: 0,
-      median: minWords + ((maxWords - minWords) / 2),
-      strokeColor: '',
-      message: ''
-    };
-  }
+    // set the message
+    setMessage(warning ? warning.message : warnings[index].message);
+  };
 
-  public componentDidMount() {
-    const { elementId } = this.props;
-    const element = document.getElementById(elementId) as HTMLInputElement;
-    this.updateColor(this.getNumberOfWords(element.value));
-    this.updateMessage(this.getNumberOfWords(element.value));
-    this.updateCurrentNumberOfWords(this.getNumberOfWords(element.value));
-    this.getCurrentNumberOfWords();
-  }
-
-  // This method calculates the total number of words (and the color of the progressRing) within the input element and updates the state on any input changes
-  protected getCurrentNumberOfWords(): void {
-    const { elementId } = this.props;
-    const element = document.getElementById(elementId) as HTMLInputElement;
-    const { wordLimit } = this.props;
-    let previousNumOfChars = 0;
-
-    if (element !== null) {
-      element.addEventListener('input', e => {
-        const target = e.target as HTMLInputElement;
-        const currentNumberOfWords = this.getNumberOfWords(target.value);
-        const totalChars = target.value.length;
-        // update total words state
-        this.updateCurrentNumberOfWords(currentNumberOfWords);
-        // update the color
-        this.updateColor(currentNumberOfWords);
-        // update message
-        this.updateMessage(currentNumberOfWords);
-
-        // set hard cap
-        if( previousNumOfChars > totalChars && currentNumberOfWords < wordLimit) {
-          // how can you set textarea to unlimited?
-          console.log(previousNumOfChars);
-          console.log(totalChars);
-          target.maxLength = 10000000;
-        } else {
-          if(currentNumberOfWords >= wordLimit) {
-
-            target.maxLength = totalChars;
-
-            previousNumOfChars = totalChars;
-          }
-        }
-      });
-    }
-  }
-
-  // This method takes a string and returns the total number of words.
-  protected getNumberOfWords(innerText: string): number {
-    return innerText.replace(/[ ]{2,}/gi, ' ').split(' ').length - 1;
-  }
-
-  // update the totalWord count
-  protected updateCurrentNumberOfWords(currentNumberOfWords: number): void {
-    this.setState({ currentNumberOfWords });
-  }
-
-  protected updateColor(currentNumberOfWords: number): void {
-    const { maxWords, minWords } = this.props;
-    const { median } = this.state;
+  const updateColor = (): void => {
+    const median = minWords + (maxWords - minWords) / 2;
     let hue = currentNumberOfWords;
     // the median between the minimum and maximum numbers
     const percentage = currentNumberOfWords / median;
-    const difference = (120 * percentage) - 120;
+    const difference = 120 * percentage - 120;
 
     /*
       https://mothereffinghsl.com/
@@ -111,104 +77,99 @@ class WordCounter extends React.Component<WordCounterProps, WordCounterState> {
       Red: 0 hue
     */
 
-
-    if(currentNumberOfWords <= minWords) {
+    if (currentNumberOfWords <= minWords) {
       hue = 120 * percentage;
-
-    } else if (currentNumberOfWords > minWords && currentNumberOfWords <= maxWords) {
-
-        if(currentNumberOfWords <= median) {
-          hue = 120 * percentage;
-        } else {
-          hue = 120 - difference;
-        }
-
-    } else if(currentNumberOfWords > maxWords) {
+    } else if (
+      currentNumberOfWords > minWords &&
+      currentNumberOfWords <= maxWords
+    ) {
+      if (currentNumberOfWords <= median) {
+        hue = 120 * percentage;
+      } else {
         hue = 120 - difference;
+      }
+    } else if (currentNumberOfWords > maxWords) {
+      hue = 120 - difference;
 
-        if(hue <= 0) {
-          hue = 0;
-        }
+      if (hue <= 0) {
+        hue = 0;
+      }
     }
 
     // set the stroke color
-    this.setState({
-      strokeColor: `hsl(${hue}, 80%, 50%)`,
-    });
-  }
+    setStrokeColor(`hsl(${hue}, 80%, 50%)`);
+  };
 
-  // updates the message
-  protected updateMessage = (currentNumberOfWords: number) => {
-    const { warnings } = this.props;
-    let index: number = 0;
-    const warning = warnings.find(({max}, i) => {
-      index = i;
-      return warnings[i+1] && currentNumberOfWords >= max && currentNumberOfWords < warnings[i+1].max;
-    });
+  // This method calculates the total number of words (and the color of the progressRing) within the input element and updates the state on any input changes
+  const getCurrentNumberOfWords = (element: HTMLTextAreaElement): void => {
+    let previousNumOfChars = 0;
 
-    this.setState({ message: warning ? warning.message : warnings[index].message })
-  }
+    if (element !== null) {
+      element.addEventListener(
+        "input",
+        (e): void => {
+          const target = e.target as HTMLTextAreaElement;
+          setCurrentNumberOfWords(getNumberOfWords(target.value));
+          const totalChars = target.value.length;
+          // update total words state
+          this.updateCurrentNumberOfWords(currentNumberOfWords);
+          // update the color
+          updateColor();
+          // update message
+          updateMessage();
 
-  public render(): React.ReactElement {
-    const { currentNumberOfWords, strokeColor, message, median } = this.state;
-    const { maxWords, minWords } = this.props;
-    return (
-      <div
-        className="word-counter"
-        role="progressbar"
-        aria-valuenow={currentNumberOfWords}
-        aria-valuemin={minWords}
-        aria-valuemax={maxWords}
-      >
-        <ProgressRing
-          radius={25}
-          stroke={5}
-          progress={currentNumberOfWords}
-          strokeColor={strokeColor}
-          max={minWords}
-        />
-        <span>{currentNumberOfWords} {message}</span>
-      </div>
-    );
-  }
-}
+          // set hard cap
+          if (
+            previousNumOfChars > totalChars &&
+            currentNumberOfWords < wordLimit
+          ) {
+            // how can you set textarea to unlimited?
+            target.maxLength = 10000000;
+          } else if (currentNumberOfWords >= wordLimit) {
+            target.maxLength = totalChars;
 
-// Find all skills textarea elements
-if (document.querySelectorAll('div[data-word-counter-id]')) {
-  const elements = document.querySelectorAll('div[data-word-counter-id]');
-
-  const warnings = [
-    { max: 0, message: 'This is too short, try including examples or lessons learned.'},
-    { max: 10, message: 'Seems short, try adding an example or two.'},
-    { max: 20, message: 'Looks good.'},
-    { max: 80, message: 'This is starting to get too long.'},
-    { max: 100, message: 'This looks really long, try summarizing some text.'},
-    { max: 130, message: 'This is way too long, try deleting irrelevant content, or see an example.'},
-  ];
-
-  Array.prototype.slice.call(elements).forEach(container => {
-    if (container != null && container.hasAttribute('data-word-counter-id')) {
-      const elementId = JSON.parse(container.getAttribute(
-        'data-word-counter-id',
-      ) as string);
-      const maxWords = JSON.parse(container.getAttribute(
-        'data-max-words',
-      ) as string);
-      const minWords = JSON.parse(container.getAttribute(
-        'data-min-words',
-      ) as string);
-      ReactDOM.render(
-        <WordCounter
-          elementId={elementId}
-          maxWords={+maxWords}
-          minWords={+minWords}
-          warnings={warnings}
-          wordLimit={60}
-        />,
-        container,
+            previousNumOfChars = totalChars;
+          }
+        },
       );
     }
-  });
-}
+  };
+
+  /*
+    useEffect() is a combination of componentDidMount, componentDidUpdate, and componentWillUnmount
+  */
+  useEffect(
+    (): void => {
+      const element: HTMLTextAreaElement = document.getElementById(
+        elementId,
+      ) as HTMLTextAreaElement;
+      setCurrentNumberOfWords(getNumberOfWords(element.value));
+      updateColor();
+      updateMessage();
+      getCurrentNumberOfWords(element);
+    },
+  );
+
+  return (
+    <div
+      className="word-counter"
+      role="progressbar"
+      aria-valuenow={currentNumberOfWords}
+      aria-valuemin={minWords}
+      aria-valuemax={maxWords}
+    >
+      <ProgressRing
+        radius={25}
+        stroke={5}
+        progress={currentNumberOfWords}
+        strokeColor={strokeColor}
+        max={minWords}
+      />
+      <span style={currentNumberOfWords === 0 ? { color: "#80808085" } : {}}>
+        {message}
+      </span>
+    </div>
+  );
+};
 
 export default WordCounter;
--- resources/assets/js/stories/WordCounter/WordCounter.stories.tsx
+++ resources/assets/js/stories/WordCounter/WordCounter.stories.tsx
@@ -1,29 +1,47 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { withKnobs, number } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
-import WordCounter from '../../components/WordCounter/WordCounter';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { withKnobs, number } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import WordCounter from "../../components/WordCounter/WordCounter";
 
 const warnings = [
-  { max: 0, message: 'This is too short, try including examples or lessons learned.'},
-  { max: 10, message: 'Seems short, try adding an example or two.'},
-  { max: 20, message: 'Looks good.'},
-  { max: 80, message: 'This is starting to get too long.'},
-  { max: 100, message: 'This looks really long, try summarizing some text.'},
-  { max: 130, message: 'This is way too long, try deleting irrelevant content, or see an example.'},
+  {
+    max: 0,
+    message: "Start typing your answer above.",
+  },
+  {
+    max: 1,
+    message: "This is too short, try including examples or lessons learned.",
+  },
+  { max: 10, message: "Seems short, try adding an example or two." },
+  { max: 20, message: "Looks good." },
+  { max: 80, message: "This is starting to get too long." },
+  { max: 100, message: "This looks really long, try summarizing some text." },
+  {
+    max: 130,
+    message:
+      "This is way too long, try deleting irrelevant content, or see an example.",
+  },
 ];
 
-const stories = storiesOf('Word Counter', module)
+const stories = storiesOf("Word Counter", module)
   .addDecorator(withInfo)
   .addDecorator(withKnobs);
 
 stories.add(
-  'Word Counter',
+  "Word Counter",
   (): React.ReactElement => (
     <div>
-      <textarea id="word-counter" cols={100} rows={10}></textarea>
-      <WordCounter elementId={"word-counter"} maxWords={number('Max', 20)} minWords={number('Min', 80)} warnings={warnings} wordLimit={number('Hard Limit', 200)} />
+      <textarea id="word-counter" cols={100} rows={10} />
+      <WordCounter
+        elementId="word-counter"
+        maxWords={number("Max", 20)}
+        minWords={number("Min", 80)}
+        wordLimit={number("Hard Limit", 200)}
+        numberOfWords={number("Total", 2)}
+        warnings={warnings}
+      />
     </div>
   ),
   { info: { inline: true } },
