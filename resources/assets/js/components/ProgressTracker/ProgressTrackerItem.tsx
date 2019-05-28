ba99e0c0ca2648fd4b6453b9a9097cb6abc27d14
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-05-28 11:37:02 -0400

    Added more style props to Progress Tracker component

--- resources/assets/js/components/ProgressTracker/ProgressTrackerItem.tsx
+++ resources/assets/js/components/ProgressTracker/ProgressTrackerItem.tsx
@@ -1,13 +1,14 @@
-import * as React from 'react';
+import * as React from "react";
 
 interface ProgressTrackerItemProps {
-  state: 'active' | 'complete' | 'error' | 'null';
+  state: "active" | "complete" | "error" | "null";
   label: string;
   title: string;
+  fontColor?: string;
 }
 const ProgressTrackerItem: React.FunctionComponent<
   ProgressTrackerItemProps
-> = ({ label, title, state }): React.ReactElement => {
+> = ({ label, title, state, fontColor }): React.ReactElement => {
   return (
     <div
       className="tracker-item"
@@ -15,13 +16,14 @@ const ProgressTrackerItem: React.FunctionComponent<
       data-c-alignment="base(left)"
       data-c-grid="middle"
       data-c-margin="top(half) right(normal) bottom(half) left(normal)"
+      data-c-color={fontColor}
     >
-      <div className="tracker-icon" data-c-color="white">
-        {state === 'active' && <i className="fas fa-arrow-down" />}
-        {state === 'complete' && <i className="fas fa-check" />}
-        {state === 'error' && <i className="fas fa-exclamation-triangle" />}
+      <div className="tracker-icon">
+        {state === "active" && <i className="fas fa-arrow-down" />}
+        {state === "complete" && <i className="fas fa-check" />}
+        {state === "error" && <i className="fas fa-exclamation-triangle" />}
       </div>
-      <div className="tracker-title" data-c-color="black">
+      <div className="tracker-title">
         <span data-c-font-size="small">{label}</span>
         <span data-c-font-weight="bold">{title}</span>
       </div>
