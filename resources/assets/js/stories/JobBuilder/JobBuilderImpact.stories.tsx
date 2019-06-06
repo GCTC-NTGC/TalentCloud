0bbb346b0b84400def4cb2dbe6b2bd424ea53148
Xander Miller <xander.miller@gmail.com>
2019-06-06 14:37:23 -0400

    adds impactForm to storybook

--- resources/assets/js/stories/JobBuilder/JobBuilderImpact.stories.tsx
+++ resources/assets/js/stories/JobBuilder/JobBuilderImpact.stories.tsx
@@ -3,15 +3,16 @@ import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 import { withKnobs } from "@storybook/addon-knobs";
 import JobBuilderImpact from "../../components/JobBuilderImpact/JobBuilderImpact";
+import ImpactForm from "../../components/JobBuilderImpact/ImpactForm";
 
 const stories = storiesOf("Job Builder - Impact", module)
   .addDecorator(withInfo)
   .addDecorator(withKnobs);
 
-stories.add(
-  "Job Builder Impact",
-  (): React.ReactElement => <JobBuilderImpact />,
-  {
+stories
+  .add("Job Builder Impact", (): React.ReactElement => <JobBuilderImpact />, {
     info: { inline: true },
-  },
-);
+  })
+  .add("Impact Form", (): React.ReactElement => <ImpactForm />, {
+    info: { inline: true },
+  });
