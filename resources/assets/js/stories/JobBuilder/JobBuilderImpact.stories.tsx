6731987056deaa6bfd8bb9d1f2eeb57a37056e38
Xander Miller <xander.miller@gmail.com>
2019-06-07 10:07:18 -0400

    Adds language selector to stories

--- resources/assets/js/stories/JobBuilder/JobBuilderImpact.stories.tsx
+++ resources/assets/js/stories/JobBuilder/JobBuilderImpact.stories.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { IntlProvider } from "react-intl";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, select } from "@storybook/addon-knobs";
 import JobBuilderImpact from "../../components/JobBuilderImpact/JobBuilderImpact";
 import ImpactForm from "../../components/JobBuilderImpact/ImpactForm";
 
@@ -9,10 +10,37 @@ const stories = storiesOf("Job Builder - Impact", module)
   .addDecorator(withInfo)
   .addDecorator(withKnobs);
 
+const langOptions = {
+  English: "en",
+  French: "fr",
+};
+
+// const selectLang = ;
+
 stories
-  .add("Job Builder Impact", (): React.ReactElement => <JobBuilderImpact />, {
-    info: { inline: true },
-  })
-  .add("Impact Form", (): React.ReactElement => <ImpactForm />, {
-    info: { inline: true },
-  });
+  .add(
+    "Job Builder Impact",
+    (): React.ReactElement => (
+      <IntlProvider
+        locale={select("Language", langOptions, "en", "Lang-Group")}
+      >
+        <JobBuilderImpact />
+      </IntlProvider>
+    ),
+    {
+      info: { inline: true },
+    },
+  )
+  .add(
+    "Impact Form",
+    (): React.ReactElement => (
+      <IntlProvider
+        locale={select("Language", langOptions, "en", "Lang-Group2")}
+      >
+        <ImpactForm />
+      </IntlProvider>
+    ),
+    {
+      info: { inline: true },
+    },
+  );
