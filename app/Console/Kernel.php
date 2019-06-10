b566c3a01ccf7fed55e7e2f104a82e08662b04c9
Unknown <tristan.m.orourke@gmail.com>
2019-06-10 15:44:05 -0400

    Add task scheduler command to restart worker queue if stopped
    Run `php artisan schedule:run` with a cron job to ensure worker queue is always running.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,11 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        // start the queue daemon, if its not running
+        if (!$this->osProcessIsRunning('queue:work')) {
+            $schedule->command('queue:work')->everyMinute();
+        }
     }
 
     /**
@@ -38,5 +43,31 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    /**
+     * checks, if a process with $needle in the name is running
+     *
+     * @param string $needle
+     * @return bool
+     */
+    protected function osProcessIsRunning($needle)
+    {
+        // get process status. the "-ww"-option is important to get the full output!
+        exec('ps aux -ww', $process_status);
+
+
+        // search $needle in process status
+        $result = array_filter($process_status,
+            function($var) use ($needle)
+            {   return strpos($var, $needle); });
+
+
+        // if the result is not empty, the needle exists in running processes
+        if (!empty($result)) {
+            return true;
+        }
+
+        return false;
     }
 }
