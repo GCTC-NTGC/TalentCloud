59303a6c77a3d5a567c89450e6bd164c02f71bb0
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-06-10 11:20:32 -0400

    Created Checkbox group component, however still specific to Formik (WIP)

---
+++ resources/assets/js/components/Forms/CheckboxGroup.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import Input from "./Input";
+
+interface CheckboxGroupProps {
+  id: string;
+  label: string;
+  grid?: string;
+  required?: boolean;
+  items?: [];
+  children?: any;
+  value: any;
+  error?: string;
+  touched?: boolean;
+
+  // formik
+  field?: any;
+  form?: any;
+  onChange: any;
+  onBlur?: any;
+}
+
+const CheckboxGroup: React.FunctionComponent<CheckboxGroupProps> = ({
+  id,
+  label: checkboxGroupLabel,
+  grid,
+  required,
+  children,
+  value,
+  error,
+  touched,
+  onChange,
+  onBlur,
+}): React.ReactElement => {
+  const handleChange = (event): void => {
+    // Get target checkbox element
+    const target = event.currentTarget;
+
+    // if the target is checked push the "name" to the values array
+    if (target.checked) {
+      value.push(target.id);
+    } else {
+      // if the target is unchecked then filter out the "name" from the array
+      value.splice(value.indexOf(target.id), 1);
+    }
+
+    onChange(id, value);
+  };
+
+  const handleBlur = () => {
+    onBlur(id, true);
+  };
+  return (
+    <div
+      data-c-grid-item={grid}
+      data-c-input="checkbox"
+      data-c-required={required}
+      data-c-invalid={touched && error ? true : null}
+    >
+      <label>{checkboxGroupLabel}</label>
+      {required && <span>Required</span>}
+      <div data-c-grid>
+        {React.Children.map(children, child => {
+          return React.cloneElement(child, {
+            field: {
+              value: value.includes(child.props.htmlId),
+              onChange: handleChange,
+              onBlur: handleBlur,
+            },
+            checked: value.includes(child.props.htmlId),
+          });
+        })}
+      </div>
+      <span>{touched && error}</span>
+    </div>
+  );
+};
+
+export default CheckboxGroup;
