f56bb2dc371205d2162ce11fece8f8062ef87dac
Unknown <tristan.m.orourke@gmail.com>
2019-06-13 16:29:50 -0400

    Created new modal stories for storybook
    You can toggle visibility. The createPortal function the Modal uses requires a storybook workaround, see the changes to config.ts

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -6,6 +6,16 @@ const req = require.context(
   /\.stories\.tsx$/
 );
 
+// Create a parent element for modals.
+// Modals use the createPortal function, which needs a dom node passed in as a parent.
+// That node needs to exist before the story file runs.
+// See for explanation: https://github.com/storybookjs/storybook/issues/4604
+// TODO: find better workaround
+const modalRoot = document.createElement("div");
+modalRoot.setAttribute("id", "modal-root");
+modalRoot.setAttribute("data-clone", "");
+document.body.append(modalRoot);
+
 function loadStories() {
   req.keys().forEach(req);
 }
