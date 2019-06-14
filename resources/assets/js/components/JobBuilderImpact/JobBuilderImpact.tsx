8e72cd3582efa23dfbbbbeaa5e1949a48a66efea
Xander Miller <xander.miller@gmail.com>
2019-06-14 10:04:20 -0400

    Adds localization messages for department impact

--- resources/assets/js/components/JobBuilderImpact/JobBuilderImpact.tsx
+++ resources/assets/js/components/JobBuilderImpact/JobBuilderImpact.tsx
@@ -1,10 +1,94 @@
 import * as React from "react";
-import { injectIntl, InjectedIntlProps, FormattedMessage } from "react-intl";
+import {
+  injectIntl,
+  InjectedIntlProps,
+  FormattedMessage,
+  defineMessages,
+} from "react-intl";
 import ProgressTracker from "../ProgressTracker/ProgressTracker";
 import { items } from "../ProgressTracker/fixtures/progressItems";
 import ImpactForm from "./ImpactForm";
 
 interface JobBuilderImpactProps {}
+
+const departmentImpactStatements = defineMessages({
+  treasuryBoard: {
+    id: "department.impactStatement.treasuryBoard",
+    defaultMessage:
+      "The Treasury Board of Canada Secretariat provides advice and makes recommendations on how the government spends money, how it regulates and how it is managed ensuring tax dollars are spent wisely and effectively for Canadians.",
+    description: "Department Impact Statement of Treasury Board",
+  },
+  naturalResources: {
+    id: "department.impactStatement.naturalResources",
+    defaultMessage:
+      "Natural Resources Canada seeks to enhance the responsible development and use of Canada’s natural resources and the competitiveness of Canada’s natural resources products.",
+    description: "Department Impact Statement of Natural Resources",
+  },
+  transport: {
+    id: "department.impactStatement.transport",
+    defaultMessage:
+      "Transport Canada works to serve the public interest through the promotion of a safe and secure, efficient and environmentally responsible transportation system in Canada.",
+    description: "Department Impact Statement of Transport",
+  },
+  environmentAndClimateChange: {
+    id: "department.impactStatement.environment",
+    defaultMessage:
+      "ECCC informs Canadians about protecting and conserving our natural heritage, and ensuring a clean, safe and sustainable environment for present and future generations.",
+    description:
+      "Department Impact Statement of Environment and Climate Change",
+  },
+  employmentAndSocialDevelopment: {
+    id: "department.impactStatement.employment",
+    defaultMessage:
+      "Employment and Social Development Canada (ESDC) works to improve the standard of living and quality of life for all Canadians. We do this by promoting a labour force that is highly skilled. We also promote an efficient and inclusive labour market.",
+    description:
+      "Department Impact Statement of Employment and Social Development",
+  },
+  globalAffairs: {
+    id: "department.impactStatement.globalAffairs",
+    defaultMessage:
+      "Global Affairs Canada manages Canada’s diplomatic relations, provides consular services to Canadians, promotes the country’s international trade, and leads Canada’s international development and humanitarian assistance.",
+    description: "Department Impact Statement of Global Affairs",
+  },
+  boarderServicesAgency: {
+    id: "department.impactStatement.boarder",
+    defaultMessage:
+      "The Canada Border Services Agency (CBSA) facilitates the flow of legitimate travellers and trade. The Agency also enforces more than 90 acts and regulations that keep our country and Canadians safe.",
+    description: "Department Impact Statement of Boarder Services Agency",
+  },
+  innovationScience: {
+    id: "department.impactStatement.science",
+    defaultMessage:
+      "Innovation, Science and Economic Development Canada (ISED) works with Canadians in all areas of the economy and in all parts of the country to improve conditions for investment, enhance Canada's innovation performance, increase Canada's share of global trade and build a fair, efficient and competitive marketplace.",
+    description:
+      "Department Impact Statement of Innovation, Science and Economic Development",
+  },
+  publicServiceAndProcurement: {
+    id: "department.impactStatement.procurement",
+    defaultMessage:
+      "Public Services and Procurement Canada serves federal departments and agencies as their central purchasing agent, real property manager, treasurer, accountant, pay and pension administrator, integrity adviser and linguistic authority.",
+    description:
+      "Department Impact Statement of Public Service and Procurement",
+  },
+  departmentNationalDefence: {
+    id: "department.impactStatement.defence",
+    defaultMessage:
+      "The Department of National Defence and the Canadian Armed Forces implement government decisions concerning the defence of Canadians’ interests at home and abroad.",
+    description: "Department Impact Statement of Department National Defence",
+  },
+  sharedServicesCanada: {
+    id: "department.impactStatement.sharedServices",
+    defaultMessage:
+      "Shared Services Canada (SSC) delivers digital services to Government of Canada organizations. We provide modern, secure and reliable IT services so federal organizations can deliver digital programs and services that meet Canadians needs.",
+    description: "Department Impact Statement of Shared Services Canada",
+  },
+  healthCanada: {
+    id: "department.impactStatement.health",
+    defaultMessage:
+      "Health Canada is responsible for helping Canadians maintain and improve their health. It ensures that high-quality health services are accessible, and works to reduce health risks.",
+    description: "Department Impact Statement of Shared Services Canada",
+  },
+});
 
 const JobBuilderImpact: React.FunctionComponent<
   JobBuilderImpactProps & InjectedIntlProps
