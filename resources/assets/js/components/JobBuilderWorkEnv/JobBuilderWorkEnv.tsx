9d65521dfbcbd6b64c6b7fc93826b4bcc169fe1d
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-06-18 10:21:52 -0400

    localized job builder work env container

--- resources/assets/js/components/JobBuilderWorkEnv/JobBuilderWorkEnv.tsx
+++ resources/assets/js/components/JobBuilderWorkEnv/JobBuilderWorkEnv.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { injectIntl, InjectedIntlProps, FormattedMessage } from "react-intl";
 import ProgressTracker from "../ProgressTracker/ProgressTracker";
 import { items } from "../ProgressTracker/fixtures/progressItems";
 import WorkEnvForm from "./WorkEnvForm";
@@ -6,7 +7,7 @@ import WorkEnvForm from "./WorkEnvForm";
 interface JobBuilderWorkEnvProps {}
 
 const JobBuilderWorkEnv: React.FunctionComponent<
-  JobBuilderWorkEnvProps
+  JobBuilderWorkEnvProps & InjectedIntlProps
 > = (): React.ReactElement => {
   return (
     <section data-c-background="white(100)">
@@ -23,23 +24,33 @@ const JobBuilderWorkEnv: React.FunctionComponent<
           data-c-font-weight="bold"
           data-c-margin="bottom(double)"
         >
-          Work Environment
+          <FormattedMessage
+            id="jobBuilder.workEnv.title"
+            defaultMessage="Work Environment"
+            description="Header of job poster builder work environment step."
+          />
         </h3>
         <p>
-          Applicants care a lot about the team they’ll be working with and the
-          physical workspace as well. Sharing information about these things
-          help applicants determine if they’ll be a good fit, and can reduce the
-          number of “wishful thinking” applications that slow down the screening
-          process.
+          <FormattedMessage
+            id="jobBuilder.workEnv.stepDescription"
+            defaultMessage="Applicants care a lot about the team they’ll be working with and the physical workspace as well. Sharing information about these things help applicants determine if they’ll be a good fit, and can reduce the number of “wishful thinking” applications that slow down the screening process."
+            description="Description of job poster builder work environment step."
+          />
         </p>
         <div data-c-grid-item="base(1of1)">
           <h4 data-c-font-size="h4" data-c-margin="top(triple) bottom(normal)">
-            Our Work Environment
+            <FormattedMessage
+              id="jobBuilder.workEnv.ourWorkEnv"
+              defaultMessage="Our Work Environment"
+              description="Section 1 of job poster builder work environment step."
+            />
           </h4>
           <p data-c-margin="bottom(normal)">
-            Share a little about your physical space, the technology used by
-            your team, and the amenities close to your office. Check all that
-            apply.
+            <FormattedMessage
+              id="jobBuilder.workEnv.ourWorkEnvSubtext"
+              defaultMessage="Share a little about your physical space, the technology used by your team, and the amenities close to your office. Check all that apply."
+              description="Section 1 subtext of job poster builder work environment step."
+            />
           </p>
         </div>
         <WorkEnvForm />
@@ -48,4 +59,4 @@ const JobBuilderWorkEnv: React.FunctionComponent<
   );
 };
 
-export default JobBuilderWorkEnv;
+export default injectIntl(JobBuilderWorkEnv);
