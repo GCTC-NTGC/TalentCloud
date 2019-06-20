e98f560eb7e3ee993e0f802b0796f6c70ebf7a3e
Xander Miller <xander.miller@gmail.com>
2019-06-20 11:50:39 -0400

    updates impact for to use withIntl storybook plugin

--- resources/assets/js/stories/JobBuilder/JobBuilderImpact.stories.tsx
+++ resources/assets/js/stories/JobBuilder/JobBuilderImpact.stories.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
+import { withIntl } from "storybook-addon-intl";
 import { withKnobs, select } from "@storybook/addon-knobs";
-import IntlContainer from "../../IntlContainer";
 import JobBuilderImpact from "../../components/JobBuilderImpact/JobBuilderImpact";
 import ImpactForm from "../../components/JobBuilderImpact/ImpactForm";
 
 const stories = storiesOf("Job Builder - Impact", module)
   .addDecorator(withInfo)
-  .addDecorator(withKnobs);
-
-const langOptions = {
-  English: "en",
-  French: "fr",
-};
+  .addDecorator(withKnobs)
+  .addDecorator(withIntl);
 
 const deptOptions = {
   "Treasury Board": "treasuryBoard",
@@ -36,26 +32,14 @@ stories
   .add(
     "Job Builder Impact",
     (): React.ReactElement => (
-      <IntlContainer locale={select("Language", langOptions, "en")}>
-        <JobBuilderImpact
-          department={select("Department", deptOptions, "treasuryBoard")}
-        />
-      </IntlContainer>
+      <JobBuilderImpact
+        department={select("Department", deptOptions, "treasuryBoard")}
+      />
     ),
     {
       info: { inline: true },
     },
   )
-  .add(
-    "Impact Form",
-    (): React.ReactElement => (
-      <IntlContainer
-        locale={select("Language", langOptions, "en", "Lang-Group2")}
-      >
-        <ImpactForm />
-      </IntlContainer>
-    ),
-    {
-      info: { inline: true },
-    },
-  );
+  .add("Impact Form", (): React.ReactElement => <ImpactForm />, {
+    info: { inline: true },
+  });
