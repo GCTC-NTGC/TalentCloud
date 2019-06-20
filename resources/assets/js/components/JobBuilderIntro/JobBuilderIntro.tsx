5f21105023867f62fff2e491b916023ac1cc979f
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-06-20 11:37:25 -0400

    refactored IntroForm to use Formik component instead of HOC structure, added missing localizations

--- resources/assets/js/components/JobBuilderIntro/JobBuilderIntro.tsx
+++ resources/assets/js/components/JobBuilderIntro/JobBuilderIntro.tsx
@@ -1,15 +1,16 @@
 import * as React from "react";
-import ReactDOM from "react-dom";
-import { injectIntl, InjectedIntlProps, FormattedMessage } from "react-intl";
+import { injectIntl, InjectedIntlProps } from "react-intl";
 import ProgressTracker from "../ProgressTracker/ProgressTracker";
 import { items } from "../ProgressTracker/fixtures/progressItems";
 import IntroForm from "./IntroForm";
 
-interface JobBuilderIntroProps {}
+interface JobBuilderIntroProps {
+  handleSubmit: (values) => void;
+}
 
 const JobBuilderIntro: React.FunctionComponent<
   JobBuilderIntroProps & InjectedIntlProps
-> = (): React.ReactElement => {
+> = ({ handleSubmit }): React.ReactElement => {
   return (
     <section>
       <ProgressTracker
@@ -19,51 +20,8 @@ const JobBuilderIntro: React.FunctionComponent<
         classNames="manager-jpb-tracker"
         itemsWrapperClassNames="tracker manager-jpb-tracker-wrapper"
       />
-      <div data-c-container="form" data-c-padding="top(triple) bottom(triple)">
-        <h3
-          data-c-font-size="h3"
-          data-c-font-weight="bold"
-          data-c-margin="bottom(normal)"
-        >
-          <FormattedMessage
-            id="jobBuilder.intro.welcome"
-            defaultMessage="EN Welcome to the Job Poster Builder"
-            description="Header of Job Poster Builder Intro Step"
-          />
-        </h3>
-        <p data-c-margin="bottom(normal)">
-          <FormattedMessage
-            id="jobBuilder.intro.subtitle01"
-            defaultMessage="This tool will help you create a job poster that attracts the right talent."
-            description="Subtitle 1 of Job Poster Builder Intro Step"
-          />
-        </p>
-        <p data-c-margin="bottom(double)">
-          <FormattedMessage
-            id="jobBuilder.intro.subtitle02"
-            defaultMessage="We’ve also provided instructions and examples to help guide you through the process but if you still have questions, contact "
-            description="Subtitle 2 of Job Poster Builder Intro Step"
-          />
-          <a
-            href="mailto:talent.cloud-nuage.de.talents@tbs-sct.gc.ca"
-            title="Email Talent Cloud."
-          >
-            Talent Cloud
-          </a>
-        </p>
-        <h4
-          data-c-colour="c3"
-          data-c-font-size="h4"
-          data-c-margin="bottom(double)"
-        >
-          <FormattedMessage
-            id="jobBuilder.intro.beforeWeStart"
-            defaultMessage="Before we get started please review your information."
-            description="Before starting header of Job Poster Builder Intro Step"
-          />
-        </h4>
-        <IntroForm />
-      </div>
+
+      <IntroForm handleSubmit={handleSubmit} />
     </section>
   );
 };
--- resources/assets/js/stories/JobBuilderIntro/JobBuilderIntro.stories.tsx
+++ resources/assets/js/stories/JobBuilderIntro/JobBuilderIntro.stories.tsx
@@ -1,34 +1,20 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
-import { withKnobs, select } from "@storybook/addon-knobs";
-import IntlContainer from "../../IntlContainer";
+import { withIntl } from "storybook-addon-intl";
+import { withKnobs } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
 import JobBuilderIntro from "../../components/JobBuilderIntro/JobBuilderIntro";
-import IntroForm from "../../components/JobBuilderIntro/IntroForm";
 
 const stories = storiesOf("Job Builder - Intro", module)
   .addDecorator(withInfo)
-  .addDecorator(withKnobs);
+  .addDecorator(withKnobs)
+  .addDecorator(withIntl);
 
-const langOptions = {
-  English: "en",
-  French: "fr",
-};
-
-stories
-  .add(
-    "Job Builder Body",
-    (): React.ReactElement => (
-      <IntlContainer
-        locale={select("Language", langOptions, "en", "Lang-Group")}
-      >
-        <JobBuilderIntro />
-      </IntlContainer>
-    ),
-    {
-      info: { inline: true },
-    },
-  )
-  .add("Intro Form", (): React.ReactElement => <IntroForm />, {
+stories.add(
+  "Intro Body",
+  (): React.ReactElement => <JobBuilderIntro handleSubmit={action("Submit")} />,
+  {
     info: { inline: true },
-  });
+  },
+);
