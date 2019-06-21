862d346cc399a92801d89665d2aa5b37560eb1a4
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-06-21 16:12:23 -0400

    Seperated Formik concerns from Select component, created new SelectFormik component

---
+++ resources/assets/js/components/Form/SelectFormik.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { FieldProps, FormikActions, FormikErrors } from "formik";
+import { FormattedMessage } from "react-intl";
+import { inputMessages } from "./Messages";
+import Select from "../Forms/Select";
+
+interface SelectFormikProps {
+  // HTML ID of the input.
+  id: string;
+  // Text for the associated label of the input.
+  label: string;
+  // data-clone-grid-item value, see https://designwithclone.ca/#flexbox-grid.
+  grid: string;
+  // If this input is required for submission.
+  required: boolean;
+  /** Selected string contains the default value of the select box */
+  selected: string | number | null;
+  // Options that are rendered out within the input.
+  options: { label: string; value: string | number }[];
+  // Text for an empty selection.
+  nullSelection: string;
+  // Formik field prop of the shape { name, value, onChange, onBlur }
+  field: FieldProps["field"];
+  // Formik form prop of the shape { errors }
+  form: FieldProps["form"];
+}
+
+const SelectFormik: React.FunctionComponent<SelectFormikProps> = ({
+  id,
+  label,
+  grid,
+  required,
+  options,
+  selected,
+  nullSelection,
+  field: { name, value, onChange, onBlur },
+  form: { errors, touched },
+  ...props
+}): React.ReactElement => {
+  // TODO: find solution for ts error
+  //@ts-ignore
+  const errorText: string = errors[name] ? errors[name] : undefined;
+  const invalid = touched[name] && errors[name] ? true : null;
+  return (
+    <Select
+      id={id}
+      label={label}
+      name={name}
+      nullSelection={nullSelection}
+      selected={selected}
+      options={options}
+      grid={grid}
+      onChange={onChange}
+      onBlur={onBlur}
+      errorText={errorText}
+      required
+      invalid={invalid}
+    />
+  );
+};
+
+export default SelectFormik;
