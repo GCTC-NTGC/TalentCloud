1f668b94853bfc70dcb60a8642fcfa8ae10785db
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-06-27 09:32:34 -0400

    Renamed and Refactored SelectInput (previously SelectFormik)  component

--- resources/assets/js/components/Form/SelectInput.tsx
+++ resources/assets/js/components/Form/SelectInput.tsx
@@ -2,41 +2,39 @@ import React from "react";
 import { FieldProps } from "formik";
 import Select from "./Select";
 
-interface SelectFormikProps {
-  // HTML ID of the input.
+interface SelectInputProps {
+  /** HTML ID of the input. */
   id: string;
-  // Text for the associated label of the input.
+  /** Text for the associated label of the input. */
   label: string;
-  // data-clone-grid-item value, see https://designwithclone.ca/#flexbox-grid.
+  /** data-clone-grid-item value, see https:/;designwithclone.ca/#flexbox-grid. */
   grid: string;
-  // If this input is required for submission.
+  /** If this input is required for submission. */
   required: boolean;
   /** Selected string contains the default value of the select box */
   selected: string | number | null;
-  // Options that are rendered out within the input.
+  /** Options that are rendered out within the input. */
   options: { label: string; value: string | number }[];
-  // Text for an empty selection.
+  /** Text for an empty selection. */
   nullSelection: string;
-  // Formik field prop of the shape { name, value, onChange, onBlur }
+  /** Formik field prop of the shape { name, value, onChange, onBlur } */
   field: FieldProps["field"];
-  // Formik form prop of the shape { errors }
+  /** Formik form prop of the shape { errors } */
   form: FieldProps["form"];
 }
 
-const SelectFormik: React.FunctionComponent<SelectFormikProps> = ({
+const SelectInput: React.FunctionComponent<SelectInputProps> = ({
   id,
   label,
   grid,
   required,
   options,
-  selected,
   nullSelection,
   field: { name, value, onChange, onBlur },
   form: { errors, touched },
-  ...props
 }): React.ReactElement => {
   // TODO: find solution for ts error
-  //@ts-ignore
+  // @ts-ignore
   const errorText: string = errors[name] ? errors[name] : undefined;
   const invalid = touched[name] && errors[name] ? true : null;
   return (
@@ -44,6 +42,7 @@ const SelectFormik: React.FunctionComponent<SelectFormikProps> = ({
       id={id}
       label={label}
       name={name}
+      required={required}
       nullSelection={nullSelection}
       options={options}
       grid={grid}
@@ -51,10 +50,9 @@ const SelectFormik: React.FunctionComponent<SelectFormikProps> = ({
       onChange={onChange}
       onBlur={onBlur}
       errorText={errorText}
-      required
       invalid={invalid}
     />
   );
 };
 
-export default SelectFormik;
+export default SelectInput;
