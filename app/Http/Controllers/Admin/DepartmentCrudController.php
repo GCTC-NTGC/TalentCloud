022d358b2d1e66e877848ac76366c868d43fdb9b
Grant <grant.d.barnes@gmail.com>
2019-06-27 14:37:14 -0400

    edit model / create controller

---
+++ app/Http/Controllers/Admin/DepartmentCrudController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Illuminate\Support\Facades\App;
+// Validation.
+use App\Http\Requests\DepartmentCrudRequest as StoreRequest;
+use App\Http\Requests\DepartmentCrudRequest as UpdateRequest;
+
+class DepartmentCrudController extends CrudController
+{
+    /**
+     * Prepare the admin interface by setting the associated
+     * model, setting the route, and adding custom columns/fields.
+     *
+     * @return void
+     */
+    public function setup() : void
+    {
+        // Eloquent model to associate with this collection of views and controller actions.
+        $this->crud->setModel('App\Models\Lookup\DepartmentTranslation');
+        // Custom backpack route.
+        $this->crud->setRoute('admin/department');
+        // Custom strings to display within the backpack UI.
+        $this->crud->setEntityNameStrings('department', 'Departments');
+
+        // Add custom columns to the Department index view.
+        $this->crud->addColumn([
+            'name' => 'value',
+            'type' => 'text',
+            'label' => 'Name',
+        ]);
+
+        // Add custom fields to the create/update views.
+        $this->crud->addField([
+            'name' => 'value',
+            'type' => 'text',
+            'label' => 'Name',
+        ]);
+
+        // TODO: Impact field.
+    }
+
+    /**
+     * Action for updating an existing Department in the database.
+     *
+     * @param  \App\Http\Requests\DepartmentCrudRequest $request Incoming form request.
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(StoreRequest $request) // phpcs:ignore
+    {
+        return parent::storeCrud();
+    }
+
+    /**
+     * Action for updating an existing Department in the database.
+     *
+     * @param  \App\Http\Requests\DepartmentCrudRequest $request Incoming form request.
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UpdateRequest $request) // phpcs:ignore
+    {
+        return parent::updateCrud();
+    }
+}
--- app/Models/Lookup/DepartmentTranslation.php
+++ app/Models/Lookup/DepartmentTranslation.php
@@ -8,28 +8,47 @@
 namespace App\Models\Lookup;
 
 use App\Models\BaseModel;
+use Backpack\CRUD\CrudTrait;
 
 /**
  * Class DepartmentTranslation
- * 
+ *
  * @property int $id
  * @property int $department_id
  * @property string $locale
  * @property string $value
  * @property \Jenssegers\Date\Date $created_at
  * @property \Jenssegers\Date\Date $updated_at
- * 
+ *
  * @property \App\Models\Lookup\Department $department
  */
-class DepartmentTranslation extends BaseModel {
+class DepartmentTranslation extends BaseModel
+{
+    use CrudTrait;
 
+    /**
+     *
+     * @var $casts
+     */
     protected $casts = [
         'department_id' => 'int'
     ];
-    protected $fillable = [];
 
-    public function department() {
+    /**
+     *
+     * @var $fillable
+     */
+    protected $fillable = [
+        'value' => 'string'
+    ];
+
+    /**
+     * Action for updating an existing Department in the database.
+     *
+     * @return \App\Models\Lookup\Department
+     */
+    public function department(): object
+    {
         return $this->belongsTo(\App\Models\Lookup\Department::class);
     }
-
 }
