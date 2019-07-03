d775c95d26c6da850577007c9d3222a1dcc987db
Grant Barnes <grant.d.barnes@gmail.com>
2019-07-03 07:56:32 -0400

    add impact statements to lang file

--- resources/lang/en/common/lookup/departments.php
+++ resources/lang/en/common/lookup/departments.php
@@ -13,51 +13,51 @@ return [
     'departments' => [
         'treasury_board' => [
             'name' => 'Treasury Board of Canada Secretariat',
-            'impact' => 'EN01'
+            'impact' => 'The Treasury Board of Canada Secretariat provides advice and makes recommendations on how the government spends money, how it regulates and how it is managed ensuring tax dollars are spent wisely and effectively for Canadians.'
         ],
         'natural_resources' => [
             'name' => 'Natural Resources Canada',
-            'impact' => 'EN02'
+            'impact' => 'Natural Resources Canada seeks to enhance the responsible development and use of Canada\'s natural resources and the competitiveness of Canada\'s natural resources products.'
         ],
         'transport' => [
             'name' => 'Transport Canada',
-            'impact' => 'EN03'
+            'impact' => 'Transport Canada works to serve the public interest through the promotion of a safe and secure, efficient and environmentally responsible transportation system in Canada.'
         ],
         'enviroment_and_climate_change' => [
             'name' => 'Environment and Climate Change Canada',
-            'impact' => 'EN04'
+            'impact' => 'ECCC informs Canadians about protecting and conserving our natural heritage, and ensuring a clean, safe and sustainable environment for present and future generations.'
         ],
         'employment_and_social_development' => [
             'name' => 'Employment and Social Development Canada',
-            'impact' => 'EN05'
+            'impact' => 'Employment and Social Development Canada (ESDC) works to improve the standard of living and quality of life for all Canadians. We do this by promoting a labour force that is highly skilled. We also promote an efficient and inclusive labour market.'
         ],
         'global_affairs' => [
             'name' => 'Global Affairs Canada',
-            'impact' => 'EN06'
+            'impact' => 'Global Affairs Canada manages Canada\'s diplomatic relations, provides consular services to Canadians, promotes the country\'s international trade, and leads Canada\'s international development and humanitarian assistance.'
         ],
         'border_services_agency' => [
             'name' => 'Canada Border Services Agency',
-            'impact' => 'EN07'
+            'impact' => 'The Canada Border Services Agency (CBSA) facilitates the flow of legitimate travellers and trade. The Agency also enforces more than 90 acts and regulations that keep our country and Canadians safe.'
         ],
-        'fisheries_and_oceans' => [
-            'name' => 'Fisheries and Oceans Canada',
-            'impact' => 'EN08'
+        'shared_services' => [
+            'name' => 'Shared Services Canada',
+            'impact' => 'Shared Services Canada (SSC) delivers digital services to Government of Canada organizations. We provide modern, secure and reliable IT services so federal organizations can deliver digital programs and services that meet Canadians needs.'
         ],
         'innovation_science' => [
             'name' => 'Innovation, Science and Economic Development Canada',
-            'impact' => 'EN09'
+            'impact' => 'Innovation, Science and Economic Development Canada (ISED) works with Canadians in all areas of the economy and in all parts of the country to improve conditions for investment, enhance Canada\'s innovation performance, increase Canada\'s share of global trade and build a fair, efficient and competitive marketplace.'
         ],
         'public_service_and_procurement' => [
-            'name' => 'Public Services and Procurement Canada',
-            'impact' => 'EN10'
+            'name' => 'public Services and Procurement Canada',
+            'impact' => 'Public Services and Procurement Canada serves federal departments and agencies as their central purchasing agent, real property manager, treasurer, accountant, pay and pension administrator, integrity adviser and linguistic authority.'
         ],
         'department_national_defence' => [
             'name' => 'Department of National Defence',
-            'impact' => 'EN11'
+            'impact' => 'The Department of National Defence and the Canadian Armed Forces implement government decisions concerning the defence of Canadians\' interests at home and abroad.'
         ],
-        'office_privacy_comissioner' => [
-            'name' => 'Office of the Privacy Commissioner of Canada',
-            'impact' => 'EN12'
+        'health' => [
+            'name' => 'Health Canada',
+            'impact' => 'Health Canada is responsible for helping Canadians maintain and improve their health. It ensures that high-quality health services are accessible, and works to reduce health risks.'
         ],
     ]
 ];
--- resources/lang/fr/common/lookup/departments.php
+++ resources/lang/fr/common/lookup/departments.php
@@ -13,51 +13,52 @@ return [
     'departments' => [
         'treasury_board' => [
             'name' => 'Secrétariat du Conseil du Trésor du Canada',
-            'impact' => 'FR01'
+            'impact' => 'Le Secrétariat du Conseil du Trésor du Canada fournit des conseils et des recommandations sur la façon dont le gouvernement investit dans les programmes et les services, ainsi que sur la façon dont il en assure la réglementation et la gestion pour faire en sorte que l\'argent des contribuables soit utilisé de manière judicieuse et efficace pour les Canadiens.'
         ],
         'natural_resources' => [
             'name' => 'Ressources naturelles Canada',
-            'impact' => 'FR02'
+            'impact' => 'Ressources naturelles Canada cherche à renforcer le développement et l\'utilisation responsables des ressources naturelles du Canada et la compétitivité des produits tirés des ressources naturelles du pays.'
         ],
         'transport' => [
             'name' => 'Transports Canada',
-            'impact' => 'FR03'
+            'impact' => 'Transports Canada sert l\'intérêt public en favorisant un réseau de transport au Canada qui est sûr et sécuritaire, efficace et respectueux de l\'environnement.'
         ],
         'enviroment_and_climate_change' => [
             'name' => 'Environnement et Changement climatique Canada',
-            'impact' => 'FR04'
+            'impact' => 'Environnement et Changement climatique Canada s\'engage à protéger l\'environnement, à conserver le patrimoine naturel du pays et à fournir des renseignements météorologiques pour tenir les Canadiens informés et en sécurité.'
         ],
         'employment_and_social_development' => [
             'name' => 'Emploi et Développement social Canada',
-            'impact' => 'FR05'
+            'impact' => 'Emploi et Développement social Canada (EDSC) travaille à améliorer le niveau de vie et la qualité de vie de tous les Canadiens en faisant la promotion d\'une main-d\'œuvre mobile et hautement spécialisée ainsi que d\'un marché du travail efficace et favorable à l\'inclusion.
+            '
         ],
         'global_affairs' => [
             'name' => 'Affaires mondiales Canada',
-            'impact' => 'FR06'
+            'impact' => 'Affaires mondiales Canada gère les relations diplomatiques du Canada, fournit les services consulaires aux canadiens, fait la promotion du commerce international du pays et dirige le développement international et l\'aide humanitaire du Canada.'
         ],
         'border_services_agency' => [
             'name' => 'Agence des services frontaliers du Canada',
-            'impact' => 'FR07'
+            'impact' => 'L\'Agence des services frontaliers du Canada (ASFC) facilite la circulation des voyageurs et des marchandises commerciales légitimes. L\'Agence applique aussi plus de 90 lois et règlements qui assurent la sécurité du pays et des Canadiens.'
         ],
-        'fisheries_and_oceans' => [
-            'name' => 'Pêches et Océans Canada',
-            'impact' => 'FR08'
+        'shared_services' => [
+            'name' => 'Services partagés Canada',
+            'impact' => 'Services partagés Canada (SPC) offre des services numériques aux organismes du gouvernement du Canada. Nous offrons des services de TI modernes, sécurisés et fiables pour permettre aux organismes fédéraux d\'offrir des programmes et des services numériques qui répondent aux besoins des Canadiens et des Canadiennes.'
         ],
         'innovation_science' => [
             'name' => 'Innovation, Sciences et Développement économique Canada',
-            'impact' => 'FR09'
+            'impact' => 'Innovation, Sciences et Développement économique Canada (ISDE) travaille avec les Canadiens de tous les secteurs de l\'économie et de toutes les régions du pays à instaurer un climat favorable à l\'investissement, à stimuler l\'innovation, à accroître la présence canadienne sur les marchés mondiaux et à créer un marché équitable, efficace et concurrentiel.'
         ],
         'public_service_and_procurement' => [
             'name' => 'Services publics et Approvisionnement Canada',
-            'impact' => 'FR10'
+            'impact' => 'Services publics et Approvisionnement Canada sert les ministères et organismes fédéraux en tant qu\'acheteur central, gestionnaire de biens immobiliers, trésorier, comptable, administrateur de la paye et des pensions, conseiller en matière d\'intégrité et spécialiste des questions linguistiques.'
         ],
         'department_national_defence' => [
             'name' => 'Ministère de la Défense nationale',
-            'impact' => 'FR11'
+            'impact' => 'Le ministère de la Défense nationale et les Forces armées canadiennes mettent en œuvre les décisions du gouvernement en ce qui concerne la défense des intérêts canadiens au pays et à l\'étranger.'
         ],
-        'office_privacy_comissioner' => [
-            'name' => 'Commissariat à la protection de la vie privée du Canada',
-            'impact' => 'FR12'
+        'health' => [
+            'name' => 'Santé Canada',
+            'impact' => 'Santé Canada a pour mandat d\'aider les Canadiens à conserver et à améliorer leur santé. Il s\'assure d\'offrir des services de santé de grande qualité, et cherche à réduire les risques pour la santé.'
         ],
     ]
 ];
