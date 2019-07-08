fb210e16a32131e6c806be754b13fe61c248d779
Unknown <tristan.m.orourke@gmail.com>
2019-07-08 17:59:49 -0400

    Fix type errors on CheckboxGroup

--- resources/assets/js/components/Form/CheckboxGroup.tsx
+++ resources/assets/js/components/Form/CheckboxGroup.tsx
@@ -13,9 +13,9 @@ interface CheckboxGroupProps {
   /** If this input is required for submission. */
   required: boolean;
   /** Error to display. */
-  error: undefined[] | undefined;
+  error: (string | undefined)[] | undefined;
   /** If this group has been affected by user input or a submission. */
-  touched: undefined[] | undefined;
+  touched: (boolean | undefined)[] | undefined;
   /** Array of all selected values */
   value: string[] | number[];
   /** Function which takes an id and value as arguments, and sets the field (id) to the value */
