96ae56a1442e475023b165f6e353d7207a919ddf
Unknown <tristan.m.orourke@gmail.com>
2019-07-12 10:04:51 -0400

    WIP Job Builder Impact Page component

---
+++ resources/assets/js/components/JobBuilderImpact/JobBuilderImpactPage.tsx
@@ -0,0 +1,137 @@
+import React, { useEffect } from "react";
+import { InjectedIntlProps, FormattedMessage } from "react-intl";
+import { Department, Job } from "../../models/types";
+import { ProgressTrackerItem } from "../ProgressTracker/types";
+import {
+  isJobBuilderIntroComplete,
+  isJobBuilderDetailsComplete,
+} from "../JobBuilder/jobBuilderHelpers";
+import ProgressTracker from "../ProgressTracker/ProgressTracker";
+import JobBuilderImpact from "./JobBuilderImpact";
+import {
+  progressTrackerLabels,
+  progressTrackerTitles,
+} from "../JobBuilder/jobBuilderMessages";
+
+interface JobBuilderImpactPageProps {
+  jobId: number | null;
+  job: Job | null;
+  loadJob: (jobId: number) => void;
+  departments: Department[];
+  loadDepartments: () => void;
+  handleCreateJob: (newJob: Job) => Promise<boolean>;
+  handleUpdateJob: (newJob: Job) => Promise<boolean>;
+}
+
+const JobBuilderImpactPage: React.FunctionComponent<
+  JobBuilderImpactPageProps & InjectedIntlProps
+> = ({
+  jobId,
+  job,
+  loadJob,
+  departments,
+  loadDepartments,
+  handleCreateJob,
+  handleUpdateJob,
+  intl,
+}): React.ReactElement => {
+  // Load Job and Departments from api
+  useEffect((): void => {
+    if (jobId !== null) {
+      loadJob(jobId);
+    }
+  }, [jobId, loadJob]);
+  useEffect((): void => {
+    if (departments.length === 0) {
+      loadDepartments();
+    }
+  }, [departments, loadDepartments]);
+
+  const waitingForJob = jobId !== null && job === null;
+  const handleModalCancel = (): void => {
+    // Do nothing on cancel
+  };
+  const handleModalConfirm = (): void => {
+    window.location.href = "/manager/jobs";
+  }; // TODO: go to next page
+  const handleSubmit = job ? handleUpdateJob : handleCreateJob;
+  const progressTrackerItems: ProgressTrackerItem[] = [
+    {
+      state: job && isJobBuilderIntroComplete(job) ? "complete" : "error",
+      label: intl.formatMessage(progressTrackerLabels.start),
+      title: intl.formatMessage(progressTrackerTitles.welcome),
+    },
+    {
+      state:
+        job && isJobBuilderDetailsComplete(job, intl.locale)
+          ? "complete"
+          : "error",
+      label: intl.formatMessage(progressTrackerLabels.step01),
+      title: intl.formatMessage(progressTrackerTitles.jobInfo),
+    },
+    {
+      state: "active",
+      label: intl.formatMessage(progressTrackerLabels.step02),
+      title: intl.formatMessage(progressTrackerTitles.workEnv),
+    },
+    {
+      state: "null",
+      label: intl.formatMessage(progressTrackerLabels.step03),
+      title: intl.formatMessage(progressTrackerTitles.impact),
+    },
+    {
+      state: "null",
+      label: intl.formatMessage(progressTrackerLabels.step04),
+      title: intl.formatMessage(progressTrackerTitles.tasks),
+    },
+    {
+      state: "null",
+      label: intl.formatMessage(progressTrackerLabels.step05),
+      title: intl.formatMessage(progressTrackerTitles.skills),
+    },
+    {
+      state: "null",
+      label: intl.formatMessage(progressTrackerLabels.finish),
+      title: intl.formatMessage(progressTrackerTitles.review),
+    },
+  ];
+  return (
+    <section>
+      <ProgressTracker
+        items={progressTrackerItems}
+        backgroundColor="black"
+        fontColor="white"
+        classNames="manager-jpb-tracker"
+        itemsWrapperClassNames="tracker manager-jpb-tracker-wrapper"
+      />
+      {waitingForJob ? (
+        <div
+          data-c-container="form"
+          data-c-padding="top(triple) bottom(triple)"
+        >
+          <h3
+            data-c-font-size="h3"
+            data-c-font-weight="bold"
+            data-c-margin="bottom(double)"
+          >
+            <FormattedMessage
+              id="jobBuilderImpact.jobloading"
+              defaultMessage="Job Loading..."
+              description="Message indicating that the current job is still being loaded."
+            />
+          </h3>
+        </div>
+      ) : (
+        <JobBuilderImpact
+          job={job}
+          departments={departments}
+          handleSubmit={handleSubmit}
+          handleModalCancel={handleModalCancel}
+          handleModalConfirm={handleModalConfirm}
+        />
+      )}
+    </section>
+  );
+};
+
+export default JobBuilderImpactPage;
