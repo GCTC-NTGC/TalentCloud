5dcbda5327c7d211c3e082bff147dcb5c75bac52
Grant <grant.d.barnes@gmail.com>
2019-07-16 15:20:06 -0400

    match departments to live DB / order of migrations

--- database/migrations/2019_07_04_123456_add_nrc_to_departments.php
+++ database/migrations/2019_07_04_123456_add_nrc_to_departments.php
@@ -12,12 +12,12 @@ class AddNrcToDepartments extends Migration
     public function up()
     {
         DB::table('departments')->insert([
-            ['name' => 'national_research_council']
+            ['id' => 13, 'name' => 'national_research_council']
         ]);
 
         DB::table('department_translations')->insert([
-            ['department_id' => '13', 'locale' => 'en', 'value' => 'National Research Council Canada', 'impact' => 'The National Research Council Canada (NRC) is the Government of Canada\'s largest science and research organization. The NRC partners with Canadian industry to take research impacts from the lab to the marketplace, where people can experience the benefits.'],
-            ['department_id' => '13', 'locale' => 'fr', 'value' => 'Conseil national de recherches Canada', 'impact' => 'Le Conseil national de recherches Canada (CNRC) est l\'organisation scientifique et de recherche la plus importante du gouvernement canadien. Le CNRC s\'associe à l\'industrie canadienne pour sortir la recherche du laboratoire et lui trouver des applications commerciales dont les retombées bénéficieront à l\'ensemble de la population.'],
+            ['id' => 25, 'department_id' => '13', 'locale' => 'en', 'value' => 'National Research Council Canada'],
+            ['id' => 26, 'department_id' => '13', 'locale' => 'fr', 'value' => 'Conseil national de recherches Canada'],
         ]);
     }
 
