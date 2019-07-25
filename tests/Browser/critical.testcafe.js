b679a2e1a0543e2fedaf7fd6d9c1663191a12074
Grant Barnes <grant.d.barnes@gmail.com>
2019-07-25 14:33:02 -0400

    user roles, add certification / equiv experience

--- tests/Browser/critical.testcafe.js
+++ tests/Browser/critical.testcafe.js
@@ -1,26 +1,46 @@
-import { Selector } from "testcafe";
+import { Selector, Role } from "testcafe";
 
 fixture(`Critical`).page(`talent.test`);
 
+// Logins for each user role, allows quick switching.
+
+const applicantUser = Role("http://talent.test/login", async t => {
+  await t
+    .typeText("#email", "applicant@test.com")
+    .typeText("#password", "password")
+    .click(Selector("button").withText("Login"));
+});
+
+const managerUser = Role("http://talent.test/manager/login", async t => {
+  await t
+    .typeText("#email", "manager@test.com")
+    .typeText("#password", "password")
+    .click(Selector("button").withText("Login"));
+});
+
+const adminUser = Role("http://talent.test/admin/login", async t => {
+  await t
+    .typeText(
+      Selector(".form-control").withAttribute("name", "email"),
+      "admin@test.com",
+    )
+    .typeText(
+      Selector(".form-control").withAttribute("name", "password"),
+      "password",
+    )
+    .pressKey("enter");
+});
+
 test("Applicant Profile", async t => {
   await t
-    // Login as test applicant.
-    // TODO: register new user instead
-    .click(Selector("a").withText("Login"))
-    .typeText(Selector("#email"), "applicant@test.com")
-    .typeText(Selector("#password"), "password")
-    .click(Selector("button").withText("Login"))
-
-    // Experience page (incomplete).
+    // Logged in as applicant.
+    .useRole(applicantUser)
+    // Go to My Experience page.
     .navigateTo("/profile/experience")
     .expect(Selector("h1").withText("My Experience").visible)
     .ok()
+    // Add new diploma.
     .click(Selector("button").withText("Add Diploma/Degree"))
-    .expect(
-      Selector("select").withAttribute("id", "degrees[new][1]degreeType")
-        .visible,
-    )
-    .ok()
     .pressKey("tab")
     .click(Selector("select").withAttribute("id", "degrees[new][1]degreeType"))
     .click(
@@ -40,22 +60,93 @@ test("Applicant Profile", async t => {
     .click(Selector("button").withAttribute("value", "degrees[new][1]"))
     .expect(Selector("span").withText("Phd, Computer Science").visible)
     .ok()
-
-    // Skills page (incomplete).
-    // Regression test for skill help modal.
+    // Add new course.
+    .click(Selector("button").withText("Add Course/Certification"))
+    .pressKey("tab")
+    .typeText(
+      Selector("input").withAttribute("id", "courses[new][1]courseName"),
+      "Advanced Memes",
+    )
+    .typeText(
+      Selector("input").withAttribute("id", "courses[new][1]courseInstitution"),
+      "Yale University",
+    )
+    .click(
+      Selector("select").withAttribute("id", "courses[new][1]courseStatus"),
+    )
+    .click(
+      Selector("select")
+        .withAttribute("id", "courses[new][1]courseStatus")
+        .find("option")
+        .withAttribute("value", "2"),
+    )
+    .click(Selector("button").withAttribute("value", "courses[new][1]"))
+    .expect(Selector("span").withText("Advanced Memes").visible)
+    .ok()
+    // Add new experience.
+    .click(Selector("button").withText("Add Equivalent Experience"))
+    .pressKey("tab")
+    .typeText(
+      Selector("input").withAttribute("id", "work_experiences[new][1]workRole"),
+      "The boss",
+    )
+    .typeText(
+      Selector("input").withAttribute(
+        "id",
+        "work_experiences[new][1]workCompany",
+      ),
+      "My house",
+    )
+    .typeText(
+      Selector("textarea").withAttribute(
+        "id",
+        "work_experiences[new][1]workDescription",
+      ),
+      "It was the best of times, it was the worst of times.",
+    )
+    .click(
+      Selector("button").withAttribute("value", "work_experiences[new][1]"),
+    )
+    .expect(Selector("span").withText("The boss, My house").visible)
+    .ok()
+    // Go to My Skills page.
     .navigateTo("/profile/skills")
     .expect(Selector("h1").withText("My Skills").visible)
     .ok()
-    .click(Selector("button").withText("Add Skill"))
+    // Add soft skill.
+    .click(Selector("button").withText("Add soft skill"))
+    .click(
+      Selector("select").withAttribute(
+        "id",
+        "skill_declarations[new][soft][1]skillSelection",
+      ),
+    )
+    .click(
+      Selector("select")
+        .withAttribute("id", "skill_declarations[new][soft][1]skillSelection")
+        .find("option")
+        .withAttribute("value", "7"),
+    )
+    // TODO: Finish skills.
+
+    // Check the skill help modal.
     .click(Selector("button").withText("(Need help? See example.)"))
     .expect(Selector("h3").withText("Writing my application").visible)
     .ok()
     .click(Selector("button").withText("Got it!"))
 
-    // About page
+    // TODO: Add new soft skill.
+    // TODO: Add new hard skill.
+
+    // TODO: References page.
+
+    // TODO: Work samples page.
+
+    // Go to About Me page
     .navigateTo("/profile/about")
     .expect(Selector("h1").withText("About Me").visible)
     .ok()
+    // Change password.
     .typeText(
       Selector(".form__input").withAttribute("name", "old_password"),
       "password",
@@ -71,6 +162,7 @@ test("Applicant Profile", async t => {
       ),
       "Password123!@#",
     )
+    // Edit social media / tagline.
     .selectText(
       Selector(".form__input").withAttribute("name", "twitter_username"),
     )
@@ -89,9 +181,26 @@ test("Applicant Profile", async t => {
       "I didn't do it.",
     )
     .click(Selector("button").withText("Save Changes"))
-    .expect(Selector("input").withAttribute("value", "I didn't do it.").exists)
-    .ok()
-    .click(Selector("a").withText("My Skills"))
-    .expect(Selector("h1").withText("My Skills").visible)
-    .ok()
+    // Relog to check password change.
+    .click(Selector("a").withText("Logout"))
+    .click(Selector("a").withText("Login"))
+    .typeText(Selector("#email"), "applicant@test.com")
+    .typeText(Selector("#password"), "Password123!@#")
+    .click(Selector("button").withText("Login"))
+    .expect(Selector("a").withText("My Applications").visible)
+    .ok();
 });
+
+/*
+ test("Register Applicant", async t => {
+  await t
+    .click(Selector("a").withText("Register"))
+    .typeText(Selector("#name"), "Test Cafe")
+    .typeText(Selector("#email"), "applicant@testcafe.com")
+    .typeText(Selector("#password"), "Password123!@#")
+    .typeText(Selector("#password-confirm"), "Password123!@#")
+    .click(Selector("button").withText("Register"))
+    .expect(Selector("a").withText("My Profile").visible)
+    .ok();
+});
+ */
