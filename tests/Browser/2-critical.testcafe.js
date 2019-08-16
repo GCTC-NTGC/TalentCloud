a6fd170d5859108b27eef9d92d328910bbb9a47b
Grant <grant.d.barnes@gmail.com>
2019-08-16 15:53:49 -0400

    add testcafe xunit reporter

--- tests/Browser/2-critical.testcafe.js
+++ tests/Browser/2-critical.testcafe.js
@@ -1,7 +1,7 @@
 import { Selector } from "testcafe";
 import { applicantUser, adminUser } from "./helpers/roles";
 
-fixture(`Critical - Applicant Profile`).page(`talent.test:8000`);
+fixture(`Critical - Applicant Profile`).page(`talent.test`);
 
 // Skip when writing new tests
 // fixture.skip(`Critical - Applicant Profile`);
@@ -310,7 +310,7 @@ function randomEmail() {
   return email;
 }
 
-fixture(`Critical - Registration`).page(`talent.test:8000`);
+fixture(`Critical - Registration`).page(`talent.test`);
 // Skip when writing new tests
 // fixture.skip(`Critical - Registration`);
 
