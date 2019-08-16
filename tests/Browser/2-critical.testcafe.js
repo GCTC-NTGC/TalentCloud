923abdaa6eabc1191ce92de52ff8a69f9baceb96
Grant <grant.d.barnes@gmail.com>
2019-08-16 15:42:38 -0400

    specify port for testcafe

--- tests/Browser/2-critical.testcafe.js
+++ tests/Browser/2-critical.testcafe.js
@@ -1,7 +1,7 @@
 import { Selector } from "testcafe";
 import { applicantUser, adminUser } from "./helpers/roles";
 
-fixture(`Critical - Applicant Profile`).page(`talent.test`);
+fixture(`Critical - Applicant Profile`).page(`talent.test:8000`);
 
 // Skip when writing new tests
 // fixture.skip(`Critical - Applicant Profile`);
@@ -310,7 +310,7 @@ function randomEmail() {
   return email;
 }
 
-fixture(`Critical - Registration`).page(`talent.test`);
+fixture(`Critical - Registration`).page(`talent.test:8000`);
 // Skip when writing new tests
 // fixture.skip(`Critical - Registration`);
 
