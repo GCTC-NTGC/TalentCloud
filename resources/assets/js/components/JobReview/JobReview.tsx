4726904190347a9e5773426ec830a85dcafcaf06
tristan-orourke <tristan.m.orourke@gmail.com>
2019-08-28 15:09:05 -0400

    Accommodate possibility of undefined manager translations from server

--- resources/assets/js/components/JobReview/JobReview.tsx
+++ resources/assets/js/components/JobReview/JobReview.tsx
@@ -274,6 +274,53 @@ const languageRequirementIcons = (
   }
 };
 
+const renderManagerSection = (
+  manager: Manager | null,
+  managerDeptName: string,
+  locale: "en" | "fr",
+): React.ReactElement => {
+  if (manager === null) {
+    return (
+      <p data-c-margin="bottom(normal)">
+        <FormattedMessage
+          id="jobBuilder.review.managerDataLoading"
+          defaultMessage="Manager data is loading..."
+          description="Placeholder text as Manager data loads."
+        />
+      </p>
+    );
+  }
+  const managerTranslation = manager[locale];
+  if (managerTranslation) {
+    return (
+      <>
+        <p data-c-margin="bottom(normal)">{manager.name}</p>
+        <p data-c-margin="bottom(normal)">
+          <FormattedMessage
+            id="jobBuilder.review.managerPosition"
+            defaultMessage="{position} at {department}"
+            description="Description of the Manager's position & department."
+            values={{
+              position: managerTranslation.position,
+              department: managerDeptName,
+            }}
+          />
+        </p>
+        <p>{managerTranslation.about_me}</p>
+      </>
+    );
+  }
+  return (
+    <p>
+      <FormattedMessage
+        id="jobBuilder.review.managerIncomplete"
+        defaultMessage="Please complete your manager profile."
+        description="Note that the Manager's profile is incomplete and should be edited before continuing."
+      />
+    </p>
+  );
+};
+
 interface JobReviewProps {
   job: Job;
   manager: Manager | null;
@@ -319,6 +366,7 @@ export const JobReview: React.FunctionComponent<
     return department !== null ? department[locale].name : "MISSING DEPARTMENT";
   };
   const departmentName = getDeptName(job.department_id);
+  const managerDeptName = manager ? getDeptName(manager.department_id) : "";
 
   // Map the skills into a dictionary for quicker access
   const skillsById = mapToObject(skills, getId);
@@ -624,25 +672,7 @@ export const JobReview: React.FunctionComponent<
           linkLabel={intl.formatMessage(messages.managerProfileLink)}
           link={managerEditProfile(locale)}
         >
-          {manager !== null ? (
-            <>
-              {/** TODO: Double check which fields to show for the manager section */}
-              <p data-c-margin="bottom(normal)">{manager.name}</p>
-              <p data-c-margin="bottom(normal)">
-                {manager[locale].position} at{" "}
-                {getDeptName(manager.department_id)}
-              </p>
-              <p>{manager[locale].about_me}</p>
-            </>
-          ) : (
-            <p data-c-margin="bottom(normal)">
-              <FormattedMessage
-                id="jobBuilder.review.managerDataLoading"
-                defaultMessage="Manager data is loading..."
-                description="Placeholder text as Manager data loads."
-              />
-            </p>
-          )}
+          {renderManagerSection(manager, managerDeptName, locale)}
         </JobReviewSection>
         <JobReviewSection
           title={intl.formatMessage(messages.workCultureHeading)}
