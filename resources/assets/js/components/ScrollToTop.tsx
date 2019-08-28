4cb0a890b3a3010bf157d5c6c64207fa8dc8d077
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-08-28 15:43:57 -0400

    Added scrollBehaviour option to ScrollToTop

--- resources/assets/js/components/ScrollToTop.tsx
+++ resources/assets/js/components/ScrollToTop.tsx
@@ -2,19 +2,29 @@ import React, { useEffect } from "react";
 
 interface ScrollToTopProps {
   /** The top position of element relative to the viewport.  */
-  top?: number;
+  offsetTop?: number;
+  /** Set the scroll behavior to 'auto'. Default = 'smooth' */
+  scrollBehaviorAuto?: boolean;
 }
 
 const ScrollToTop: React.FunctionComponent<ScrollToTopProps> = ({
-  top,
+  offsetTop,
+  scrollBehaviorAuto,
   children,
 }): React.ReactElement => {
   useEffect(() => {
+    const body: HTMLElement | null = document.querySelector("html");
+    // switch to auto scroll transition
+    body && scrollBehaviorAuto ? body.style.scrollBehavior = "auto" : null
+
     window.scrollTo(0, 0);
-    if (top) {
-      window.scrollBy(0, top);
+    if (offsetTop) {
+      window.scrollTo(0, offsetTop);
     }
-  }, [children, top]);
+
+    // switch back to smooth scrool transition
+    body && scrollBehaviorAuto ? body.style.scrollBehavior = 'smooth' : null
+  }, [children, offsetTop]);
   return <>{children}</>;
 };
 
