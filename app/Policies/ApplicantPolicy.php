08b2f8a8e56766e0d66104e3c0d6711796f686b8
Chris Wiseman <wiseman.christopher@gmail.com>
2019-09-05 14:11:24 -0300

    Clean up formatting and docblocks in ApplicantPolicy

--- app/Policies/ApplicantPolicy.php
+++ app/Policies/ApplicantPolicy.php
@@ -3,7 +3,6 @@
 namespace App\Policies;
 
 use App\Models\User;
-use App\Models\Manager;
 use App\Models\Applicant;
 use App\Models\JobPoster;
 use App\Policies\BasePolicy;
@@ -12,19 +11,20 @@ class ApplicantPolicy extends BasePolicy
 {
 
     /**
-     * Returns true if $manager owns a job to which $applicant has applied
-     * @param  Manager   $manager   [description]
-     * @param  Applicant $applicant [description]
-     * @return [type]               [description]
+     * Returns true if $user owns a job to which $applicant has applied.
+     *
+     * @param  \App\Models\User      $user      Generic User object for checking Manager relationship to Job Poster.
+     * @param  \App\Models\Applicant $applicant Applicant object used within applications submitted to Job Poster.
+     * @return boolean
      */
-    protected function ownsJobApplcantAppliedTo(User $user, Applicant $applicant)
+    protected function ownsJobApplicantAppliedTo(User $user, Applicant $applicant)
     {
         $applicant_id = $applicant->id;
-        $userId = $user->id;
+        $user_id = $user->id;
         return JobPoster::whereHas(
             'manager',
-            function ($q) use ($userId) {
-                $q->where('user_id', $userId);
+            function ($q) use ($user_id) {
+                $q->where('user_id', $user_id);
             }
         )->whereHas(
             'submitted_applications',
@@ -37,23 +37,23 @@ class ApplicantPolicy extends BasePolicy
     /**
      * Determine whether the user can view the applicant.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Applicant  $applicant
-     * @return mixed
+     * @param  \App\Models\User      $user      User object making the view request.
+     * @param  \App\Models\Applicant $applicant Applicant object to be viewed.
+     * @return boolean
      */
     public function view(User $user, Applicant $applicant)
     {
         $authApplicant =  $user->isApplicant() &&
             $applicant->user->is($user);
-        $authManager = $user->isManager() && $this->ownsJobApplcantAppliedTo($user, $applicant);
+        $authManager = $user->isManager() && $this->ownsJobApplicantAppliedTo($user, $applicant);
         return $authApplicant || $authManager;
     }
 
     /**
      * Determine whether the user can create applicants.
      *
-     * @param  \App\Models\User  $user
-     * @return mixed
+     * @param  \App\Models\User $user User object making the create request.
+     * @return boolean
      */
     public function create(User $user)
     {
@@ -63,9 +63,9 @@ class ApplicantPolicy extends BasePolicy
     /**
      * Determine whether the user can update the applicant.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Applicant  $applicant
-     * @return mixed
+     * @param  \App\Models\User      $user      User object making the update request.
+     * @param  \App\Models\Applicant $applicant Applicant object being updated.
+     * @return boolean
      */
     public function update(User $user, Applicant $applicant)
     {
@@ -76,9 +76,9 @@ class ApplicantPolicy extends BasePolicy
     /**
      * Determine whether the user can delete the applicant.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Applicant  $applicant
-     * @return mixed
+     * @param  \App\Models\User      $user      User object making the delete request.
+     * @param  \App\Models\Applicant $applicant Applicant object being deleted.
+     * @return void
      */
     public function delete(User $user, Applicant $applicant)
     {
@@ -87,9 +87,9 @@ class ApplicantPolicy extends BasePolicy
     /**
      * Determine whether the user can restore the applicant.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Applicant  $applicant
-     * @return mixed
+     * @param  \App\Models\User      $user      User object making the restore request.
+     * @param  \App\Models\Applicant $applicant Applicant object being restored.
+     * @return void
      */
     public function restore(User $user, Applicant $applicant)
     {
@@ -98,9 +98,9 @@ class ApplicantPolicy extends BasePolicy
     /**
      * Determine whether the user can permanently delete the applicant.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Applicant  $applicant
-     * @return mixed
+     * @param  \App\Models\User      $user      User object making the forceDelete request.
+     * @param  \App\Models\Applicant $applicant Applicant object being forceDeleted.
+     * @return void
      */
     public function forceDelete(User $user, Applicant $applicant)
     {
