7d0293a240caf25c3805126ae6b06a382d7deb38
tristan-orourke <tristan.m.orourke@gmail.com>
2019-09-05 14:41:07 -0400

    Include a missing dependency.

--- app/Http/Controllers/Api/JobApiController.php
+++ app/Http/Controllers/Api/JobApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 use App\Mail\JobPosterReviewRequested;
 use App\Http\Controllers\Controller;
