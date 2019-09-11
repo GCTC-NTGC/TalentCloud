8968dc27cfae123a7f1b190eb71c24832499446a
Chris Wiseman <wiseman.christopher@gmail.com>
2019-09-11 11:33:27 -0300

    Add modified date to footer view composer

--- app/Http/ViewComposers/FooterComposer.php
+++ app/Http/ViewComposers/FooterComposer.php
@@ -3,7 +3,10 @@
 namespace App\Http\ViewComposers;
 
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Lang;
+
+use Jenssegers\Date\Date;
 
 class FooterComposer
 {
@@ -15,6 +18,7 @@ class FooterComposer
      */
     public function compose(View $view)
     {
-        $view->with('footer', Lang::get('common/footer'));
+        $view->with('footer', Lang::get('common/footer'))
+            ->with('date_modified', humanizeDate(new Date('2019-09-12', Config::get('app.local_timezone'))));
     }
 }
