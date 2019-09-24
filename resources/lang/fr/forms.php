154a3277857c206366d189be70905b42f0e02b83
tristan-orourke <tristan.m.orourke@gmail.com>
2019-09-24 14:51:37 -0400

    Add french translations for form error copy

--- resources/lang/fr/forms.php
+++ resources/lang/fr/forms.php
@@ -10,4 +10,6 @@ return [
     */
     'required' => 'Champs obligatoires',
     'error' =>'Cette entrée a une erreur.',
+    'alert' => 'Euh oh, quelque chose ne va pas.',
+    'alert_copy' => 'S\'il vous plaît vérifier les points suivants:',
 ];
