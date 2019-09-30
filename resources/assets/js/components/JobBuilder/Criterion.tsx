693000ddbde4d0cdbddb13522da95f8d5f3704ef
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-09-30 10:51:42 -0400

    Replaced text with FormattedMessage

--- resources/assets/js/components/JobBuilder/Criterion.tsx
+++ resources/assets/js/components/JobBuilder/Criterion.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { InjectedIntlProps, injectIntl } from "react-intl";
+import { InjectedIntlProps, injectIntl, FormattedMessage } from "react-intl";
 import { Criteria, Skill } from "../../models/types";
 import { getSkillLevelName } from "../../models/jobUtil";
 import { CriteriaTypeId } from "../../models/lookupConstants";
@@ -17,13 +17,22 @@ export const Criterion: React.FunctionComponent<
     throw new Error("Unknown intl.locale");
   }
   return (
-    <div key={skill.id} data-c-margin="top(normal) bottom(double)">
+    <div
+      key={skill.id}
+      className="criterion-item"
+      data-c-margin="top(normal) bottom(double)"
+    >
       <p data-c-font-weight="bold" data-c-margin="bottom(half)">
         {skill[locale].name}
       </p>
       {criterion.criteria_type_id === CriteriaTypeId.Essential && (
         <p data-c-margin="bottom(half)">
-          Required Level:{" "}
+          <FormattedMessage
+            id="jobBuilder.skills.requiredSkill"
+            defaultMessage="Required Skill"
+            description="The text displayed before required skill level."
+          />
+          {`: `}
           {intl.formatMessage(getSkillLevelName(criterion, skill))}
         </p>
       )}
