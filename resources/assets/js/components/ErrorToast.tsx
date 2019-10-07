66157ae0fa02b8d9469b034dc7dd347f4d97d713
tristan-orourke <tristan.m.orourke@gmail.com>
2019-10-07 16:37:26 -0400

    Show ApiError response message in ErrorToast.

--- resources/assets/js/components/ErrorToast.tsx
+++ resources/assets/js/components/ErrorToast.tsx
@@ -1,14 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, FunctionComponent } from "react";
 import { connect } from "react-redux";
 import { injectIntl, FormattedMessage } from "react-intl";
+import { ApiError } from "redux-api-middleware";
 import { ErrorEntity } from "../store/Error/errorReducer";
 import { clearErrors } from "../store/Error/errorActions";
 import { getRecentError } from "../store/Error/errorSelector";
 import { RootState } from "../store/store";
 import { DispatchType } from "../configureStore";
 
-const ErrorToast = (props): React.ReactElement => {
-  const { error, dispatchClearErrors } = props;
+const constructErrorMessage = (error: ErrorEntity): string => {
+  if (error.error instanceof ApiError) {
+    // if error was of type redux-api-middleware/ApiError, we may be able to extract extra info from the response.
+    const apiError = error.error;
+    if (apiError.status && apiError.response && apiError.response.message) {
+      return `${apiError.status} - ${apiError.response.message}`;
+    }
+  }
+  return error.message;
+};
+interface ErrorToastProps {
+  error: ErrorEntity;
+  dispatchClearErrors: () => void;
+}
+const ErrorToast: FunctionComponent<ErrorToastProps> = ({
+  error,
+  dispatchClearErrors,
+}): React.ReactElement => {
   useEffect((): (() => void) => {
     const timer = setInterval((): void => {
       dispatchClearErrors();
@@ -18,6 +35,7 @@ const ErrorToast = (props): React.ReactElement => {
       clearInterval(timer);
     };
   }, [dispatchClearErrors, error]);
+
   return (
     <>
       {error !== undefined && (
@@ -34,7 +52,7 @@ const ErrorToast = (props): React.ReactElement => {
                 description="Title displayed on the Error Toast component."
               />
             </span>
-            <p>{error.message}</p>
+            <p>{constructErrorMessage(error)}</p>
           </div>
           <button
             type="button"
@@ -60,7 +78,9 @@ const mapStateToProps = (
   error: getRecentError(state),
 });
 
-const mapDispatchToProps = (dispatch: DispatchType): any => ({
+const mapDispatchToProps = (
+  dispatch: DispatchType,
+): { dispatchClearErrors: () => void } => ({
   dispatchClearErrors: (): void => {
     dispatch(clearErrors());
   },
