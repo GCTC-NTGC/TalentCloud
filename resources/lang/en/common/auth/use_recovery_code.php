2ff631c612e7c4bba649c6925548684c918e4d9b
tristan-orourke <tristan.m.orourke@gmail.com>
2019-10-22 13:06:57 -0400

    Improve recovery code copy.

--- resources/lang/en/common/auth/use_recovery_code.php
+++ resources/lang/en/common/auth/use_recovery_code.php
@@ -14,6 +14,7 @@ return [
     'return_title' => 'Return to the home page.',
     'return' => 'Return Home',
     'header' => 'Use a Recovery Code',
+    'instructions' => 'Enter a Recovery Code. Remember, each code can only be used once.',
     'form_label' => 'Recovery Code',
     'placeholder' =>  'e.g. 1234abcd-efgh5678',
     'submit' => 'Continue',
