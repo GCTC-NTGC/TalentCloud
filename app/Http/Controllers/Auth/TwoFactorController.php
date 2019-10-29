ef01f54fe2151bab77808542aab2e2cfae817c60
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-10-29 10:56:07 -0400

    New saveAndRedirect controller method to check if user already has 2FA setup before redirect.

--- app/Http/Controllers/Auth/TwoFactorController.php
+++ app/Http/Controllers/Auth/TwoFactorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use Facades\App\Services\WhichPortal;
+use Illuminate\Support\Facades\Session;
 
 class TwoFactorController extends AuthController
 {
@@ -32,7 +33,16 @@ class TwoFactorController extends AuthController
         $user->save();
         $user->refresh();
 
-        return redirect()->back();
+        $profile_url = '';
+        if (WhichPortal::isApplicantPortal()) {
+            $profile_url = route('profile');
+        } elseif (WhichPortal::isManagerPortal()) {
+            $profile_url = route('manager.profile');
+        } elseif (WhichPortal::isAdminPortal()) {
+            $profile_url = backpack_url('2fa');
+        }
+
+        return redirect($profile_url);
     }
 
     public function confirm(Request $request)
@@ -40,23 +50,31 @@ class TwoFactorController extends AuthController
         $user = $request->user();
         $secret = $request->input('secret');
         if (!empty($secret) && empty($user->google2fa_secret)) {
-            $user->google2fa_secret = $secret;
-            $user->save();
-            $user->refresh();
+            Session::put('url.secret', $secret);
         }
 
         return redirect(route('otp'));
     }
 
-    public function otp(Request $request)
+    public function saveAndRedirect(Request $request)
     {
-        $user = $request->user();
+        // If user has 2FA setup, and passes OTP validation then return to expected url.
         if (!empty($user->recovery_codes) && !empty($user->google2fa_secret)) {
             $expectedUrl = session()->get('url.expected');
             session()->remove('url.expected');
             return redirect($expectedUrl);
         }
 
+        // Check if secret is empty (first time setting up 2FA). If true then save to user.
+        $user = $request->user();
+        if (empty($user->google2fa_secret)) {
+            $user->google2fa_secret = session()->get('url.secret');
+            $user->save();
+            $user->refresh();
+            session()->remove('url.secret');
+        }
+
+        // Send first time 2FA users to receive recovery codes.
         $recovery_codes_url = '';
         if (WhichPortal::isApplicantPortal()) {
             $recovery_codes_url = route('recovery_codes.show');
