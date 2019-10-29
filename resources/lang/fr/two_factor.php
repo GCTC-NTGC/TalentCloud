c10171344a5ac78cf51750bbcbbeadf2080c57c7
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-10-29 10:58:32 -0400

    Updated two factor copy.

--- resources/lang/fr/two_factor.php
+++ resources/lang/fr/two_factor.php
@@ -12,11 +12,11 @@ return [
     */
 
     'title' => 'Activate Two Factor Authentication',
-    'return_title' => 'Return to the home page.',
-    'return' => 'Return to profile.',
+    'return' => 'Return to Profile',
     'header' => 'Activate Two Factor Authentication',
     'setup' => 'Set up your two factor application by scanning the QR code in a mobile app like Google Authenticator or Authy. Alternatively, you can use the code below.',
     'qr_alt' => 'QR Code image to use with authentication application.',
     'warning' => "You must set up your authentication app before continuing. You will be unable to log in otherwise.",
     'next_text' => 'Next',
+    'cancel_text' => 'Cancel',
 ];
