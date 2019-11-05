7b29c9725f69a2621e12a72b0066d0330a72ac84
Chris Wiseman <wiseman.christopher@gmail.com>
2019-11-05 14:46:15 -0400

    Update lifetime config value to one month

--- config/google2fa.php
+++ config/google2fa.php
@@ -15,7 +15,7 @@ return [
      * In case you need your users to be asked for a new one time passwords from time to time.
      */
 
-    'lifetime' => 0, // Don't prompt while signed in.
+    'lifetime' => 43800, // Don't prompt while signed in.
 
     /*
      * Renew lifetime at every new request.
