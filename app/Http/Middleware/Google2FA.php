d245dd189bb562b71d64c55ab517afa01b4f7250
Chris Wiseman <wiseman.christopher@gmail.com>
2019-11-05 14:47:01 -0400

    Add cookie to successful response and check for it during middleware

--- app/Http/Middleware/Google2FA.php
+++ app/Http/Middleware/Google2FA.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use PragmaRX\Google2FALaravel\Support\Authenticator;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\URL;
 
@@ -15,8 +16,12 @@ class Google2FA
     public function handle($request, Closure $next)
     {
         $authenticator = app(Authenticator::class)->boot($request);
+        $remember = $request->cookie('remember_device');
 
-        if ($authenticator->isAuthenticated()) {
+        if ($authenticator->isAuthenticated() || $remember === 'yes') {
+            if (!$authenticator->isAuthenticated()) {
+                Log::notice('User skipped OTP entry with known device.', ['id' => $request->user()->id]);
+            }
             return $next($request);
         }
         // Unlike \PragmaRX\Google2FALaravel\Middleware, set the intended url.
