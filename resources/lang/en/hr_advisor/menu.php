2af7e439eb2f6885251c78f627cf1618cff764a2
Grant <grant.d.barnes@gmail.com>
2019-11-07 19:57:08 -0500

    HR homepage and lang files

---
+++ resources/lang/en/hr_advisor/menu.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    /*
+     * --------------------------------------------------------------------------
+     * Menu Language Lines
+     * --------------------------------------------------------------------------
+     *
+     * The following language lines are used in the nav menu.
+     *
+     */
+    'items' => [
+        'home' => [
+            'name' => 'Home',
+        ],
+    ],
+    'register' => 'Register',
+    'login' => 'Login',
+    'logout' => 'Logout',
+    /* <title> tag content */
+    'home_title' => 'Talent Cloud | HR Portal: Home',
+    // Button labels.
+    'mobile_trigger_label' => 'Mobile Menu',
+    'modal_cancel_label' => 'Cancel',
+    'delete_label' => 'Delete',
+    'view_label' => 'View this application',
+    'edit_label' => 'Edit this application',
+];
---
+++ resources/lang/fr/hr_advisor/menu.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    /*
+     * --------------------------------------------------------------------------
+     * Menu Language Lines
+     * --------------------------------------------------------------------------
+     *
+     * The following language lines are used in the nav menu.
+     *
+     */
+    'items' => [
+        'home' => [
+            'name' => 'Accueil',
+        ],
+    ],
+    'register' => 'Inscription',
+    'login' => 'Ouvrir une session',
+    'logout' => 'Déconnexion',
+    /* <title> tag content */
+    'home_title' => 'Nuage de talents | Portail de RH : Accueil',
+    // Button labels.
+    'mobile_trigger_label' => 'Menu mobile',
+    'modal_cancel_label' => 'Annuler',
+    'delete_label' => 'Supprimer',
+    'view_label' => 'Voir cette application',
+    'edit_label' => 'Modifier cette application',
+];
