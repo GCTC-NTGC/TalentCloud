761af8f5aad02b34a75e015e8a6a24f5306c8d29
Grant <grant.d.barnes@gmail.com>
2019-11-18 17:46:39 -0500

    Add junit to jest / phpunit configs

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,6 @@ module.exports = {
   transformIgnorePatterns: [
     "/node_modules/(?!intl-messageformat|intl-messageformat-parser).+\\.js$",
   ],
+  // junit
+  reporters: ["default", "jest-junit"],
 };
