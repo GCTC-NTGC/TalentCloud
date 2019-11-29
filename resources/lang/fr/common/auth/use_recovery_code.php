dc9c304ae5db66f3d87919ed416ea518cc83bdbb
Grant Barnes <grant.d.barnes@gmail.com>
2019-11-28 20:06:30 -0500

    add ranslations

--- resources/lang/fr/common/auth/use_recovery_code.php
+++ resources/lang/fr/common/auth/use_recovery_code.php
@@ -1,24 +1,22 @@
 <?php
-
+/*
+|--------------------------------------------------------------------------
+| Use Recovery Code Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used by auth\use_recovery_code
+|
+*/
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Use Recovery Code Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used by auth\use_recovery_code
-    |
-    */
-
-    'title' => 'Use a Recovery Code',
-    'return_title' => 'One Time Password.',
-    'return' => 'Back to One Time Password',
-    'header' => 'Use a Recovery Code',
-    'instructions' => 'Enter a Recovery Code. Remember, each code can only be used once.',
-    'form_label' => 'Recovery Code',
-    'placeholder' =>  'e.g. 1234abcd-efgh5678',
-    'submit' => 'Login',
-    'incorrect_code' => 'Wrong code. Try again.',
-    'otp_link_title' => 'One Time Password',
-    'otp_link' => 'Log in with One Time Password.',
+    'title' => 'Utiliser un code de récupération',
+    'return_title' => 'Mot de passe à usage unique.',
+    'return' => 'Retour au mot de passe à usage unique',
+    'header' => 'Utiliser un code de récupération ',
+    'instructions' => 'Entrer un code de récupération. Souvenez-vous, chaque code ne peut être utilisé qu\'une fois.',
+    'form_label' => 'Code de récupération',
+    'placeholder' => 'p. ex. 1234abcd-efgh5678',
+    'submit' => 'Ouverture de session',
+    'incorrect_code' => 'Mauvais code. Essayer de nouveau.',
+    'otp_link_title' => 'Mot de passe à usage unique ',
+    'otp_link' => 'Ouvrir une session avec un mot de passe à usage unique.'
 ];
--- resources/lang/fr/errors.php
+++ resources/lang/fr/errors.php
@@ -1,14 +1,14 @@
 <?php
+/*
+|--------------------------------------------------------------------------
+| Error Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used by exception messages.
+|
+*/
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Error Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used by exception messages.
-    |
-    */
-    'title' => '',
-    'refresh_page' => '',
-    'two_factor_required' => ''
+    'title' => 'Erreur',
+    'refresh_page' => 'Veuillez actualiser la page.',
+    'two_factor_required' => 'Vous devez activer l\'authentification à deux facteurs pour accéder à cette ressource.'
 ];
