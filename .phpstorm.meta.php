f2b248375f2f37a1dac09008d7fa1076f775a43d
Grant Barnes <grant.d.barnes@gmail.com>
2019-12-03 13:21:49 -0500

    reload helpers

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-12-03 17:42:55.
+    * Generated on 2019-12-03 18:17:23.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
