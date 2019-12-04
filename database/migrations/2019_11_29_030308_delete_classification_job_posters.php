2488d92c298bcf6bc163817506a2480e9af5fb96
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-12-04 16:07:32 -0500

    Migration does not need to change 'classification_level'

--- database/migrations/2019_11_29_030308_delete_classification_job_posters.php
+++ database/migrations/2019_11_29_030308_delete_classification_job_posters.php
@@ -21,25 +21,21 @@ class DeleteClassificationJobPosters extends Migration
         // Reorder columns.
         Schema::table('job_posters', function (Blueprint $table) {
             $table->renameColumn('classification_id', 'id_old');
-            $table->renameColumn('classification_level', 'level_old');
             $table->dropForeign(['classification_id']);
         });
 
         Schema::table('job_posters', function (Blueprint $table) {
             $table->integer('classification_id')->nullable();
-            $table->integer('classification_level')->nullable();
             $table->foreign('classification_id')->references('id')->on('classifications')->onUpdate('CASCADE')->onDelete('NO ACTION');
         });
 
         DB::table('job_posters')->update([
             'classification_id' => DB::raw('id_old'),
-            'classification_level' => DB::raw('level_old')
         ]);
 
         Schema::table('job_posters', function (Blueprint $table) {
 
             $table->dropColumn('id_old');
-            $table->dropColumn('level_old');
         });
     }
 
