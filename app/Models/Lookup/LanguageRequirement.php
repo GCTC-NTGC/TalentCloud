6b40a6fbb5fad16315c2be318aad018ae5c16295
Grant Barnes <grant.d.barnes@gmail.com>
2019-12-05 15:11:06 -0500

    LanguageRequirement/Province

--- app/Models/Lookup/LanguageRequirement.php
+++ app/Models/Lookup/LanguageRequirement.php
@@ -1,14 +1,9 @@
 <?php
 
-/**
- * Created by Reliese Model.
- * Date: Thu, 12 Jul 2018 22:39:27 +0000.
- */
-
 namespace App\Models\Lookup;
 
 use App\Models\BaseModel;
-use Astrotomic\Translatable\Translatable as Translatable;
+use Spatie\Translatable\HasTranslations;
 
 /**
  * Class LanguageRequirement
@@ -19,26 +14,19 @@ use Astrotomic\Translatable\Translatable as Translatable;
  * @property \Jenssegers\Date\Date $updated_at
  *
  * @property \Illuminate\Database\Eloquent\Collection $job_posters
- * @property \Illuminate\Database\Eloquent\Collection $language_requirement_translations
  *
  * Localized Properties:
  * @property string $value
  */
 class LanguageRequirement extends BaseModel
 {
+    use HasTranslations;
 
-    use Translatable;
-
-    public $translatedAttributes = ['value'];
+    public $translatable = ['value'];
     protected $fillable = [];
 
     public function job_posters() //phpcs:ignore
     {
         return $this->hasMany(\App\Models\JobPoster::class);
-    }
-
-    public function language_requirement_translations() //phpcs:ignore
-    {
-        return $this->hasMany(\App\Models\Lookup\LanguageRequirementTranslation::class);
     }
 }
