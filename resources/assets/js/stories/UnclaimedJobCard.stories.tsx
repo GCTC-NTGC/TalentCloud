486f13b629b146871d6a2ea4a17bb3b48ee1adfc
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-12-05 20:25:19 -0500

    Get data from fixtures file for UnclaimedJob story

--- resources/assets/js/stories/UnclaimedJobCard.stories.tsx
+++ resources/assets/js/stories/UnclaimedJobCard.stories.tsx
@@ -4,6 +4,7 @@ import { text, select, array } from "@storybook/addon-knobs";
 import { withIntl } from "storybook-addon-intl";
 import UnclaimedJobCard from "../components/UnclaimedJobCard";
 import { JobStatus } from "../models/lookupConstants";
+import { unclaimedJobs } from "../components/HRPortal/fixtures";
 
 const stories = storiesOf("Components|Unclaimed Job Card", module).addDecorator(
   withIntl,
@@ -17,44 +18,6 @@ const statusOptions = {
   Published: JobStatus.Published,
   Review: JobStatus.Review,
 };
-
-const unclaimedJobs = [
-  {
-    title: "CS01 - Front-end Developer",
-    createdAt: "2019-MAY-02",
-    status: JobStatus.Draft,
-    hiringManagers: ["Rebecca Appleby"],
-    hrAdvisors: [],
-  },
-  {
-    title: "AS02 - Executive Assisstant",
-    createdAt: "2019-MAR-12",
-    status: JobStatus.Draft,
-    hiringManagers: ["Rebecca Appleby"],
-    hrAdvisors: ["Rebecca Appleby", "Jack Little"],
-  },
-  {
-    title: "ET03 - Business Analyst",
-    createdAt: "2019-DEC-02",
-    status: JobStatus.Review,
-    hiringManagers: ["Robin Browne"],
-    hrAdvisors: [],
-  },
-  {
-    title: "FG05 - Long-term Care Specialist",
-    createdAt: "2019-MAY-14",
-    status: JobStatus.Draft,
-    hiringManagers: ["Braeden McDoogal"],
-    hrAdvisors: ["Caitlyn Summers", "Jack Little"],
-  },
-  {
-    title: "CS03 - Digital Product Designer",
-    createdAt: "2019-JUL-24",
-    status: JobStatus.Review,
-    hiringManagers: ["Amelie Lachance"],
-    hrAdvisors: [],
-  },
-];
 
 stories
   .add(
