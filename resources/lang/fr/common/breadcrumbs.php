b64a904b223a64c8fa0726f245051ce4ed8de0cd
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-12-10 11:39:57 -0500

    Add translation files for breadcrumbs

---
+++ resources/lang/fr/common/breadcrumbs.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    /*
+     * --------------------------------------------------------------------------
+     * Breadcrumbs Language Lines
+     * --------------------------------------------------------------------------
+     *
+     * The following language lines are used in the breadcrumbs section in the header.
+     *
+     */
+
+    'hr' => [
+        'home' => 'Accueil',
+        'job-index' => 'Index des emplois'
+    ],
+    'manager' => [
+
+    ],
+    'applicant' => [
+
+    ],
+];
