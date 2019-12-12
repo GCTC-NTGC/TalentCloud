f802f067d443815a415bbff26d18bbf16ce903f7
Chris Wiseman <wiseman.christopher@gmail.com>
2019-12-12 14:47:01 -0400

    Refactor test to extend Illuminate's TestCase class, remove DB schema check

--- tests/Unit/CommentTest.php
+++ tests/Unit/CommentTest.php
@@ -7,27 +7,11 @@ use App\Models\JobPoster;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Schema;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class CommentTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
-
-    /**
-     * Check if the comments table has the correct columns.
-     *
-     * @return void
-     */
-    public function testCommentsDatabaseHasExpectedColumns()
-    {
-        $this->assertTrue(
-            Schema::hasColumns('comments', [
-                'id', 'job_poster_id', 'user_id', 'comment', 'location', 'type_id', 'created_at', 'updated_at'
-            ]),
-            1
-        );
-    }
 
     /**
      * Test one-to-many relationship between job poster and comments
@@ -39,7 +23,7 @@ class CommentTest extends TestCase
         $job_poster = factory(JobPoster::class)->create();
         $comment = factory(Comment::class)->make(['job_poster_id' => $job_poster->id]);
 
-        $this->assertInstanceOf(JobPoster::class, $comment->job_poster_id);
+        $this->assertInstanceOf(JobPoster::class, $comment->job_poster);
     }
 
     /**
