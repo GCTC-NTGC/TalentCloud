3ecd97b6ebc9a3b01bc810461c19f40ee975c44a
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-12-23 16:55:08 -0500

    Add CommentTypeId to lookupConstants

--- resources/assets/js/models/lookupConstants.ts
+++ resources/assets/js/models/lookupConstants.ts
@@ -117,6 +117,12 @@ export const ClassificationId = {
   AD: 13,
 };
 
+export const CommentTypeId = {
+  question: 1,
+  recommendation: 2,
+  requiredAction: 3,
+};
+
 export enum JobStatus {
   Approved = "Approved",
   Closed = "Closed",
