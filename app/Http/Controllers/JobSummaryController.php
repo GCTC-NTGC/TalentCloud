425800276ec07fd894674f668076a9b0b78b87de
Grant Barnes <grant.d.barnes@gmail.com>
2019-12-30 19:08:19 -0500

    template progress

--- app/Http/Controllers/JobSummaryController.php
+++ app/Http/Controllers/JobSummaryController.php
@@ -7,32 +7,23 @@ use App\Models\JobPoster;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Lang;
-use Illuminate\Support\Facades\Log;
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 class JobSummaryController extends Controller
 {
     /**
      * Display the specified job summary.
      *
-     * @param  \Illuminate\Http\Request $request   Incoming request object.
-     * @param  \App\Models\JobPoster    $jobPoster Job Poster object.
+     * @param  \Illuminate\Http\Request $request Incoming request object.
+     * @param  \App\Models\JobPoster $jobPoster Job Poster object.
      * @return \Illuminate\Http\Response
      */
     public function show(Request $request, JobPoster $jobPoster)
     {
-        $jobPoster->load([
-            'department',
-            'criteria.skill.skill_type',
-            'manager.team_culture',
-            'manager.work_environment'
-        ]);
-
         $user = Auth::user();
 
         $data = [
             // Localized strings.
-            'job_summary' => Lang::get('hr_advisor/job_summary'),
+            'summary' => Lang::get('hr_advisor/job_summary'),
             // Applicant data.
             'user' => $user,
             // Job Poster data.
