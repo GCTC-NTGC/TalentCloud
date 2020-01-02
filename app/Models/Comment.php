c76ea46ea97eac9e260422f5fbc06e7a6ec66326
tristan-orourke <tristan.m.orourke@gmail.com>
2020-01-02 10:05:25 -0500

    Added relationships between Comment and CommentType

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -15,6 +15,10 @@ use Illuminate\Database\Eloquent\Model;
  * @property \App\Models\Lookup\CommentType $type_id
  * @property \Jenssegers\Date\Date $created_at
  * @property \Jenssegers\Date\Date $updated_at
+ *
+ * @property \App\Models\JobPoster $job_poster
+ * @property \App\Models\User $user
+ * @property \App\Models\Lookup\CommentType $comment_type
  */
 
 class Comment extends Model
@@ -24,7 +28,7 @@ class Comment extends Model
      *
      * @var array
      */
-    protected $fillable = ['comment', 'location'];
+    protected $fillable = ['comment', 'location', 'type_id'];
 
     public function job_poster() // phpcs:ignore
     {
@@ -34,5 +38,10 @@ class Comment extends Model
     public function user() // phpcs:ignore
     {
         return $this->belongsTo(\App\Models\User::class);
+    }
+
+    public function comment_type() // phpcs:ignore
+    {
+        return $this->belongsTo(\App\Models\Lookup\CommentType::class, 'type_id');
     }
 }
