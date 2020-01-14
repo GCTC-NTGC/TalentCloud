4a343b7f89a5394d6c277a1eda07e6c1cfc19d58
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-01-14 16:57:25 -0500

    Add comment feed to summary page

---
+++ resources/assets/js/components/HRPortal/SummaryHrActivityFeed.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import RootContainer from "../RootContainer";
+import CommentForm from "../CommentForm";
+import ActivityFeed from "../ActivityFeed";
+
+interface SummaryHrActivityFeedProps {
+  jobId: number;
+}
+
+const SummaryHrActivityFeed: React.FunctionComponent<SummaryHrActivityFeedProps> = ({
+  jobId,
+}) => {
+  return (
+    <>
+      <CommentForm jobId={jobId} isHrAdviser location="job/review" />
+      <hr data-c-hr="thin(black)" data-c-margin="top(1) left(2) right(2)" />
+      <ActivityFeed jobId={jobId} isHrAdvisor />
+    </>
+  );
+};
+
+export default SummaryHrActivityFeed;
+
+const container = document.getElementById("summary-hr-activity-feed");
+if (container !== null) {
+  if ("jobId" in container.dataset) {
+    const jobId = Number(container.dataset.jobId as string);
+    ReactDOM.render(
+      <RootContainer>
+        <SummaryHrActivityFeed jobId={jobId} />
+      </RootContainer>,
+      container,
+    );
+  }
+}
