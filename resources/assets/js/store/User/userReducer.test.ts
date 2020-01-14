2803dddfeadb849e69100dc5e45ce8b32363299a
Chris Wiseman <wiseman.christopher@gmail.com>
2020-01-14 11:19:25 -0400

    Add test for single User endpoint

--- resources/assets/js/store/User/userReducer.test.ts
+++ resources/assets/js/store/User/userReducer.test.ts
@@ -1,10 +1,10 @@
 import { userReducer, initUserState } from "./userReducer";
-import { FETCH_ALL_USERS_SUCCEEDED } from "./userActions";
+import { FETCH_ALL_USERS_SUCCEEDED, FETCH_USER_SUCCEEDED } from "./userActions";
 import { User } from "../../models/types";
-import { fakeUsers } from "../../fakeData/fakeUsers";
+import { fakeUsers, fakeUser } from "../../fakeData/fakeUsers";
 
 describe("User Reducer tests", (): void => {
-  it("Sets adds users by id when GET_ALL_USERS_SUCCEEDED", (): void => {
+  it("Sets all users by id when GET_ALL_USERS_SUCCEEDED", (): void => {
     const initialState = {
       ...initUserState(),
     };
@@ -22,6 +22,26 @@ describe("User Reducer tests", (): void => {
         1: { ...fakeUsers()[0] },
         2: { ...fakeUsers()[1] },
         3: { ...fakeUsers()[2] },
+      },
+    };
+    expect(userReducer(initialState, getAction)).toEqual(expectState);
+  });
+  it("Sets a user by id when GET_USER_SUCCEEDED", (): void => {
+    const initialState = {
+      ...initUserState(),
+    };
+    const getAction: {
+      type: typeof FETCH_USER_SUCCEEDED;
+      payload: User;
+      meta: { id: number };
+    } = {
+      type: FETCH_USER_SUCCEEDED,
+      payload: fakeUser(),
+      meta: { id: 1 },
+    };
+    const expectState = {
+      usersById: {
+        1: { ...fakeUser() },
       },
     };
     expect(userReducer(initialState, getAction)).toEqual(expectState);
