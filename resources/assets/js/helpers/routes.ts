c5f1111b7031a7a974eac49e26db49084bfe2651
tristan-orourke <tristan.m.orourke@gmail.com>
2020-01-15 17:24:01 -0500

    Fix frontend application review api route

--- resources/assets/js/helpers/routes.ts
+++ resources/assets/js/helpers/routes.ts
@@ -36,7 +36,7 @@ export function applicationReviewUpdate(
   locale: string,
   applicationId: number,
 ): string {
-  return `/${locale}/api/applications/${applicationId}/review`;
+  return `/api/applications/${applicationId}/review`;
 }
 
 export function jobBuilderIntro(locale: string, jobId?: number): string {
