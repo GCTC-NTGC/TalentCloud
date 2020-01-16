003e03b6eaf212f8d23ad48cb1d8375eceffa789
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-01-16 11:20:41 -0500

    update breaking changes in sass config

--- webpack.mix.js
+++ webpack.mix.js
@@ -46,7 +46,9 @@ mix
   )
   .sass("resources/assets/sass/app.scss", "public/css", {
     implementation: sass,
-    includePaths: ["node_modules/@fortawesome/fontawesome-free/scss"],
+    sassOptions: {
+      includePaths: ["node_modules/@fortawesome/fontawesome-free/scss"],
+    },
   })
   .options({
     processCssUrls: false,
@@ -65,7 +67,9 @@ mix
   })
   .sass("resources/assets/sass/reliability.scss", "public/css", {
     implementation: sass,
-    includePaths: ["node_modules/@fortawesome/fontawesome-free/scss"],
+    sassOptions: {
+      includePaths: ["node_modules/@fortawesome/fontawesome-free/scss"],
+    },
   })
   .options({
     processCssUrls: false,
