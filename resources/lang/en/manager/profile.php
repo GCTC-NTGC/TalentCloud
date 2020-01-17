a071d2244aded6b583fbe12b5d6ed0463b04a1b8
tristan-orourke <tristan.m.orourke@gmail.com>
2020-01-17 15:02:17 -0500

    Delete unused sections of lang files

--- resources/lang/en/manager/profile.php
+++ resources/lang/en/manager/profile.php
@@ -71,38 +71,5 @@ return [
             'approach_learning_development' => 'My Approach to Employee Learning and Development',
             'save_leadership_style' => 'Save My Leadership Style'
         ],
-        'environment' => [
-            'work_environment' => 'Our Work Environment',
-            'things_to_know' => 'Things to Know',
-            'answer_english' => 'Answer (English)',
-            'answer_french' => 'Answer (Français)',
-            'telework_frequency' => 'How Often is Telework Allowed?',
-            'flexible_hour_frequency' => 'How Often Are Flexible Hours Allowed?',
-            'environment_photos' => 'Work Environment Photos',
-            'upload_image' => 'Upload an Image',
-            'alt_text_english' => 'Image Alt Text (English)',
-            'alt_text_french' => 'Image Alt Text (Français)',
-            'save_work_environment' => 'Save Our Work Environment',
-            'frequency' => [
-                'never' => 'Never',
-                'rarely' => 'Occasionally',
-                'sometimes' => 'Sometimes',
-                'often' => 'Frequently',
-                'always' => 'Always',
-            ]
-
-        ],
-        'culture' => [
-            'team_culture' => 'Our Team Culture',
-            'primary_information' => 'Primary Information',
-            'team_size' => 'Team Size',
-            'gc_directory_link' => 'GC Directory Link',
-            'operating_context' => 'Our Operating Context',
-            'answer_english' => 'Answer (English)',
-            'answer_french' => 'Answer (Français)',
-            'what_we_value' => 'What We Value',
-            'how_we_work' => 'How We Work',
-            'save_team_culture' => 'Save Our Team Culture'
-        ]
     ]
 ];
