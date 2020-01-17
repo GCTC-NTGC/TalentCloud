52212c509b00ab7063f3a7b5a6067e4dadd3eb88
tristan-orourke <tristan.m.orourke@gmail.com>
2020-01-17 10:37:54 -0500

    Have Job Status on summary page translated to french.

---
+++ resources/lang/en/common/lookup/job_status.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'draft' => 'Draft',
+    'in_review' => 'In Review',
+    'approved' => 'Approved',
+    'open' => 'Open',
+    'closed' => 'Closed',
+    'complete' => 'Complete'
+];
---
+++ resources/lang/fr/common/lookup/job_status.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'draft' => 'Ébauche',
+    'in_review' => 'En cours de révision',
+    'approved' => 'Approuvé',
+    'open' => 'Ouvert',
+    'closed' => 'Fermé',
+    'complete' => 'Complété'
+];
