582e8ba275077ac9e2aa792b5889cdcc5e3a1e55
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-01-21 15:04:47 -0500

    Create a ActivityFeed component

---
+++ resources/assets/js/components/ActivityFeed.tsx
@@ -0,0 +1,118 @@
+import * as React from "react";
+import { connect } from "react-redux";
+import { useIntl, FormattedMessage, MessageDescriptor } from "react-intl";
+import { RootState } from "../store/store";
+import { getComments } from "../store/Job/jobSelector";
+import { hasKey } from "../helpers/queries";
+import { LocationId } from "../models/lookupConstants";
+import CommentForm from "./CommentForm";
+import ActivityList from "./ActivityList";
+import Icon from "./Icon";
+
+interface ActivityFeedProps {
+  jobId: number;
+  isHrAdvisor: boolean;
+  generalLocation: string;
+  locationMessages: { [LocationId: string]: MessageDescriptor };
+  totalActivities: number;
+}
+
+const ActivityFeed: React.FunctionComponent<ActivityFeedProps> = ({
+  jobId,
+  isHrAdvisor,
+  generalLocation,
+  totalActivities,
+  locationMessages,
+}) => {
+  const intl = useIntl();
+  const locationOptions = Object.values(LocationId)
+    .filter(location => hasKey(locationMessages, location))
+    .map(location => ({
+      value: location,
+      label: intl.formatMessage(locationMessages[location]),
+    }));
+  return (
+    <section>
+      <div data-c-accordion-group>
+        <div data-c-accordion="" className="">
+          <button
+            aria-expanded="false"
+            data-c-accordion-trigger
+            tabIndex={0}
+            type="button"
+            data-c-background="c1(100)"
+            data-c-padding="all(1)"
+          >
+            <div>
+              <h3 data-c-font-size="h3" data-c-color="white">
+                <FormattedMessage
+                  id="activityfeed.review.header"
+                  defaultMessage="Review Your Job Poster {totalActivities}"
+                  description="The activity feed header."
+                  values={{
+                    totalActivities:
+                      totalActivities === 0 ? (
+                        <Icon
+                          icon="fa fa-spinner fa-spin"
+                          accessibleText="Number of activities is loading..."
+                          sematicIcon
+                        />
+                      ) : (
+                        `(${totalActivities})`
+                      ),
+                  }}
+                />
+              </h3>
+            </div>
+            <span data-c-visibility="invisible">
+              <FormattedMessage
+                id="activityfeed.review.accordionAccessibleLabel"
+                defaultMessage="Click to view..."
+                description="The accessibility text displayed on the activity feed accordion button."
+              />
+            </span>
+            <p
+              data-c-accordion-add=""
+              data-c-font-style="underline"
+              data-c-color="white"
+            >
+              <i className="fas fa-caret-up" />
+            </p>
+            <p
+              data-c-accordion-remove=""
+              data-c-font-style="underline"
+              data-c-color="white"
+            >
+              <i className="fas fa-caret-down" />
+            </p>
+          </button>
+          <div
+            aria-hidden="false"
+            data-c-accordion-content
+            data-c-background="grey(20)"
+            data-c-padding="all(1)"
+          >
+            <CommentForm
+              jobId={jobId}
+              isHrAdviser={isHrAdvisor}
+              location={generalLocation}
+              locationOptions={...locationOptions}
+            />
+            <hr data-c-hr="thin(black)" data-c-margin="top(1)" />
+            <ActivityList jobId={jobId} isHrAdvisor={isHrAdvisor} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const mapStateToProps = (
+  state: RootState,
+): {
+  totalActivities: number;
+} => ({
+  totalActivities: getComments(state).length,
+});
+
+export default connect(mapStateToProps, () => ({}))(ActivityFeed);
