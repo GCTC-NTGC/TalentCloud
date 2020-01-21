a49b479214785f8f74654a90e0776860958e3ca3
Grant <grant.d.barnes@gmail.com>
2020-01-21 09:34:10 -0500

    add azure storage oackage

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,6 +64,15 @@ return [
             'url' => env('AWS_URL'),
         ],
 
+        'azure' => [
+            'driver' => 'azure',
+            'name' => env('AZURE_STORAGE_NAME'),
+            'key' => env('AZURE_STORAGE_KEY'),
+            'container' => env('AZURE_STORAGE_CONTAINER'),
+            'url' => env('AZURE_STORAGE_URL'),
+            'prefix' => null,
+        ],
+
     ],
 
 ];
