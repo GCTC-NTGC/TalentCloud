45ddf154f99bd0e83a7aaf552678f61fd68b11f8
Chris Wiseman <wiseman.christopher@gmail.com>
2020-01-23 15:25:08 -0400

    Add initial tests for User API

---
+++ tests/Feature/UserApiControllerTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Applicant;
+use App\Models\HrAdvisor;
+use App\Models\JobApplication;
+use App\Models\JobPoster;
+use App\Models\Lookup\Department;
+use App\Models\Manager;
+use App\Models\User;
+
+class UserApiControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Anonymous Users can't view any User accounts.
+     *
+     * @return void
+     */
+    public function testGuestAccess(): void
+    {
+        $applicant = factory(Applicant::class)->create();
+        $response = $this->json('get', 'api/users');
+        $response->assertStatus(403);
+        $response->assertJson(['status' => 'unauthorized']);
+        $response = $this->json('get', 'api/users/' . $applicant->user->id);
+        $response->assertStatus(403);
+        $response->assertJson(['status' => 'unauthorized']);
+    }
+
+    /**
+     * Admin Users can view everyone except other Admins.
+     *
+     * @return void
+     */
+    public function testAdminAccess(): void
+    {
+        $randomUsers = factory(Applicant::class, 5)->create();
+
+        $transportCanada = Department::find(3); // Transport Canada.
+
+        $deptManager = factory(Manager::class)->state('upgraded')->create([
+            'department_id' => $transportCanada->id
+        ]);
+
+        $job = factory(JobPoster::class)->state('closed')->create(['manager_id' => $deptManager->id]);
+        $job->job_applications()->saveMany(factory(JobApplication::class, 5))->create([
+            'job_poster_id' => $job->id
+        ])->each(function ($application, $key) use ($randomUsers): void {
+            $application->applicant_id = $randomUsers[$key]->id;
+        });
+
+        $hrManager = factory(HrAdvisor::class)->create([
+            'department_id' => $transportCanada->id
+        ]);
+        $hrManager->claimed_jobs()->attach($job);
+
+        $admin = factory(User::class)->state('admin')->create();
+        $otherAdmin = factory(User::class)->state('admin')->create();
+
+        $response = $this->followingRedirects()
+            ->actingAs($admin)
+            ->json('get', 'api/users');
+        $response->assertOk();
+        $expected = [
+            'first_name' => $otherAdmin->first_name,
+            'last_name' => $otherAdmin->last_name,
+            'email' => $otherAdmin->email,
+        ];
+        $response->assertJsonMissing($expected);
+        $response->assertJsonFragment($admin->toArray());
+        $response->assertJsonFragment($hrManager->user->toArray());
+        $response->assertJsonFragment($deptManager->user->toArray());
+        foreach ($randomUsers as $user) {
+            $response->assertJsonFragment($user->user->toArray());
+        }
+    }
+}
