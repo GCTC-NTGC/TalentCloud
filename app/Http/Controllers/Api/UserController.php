26a310cccbfa499043894fa85b925049f7e5fd8e
Chris Wiseman <wiseman.christopher@gmail.com>
2020-01-23 12:11:31 -0400

    Add middleware and Gate to User API methods

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,9 +4,32 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Gate;
 
 class UserController extends Controller
 {
+    /**
+     * @var Response
+     */
+    protected $unauthorized;
+
+    /**
+     * Class constructor
+     */
+    public function __construct()
+    {
+        $this->unauthorized = response()->json(['status' => 'unauthorized'], 403);
+
+        // Only accept logged in Users.
+        $this->middleware(function ($request, $next) {
+            if ($request->user() === null) {
+                return $this->unauthorized;
+            }
+            return $next($request);
+        });
+    }
+
     /**
      * Return all users as an array
      *
@@ -14,8 +37,14 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::with(['applicant', 'manager'])->get();
-        return $users->toJson();
+        $users = User::with(['applicant', 'manager', 'hr_advisor'])->get();
+        $viewableUsers = $users->filter(function ($user) {
+            return Gate::allows('view-user', $user);
+        })->values();
+        if (empty($viewableUsers)) {
+            return $this->unauthorized;
+        }
+        return $viewableUsers->toJson();
     }
 
     /**
@@ -26,6 +55,9 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        return $user->toJson();
+        if (Gate::allows('view-user', $user)) {
+            return $user->toJson();
+        }
+        return $this->unauthorized;
     }
 }
