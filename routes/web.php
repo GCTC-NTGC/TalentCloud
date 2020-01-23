6b26f8058aedb11c9449b47cad258a1fcfc32824
Chris Wiseman <wiseman.christopher@gmail.com>
2020-01-23 12:09:53 -0400

    Shuffle routes to align with comments

--- routes/web.php
+++ routes/web.php
@@ -635,11 +635,11 @@ Route::group(
 Route::group(['prefix' => 'api'], function (): void {
     // Protected by a gate in the controller, instead of policy middleware.
     Route::get('jobs/{jobPoster}/assessment-plan', 'AssessmentPlanController@getForJob');
-    // Public, not protected by policy or gate.
-    Route::get('skills', 'Api\SkillController@index');
     Route::get('users', 'Api\UserController@index');
     Route::get('users/{user}', 'Api\UserController@show')
         ->where('user', '[0-9]+');
+    // Public, not protected by policy or gate.
+    Route::get('skills', 'Api\SkillController@index');
     Route::get('departments', 'Api\DepartmentController@index');
 
     // Resource Routes are protected by policies in controllers instead of middleware.
