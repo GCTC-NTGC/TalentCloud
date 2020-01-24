d6bfa5fbcec349643494377bdfa4ed59664dd071
Grant Barnes <grant.d.barnes@gmail.com>
2020-01-24 09:44:30 -0500

    coldfix for HR registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -211,7 +211,7 @@ class RegisterController extends AuthController
         $inGovernment = ($hrDepartment !== null);
         $user->not_in_gov = !$inGovernment;
         $user->gov_email = $inGovernment ? $data['gov_email'] : null;
-        $user->setRole('hr_advisor');
+        $user->setRole('basic');
 
         $user->save();
         $user->refresh();
