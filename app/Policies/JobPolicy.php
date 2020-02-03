ede50240bc698cbb5e9e740b907be8efeb14a03e
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-02-03 17:37:28 -0500

    Allow soft_published jobs to be viewable

--- app/Policies/JobPolicy.php
+++ app/Policies/JobPolicy.php
@@ -19,10 +19,12 @@ class JobPolicy extends BasePolicy
      */
     public function view(?User $user, JobPoster $jobPoster)
     {
-        // Anyone can view a published job
+        // Anyone can view a soft published or published job
         // Only the manager that created it can view an unpublished job
         // Hr Advisors can view all jobs.
-        return $jobPoster->status() == 'published' || $jobPoster->status() == 'closed' ||
+        return $jobPoster->status() == 'published' ||
+            $jobPoster->status() == 'closed' ||
+            $jobPoster->soft_published ||
             ($user &&
                 $user->isManager() &&
                 $jobPoster->manager->user_id == $user->id) ||
