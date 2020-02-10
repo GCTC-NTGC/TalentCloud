f74e6d185afd5846fb37710edf415bcaf93cfd24
Chris Wiseman <wiseman.christopher@gmail.com>
2020-02-10 16:30:33 -0400

    Remove extraneous data from API response

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Http\Resources\Applicant as ApplicantResource;
-use App\Http\Resources\Manager as ManagerResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class User extends JsonResource
@@ -16,10 +14,11 @@ class User extends JsonResource
      */
     public function toArray($request)
     {
-        return array_merge(parent::toArray($request), [
-            'applicant' => new ApplicantResource($this->whenLoaded('applicant')),
-            'manager' => new ManagerResource($this->whenLoaded('manager')),
-            'hr_advisor' => new JsonResource($this->whenLoaded('hr_advisor')),
-        ]);
+        $userArray = parent::toArray($request);
+        unset($userArray['applicant']);
+        unset($userArray['manager']);
+        unset($userArray['hr_advisor']);
+
+        return $userArray;
     }
 }
