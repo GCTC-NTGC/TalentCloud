6703bc170bd30a6fe0e759b337f63a05bc9c4b0b
Chris Wiseman <wiseman.christopher@gmail.com>
2020-02-13 17:25:53 -0400

    Add initial values to user role names

--- database/migrations/2020_02_11_155505_add_name_to_user_roles_table.php
+++ database/migrations/2020_02_11_155505_add_name_to_user_roles_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddNameToUserRolesTable extends Migration
@@ -13,9 +14,36 @@ class AddNameToUserRolesTable extends Migration
      */
     public function up()
     {
+        $roles = [
+            'basic' => json_encode([
+                'en' => 'Applicant',
+                'fr' => 'candidat'
+            ]),
+            'upgradedManager' => json_encode([
+                'en' => 'Manager',
+                'fr' => 'gestionnaire'
+            ]),
+            'admin' => json_encode([
+                'en' => 'Admin',
+                'fr' => 'Admin'
+            ]),
+            'hr_advisor' => json_encode([
+                'en' => 'HR Advisor',
+                'fr' => 'conseiller des RH'
+            ]),
+        ];
+
         Schema::table('user_roles', function (Blueprint $table) {
             $table->json('name')->nullable();
         });
+
+        foreach ($roles as $key => $name) {
+            DB::table('user_roles')
+                ->where('key', $key)
+                ->update([
+                    'name' => $name
+                ]);
+        }
     }
 
     /**
