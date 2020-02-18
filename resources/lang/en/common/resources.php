c28df2423f8c34f6bfd7d519a9c6862824917c8a
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-02-18 10:02:01 -0500

    Add lang files

---
+++ resources/lang/en/common/resources.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+     * --------------------------------------------------------------------------
+     * Resources
+     * --------------------------------------------------------------------------
+     *
+     * The following language lines are used when displaying resources page.
+     *
+     */
+
+    'title' => 'Manager & HR Resources',
+    'resources' => 'Resources',
+    'email_templates' => 'Email Templates',
+    'resources_text' => 'Here you can find a list of ever-growing resources for you to download that will help you leverage Talent Cloud more effectively.'
+];
