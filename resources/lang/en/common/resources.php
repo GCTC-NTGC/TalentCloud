b91d409ecbb89c818c4f7c65646f746aa8caad4a
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-02-20 17:06:50 -0500

    Add message when resources list is empty

--- resources/lang/en/common/resources.php
+++ resources/lang/en/common/resources.php
@@ -13,4 +13,5 @@ return [
     'resources' => 'Resources',
     'email_templates' => 'Email Templates',
     'resources_text' => 'Here you can find a list of resources for you to download that will help you leverage Talent Cloud more effectively.',
+    'empty_list' => 'No Resources.',
 ];
--- resources/lang/fr/common/resources.php
+++ resources/lang/fr/common/resources.php
@@ -13,4 +13,5 @@ return [
     'resources' => 'Ressources',
     'email_templates' => 'Modèles de courriel',
     'resources_text' => 'Vous trouverez ici une liste des ressources que vous pouvez télécharger afin de vous aider à miser sur le Nuage de talents de façon plus efficace.',
+    'empty_list' => 'Pas de Ressources.'
 ];
