9f8cdea2bcf89cbea82460f4162d1e2bbcf56679
Grant Barnes <grant.d.barnes@gmail.com>
2020-02-24 15:58:44 -0500

    remove preview button

--- app/Http/Controllers/Admin/ResourcesCrudController.php
+++ app/Http/Controllers/Admin/ResourcesCrudController.php
@@ -1,10 +1,13 @@
-<?php namespace App\Http\Controllers\Admin;
+<?php
+
+namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\ResourceCrudRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Illuminate\Support\Facades\App;
 
-class ResourcesCrudController extends CrudController {
+class ResourcesCrudController extends CrudController
+{
 
     use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
@@ -16,13 +19,12 @@ class ResourcesCrudController extends CrudController {
      * Setup for the Resource CRUD panel. Everything here is applied on ALL operations.
      *
      * @return void
-    */
+     */
     public function setup()
     {
-          $this->crud->setModel('App\Models\Resource');
-          $this->crud->setRoute('admin/resource');
-          $this->crud->setEntityNameStrings('resource', 'resources');
-          $this->crud->removeButton('preview');
+        $this->crud->setModel('App\Models\Resource');
+        $this->crud->setRoute('admin/resource');
+        $this->crud->setEntityNameStrings('resource', 'resources');
     }
 
     /**
@@ -32,6 +34,8 @@ class ResourcesCrudController extends CrudController {
      */
     protected function setupListOperation()
     {
+        $this->crud->removeButton('show');
+
         $locale = 'en';
         if (null !== $this->request->input('locale')) {
             $locale = $this->request->input('locale');
@@ -50,9 +54,9 @@ class ResourcesCrudController extends CrudController {
             'orderLogic' => function ($query, $column, $columnDirection) use ($locale) {
                 return $query->orderBy('name->' . $locale, $columnDirection)->select('*');
             },
-            'searchLogic' => function ($query, $column, $searchTerm) use ($locale) : void {
+            'searchLogic' => function ($query, $column, $searchTerm) use ($locale): void {
                 $query->orWhere('name->' . $locale, 'ilike', "%$searchTerm%");
-          },
+            },
         ]);
     }
 
@@ -66,22 +70,22 @@ class ResourcesCrudController extends CrudController {
         $this->crud->setValidation(ResourceCrudRequest::class);
 
         $this->crud->addField([
-          'name' => 'name',
-          'type' => 'text',
-          'label' => 'Title',
+            'name' => 'name',
+            'type' => 'text',
+            'label' => 'Title',
         ]);
 
         $this->crud->addField([
-          'name' => 'name',
-          'type' => 'text',
-          'label' => 'Title'
+            'name' => 'name',
+            'type' => 'text',
+            'label' => 'Title'
         ]);
 
         $this->crud->addField([
-          'name' => 'file',
-          'label' => 'File',
-          'type' => 'upload',
-          'upload' => true,
+            'name' => 'file',
+            'label' => 'File',
+            'type' => 'upload',
+            'upload' => true,
         ]);
     }
 
@@ -94,4 +98,4 @@ class ResourcesCrudController extends CrudController {
     {
         $this->setupCreateOperation();
     }
-  }
+}
