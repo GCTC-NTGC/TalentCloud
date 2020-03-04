20ea245a538a699b9674e5582eff8e2d93530fb0
Grant Barnes <grant.d.barnes@gmail.com>
2020-03-04 11:52:53 -0500

    Experience models and database migrations (#2981)
    * start experience migrations
    
    * add / update models and migrations
    
    * add indexes, fix renamed tables
    
    * lookup value is not redundant (Gray)
    
    * add known translations from /lang
    
    * add translations to lookup tables
    
    * singular ExperienceAward model
    
    * remove prefixes from experienceable
    
    * add changes following code review
    
    * final changes to nomenclature / pluralization
    
    * final tweaks to nomenclature
    
    * make end dates nullable (n/a if is_active = true)
    
    * Remove table names from arrays
    
    Co-authored-by: tristan-orourke <tristan.m.orourke@gmail.com>

---
+++ app/Models/ExperienceAward.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+/**
+ * Class ExperienceAward
+ *
+ * @property int $id
+ * @property string $title
+ * @property int $award_recipient_type_id
+ * @property string $issued_by
+ * @property int $award_recognition_type_id
+ * @property \Jenssegers\Date\Date $awarded_date
+ * @property int $experienceable_id
+ * @property string $experienceable_type
+ * @property \Jenssegers\Date\Date $created_at
+ * @property \Jenssegers\Date\Date $updated_at
+ *
+ * @property \App\Models\Applicant|\App\Models\JobApplication $experienceable
+ */
+class ExperienceAward extends BaseModel
+{
+    protected $casts = [
+        'title' => 'string',
+        'award_recipient_type_id' => 'int',
+        'issued_by' => 'string',
+        'award_recognition_type_id' => 'int',
+        'awarded_date' => 'date'
+    ];
+
+    protected $fillable = [
+        'title',
+        'award_recipient_type_id',
+        'issued_by',
+        'award_recognition_type_id',
+        'awarded_date'
+    ];
+
+    protected $table = 'experiences_award';
+
+    public function award_recipient_type() //phpcs:ignore
+    {
+        return $this->belongsTo(\App\Models\Lookup\AwardRecipientType::class);
+    }
+
+    public function award_recognition_type() //phpcs:ignore
+    {
+        return $this->belongsTo(\App\Models\Lookup\AwardRecognitionType::class);
+    }
+
+    public function experienceable() //phpcs:ignore
+    {
+        return $this->morphTo();
+    }
+}
---
+++ database/migrations/2020_02_21_233331_add_experience_education_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddExperienceEducationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiences_education', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('education_type_id')->unsigned();
+            $table->string('area_of_study');
+            $table->string('institution');
+            $table->integer('education_status_id')->unsigned();
+            $table->boolean('is_active')->default(false);
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->string('thesis_title')->nullable();
+            $table->boolean('has_blockcert')->nullable();
+            $table->integer('experienceable_id')->unsigned()->index();
+            $table->string('experienceable_type');
+            $table->timestamps();
+
+            $table->foreign('education_type_id')
+                ->references('id')
+                ->on('education_types')
+                ->onUpdate('CASCADE')
+                ->onDelete('NO ACTION');
+
+            $table->foreign('education_status_id')
+                ->references('id')
+                ->on('education_statuses')
+                ->onUpdate('CASCADE')
+                ->onDelete('NO ACTION');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiences_education');
+    }
+}
