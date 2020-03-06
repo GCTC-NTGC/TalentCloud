735392bc8b4d575e98dccdd88f5878df8cdb435b
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-03-06 13:29:55 -0500

    Extend InputProps in NumberInputProps

--- resources/assets/js/components/Form/NumberInput.tsx
+++ resources/assets/js/components/Form/NumberInput.tsx
@@ -1,25 +1,12 @@
 import * as React from "react";
 import { FieldProps } from "formik";
-import Input from "../Input";
+import Input, { InputProps } from "../Input";
 
-interface NumberInputProps {
-  /** HTML id of the input element */
-  id: string;
-  /** Holds text for label associated with input element */
-  label: string;
-  /** boolean indicating if input must have a value, or not */
-  required?: boolean;
-  /** Let's you specify example text that appears in input element when empty */
-  placeholder?: string;
-  /** Minimum value for the input */
-  min?: number;
-  /** Maximum value for the input */
-  max?: number;
-  /** data-clone-grid-item value: https://designwithclone.ca/#flexbox-grid */
-  grid?: string;
+interface NumberInputProps
+  extends Exclude<InputProps, "name" | "value" | "onChange" | "onBlur"> {
   /** Formik field prop of the shape { name, value, onChange, onBlur } */
   field: FieldProps["field"];
-  /** Formik form prop of the shape { errors, touched } */
+  /** Formik form prop of the shape { errors } */
   form: FieldProps["form"];
 }
 
