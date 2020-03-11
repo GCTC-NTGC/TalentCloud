e95c4ec4af4c65ea324298189854649723516bf8
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-03-11 15:39:41 -0400

    Remove comments from JobImpact story

--- resources/assets/js/stories/JobPosterBuilder/Impact.stories.tsx
+++ resources/assets/js/stories/JobPosterBuilder/Impact.stories.tsx
@@ -92,22 +92,3 @@ stories
       />
     ),
   );
-// .add(
-//   "Preview",
-//   (): React.ReactElement => (
-//     <JobImpactPreview
-//       deptImpact={text(
-//         "Department Impact",
-//         "The Navigable Waters Act Renewal team is responsible for the implementation of the electronic system related to the Canadian Navigable Waters Act (CNWA). This work will help the Government of Canada to modernize environment and regulatory processes and introduce new processes that properly serve the public.",
-//       )}
-//       teamImpact={text(
-//         "Team Impact",
-//         "Canada’s large network of navigable waters must remain open for Canadians to use. Protecting the public right of navigation is an important element of the new environmental and regulatory system in which good projects go ahead sustainably, with certainty and timely decisions, creating shared value and benefit for Canadians. The Navigable Waters Act Renewal (NWAR) team will play a key part in helping achieve this goal.",
-//       )}
-//       hireImpact={text(
-//         "Hire Impact",
-//         "As a member of a dynamic SCRUM team you’ll be responsible for implementing a new online public registry so Canadians can access information on proposed projects in their communities. You’ll also participate in the implementation of an internal system to manage the proposed work and will collaborate with other agencies involved in the CNWA.",
-//       )}
-//     />
-//   ),
-// );
