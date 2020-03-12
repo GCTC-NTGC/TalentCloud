325fca8fa386efe79eae2250271ad9f24a494911
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-03-12 10:00:41 -0400

    remove unused urls from lang file

--- resources/lang/fr/common/notifications/screen_candidates.php
+++ resources/lang/fr/common/notifications/screen_candidates.php
@@ -11,6 +11,4 @@ return [
     |*
  */
     'subject' => 'La période d’affichage de votre annonce de :position :classification vient de se terminer – Regardez les candidats maintenant.',
-    'manager_portal_link' => 'https://talent.test/fr/manager',
-    'position_link' => 'https://talent.test/fr/jobs/:id'
 ];
