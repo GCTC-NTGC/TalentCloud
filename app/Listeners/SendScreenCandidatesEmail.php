ab88716ff832ab632912c20690cdfe483d6e440a
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-03-12 14:17:20 -0400

    Compare the status id of the job poster

--- app/Listeners/SendScreenCandidatesEmail.php
+++ app/Listeners/SendScreenCandidatesEmail.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use App\Events\JobSaved;
 use App\Mail\ScreenCandidatesPrompt;
+use App\Models\Lookup\JobPosterStatus;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
@@ -28,8 +29,9 @@ class SendScreenCandidatesEmail
     {
         $job = $event->job;
 
+        $assessment = JobPosterStatus::where('key', 'assessment')->first()->id;
         // If the job poster status has changed to 'assessment' then send email
-        if ($job->isDirty('job_poster_status_id') && $job->job_poster_status->key === 'assessment') {
+        if ($job->isDirty('job_poster_status_id') && $job->job_poster_status_id === $assessment) {
             $manager_email = $job->manager->user->email;
             if (isset($manager_email)) {
                 Mail::to($manager_email)->send(new ScreenCandidatesPrompt($job));
