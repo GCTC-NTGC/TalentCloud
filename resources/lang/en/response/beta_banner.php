24bef5c9ea1589e47aa6d938dd49959565ed8711
yonikid15 <yoni.kid15@gmail.com>
2020-03-28 16:49:12 -0400

    Strategic Response - Add banner and menu to all pages (#3297)
    * Add banner to strategic response job posters
    
    * Allow response menu and banner to be used on other pages
    
    * Modify str job post header to be in same style as index page
    
    Co-authored-by: tristan-orourke <tristan.m.orourke@gmail.com>

---
+++ resources/lang/en/response/beta_banner.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Strategic Response: Beta Banner
+    |--------------------------------------------------------------------------
+    |
+    */
+    'beta_banner' => [
+        'message' => 'In our effort to get this site up and running quickly, we expect there to be a few glitches. ',
+        'need_help' => [
+            'text' => 'Need help? Get in touch.',
+            'title' => 'Click here to get support.',
+        ]
+    ],
+];
