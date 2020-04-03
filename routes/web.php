2d240d9a085185034261d2167cfec11b07330348
Grant Barnes <grant.d.barnes@gmail.com>
2020-04-03 10:11:52 -0400

    redirect /reserve to /response

--- routes/web.php
+++ routes/web.php
@@ -118,6 +118,11 @@ Route::group(
                 /* Response Home */
                 Route::get('response', 'StrategicResponseController@index')->name('response.index');
 
+                /* Reserve Redirect */
+                Route::get('reserve', function () {
+                    return redirect('response');
+                });
+
                 /* Require being logged in as applicant */
                 Route::middleware(['auth', 'role:applicant'])->group(function (): void {
 
