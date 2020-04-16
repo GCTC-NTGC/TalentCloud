fd2a3642b85c59227fe29dd5ed000857e25821cf
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-04-16 09:32:38 -0400

    Refactor breadcrumbs composer: add portal specific lang, replace segement ids with title

--- app/Http/ViewComposers/BreadcrumbsComposer.php
+++ app/Http/ViewComposers/BreadcrumbsComposer.php
@@ -38,15 +38,19 @@ class BreadcrumbsComposer
     {
         $segments = $this->parseSegments();
         $breadcrumbs_lang = Lang::get('common/breadcrumbs');
+        $portal_specific_lang = [];
+        $view->with('breadcrumbs_lang', $breadcrumbs_lang);
 
         if (WhichPortal::isManagerPortal()) {
             $segments = $segments->slice(1);
+            $portal_specific_lang = $breadcrumbs_lang['manager'];
         } elseif (WhichPortal::isHrPortal()) {
             $segments = $segments->slice(1);
+            $portal_specific_lang = $breadcrumbs_lang['hr'];
         }
 
         $view->with('breadcrumbs', $segments);
-        $view->with('breadcrumbs_lang', $breadcrumbs_lang);
+        $view->with('portal_specific_lang', $portal_specific_lang);
     }
 
     /**
@@ -57,8 +61,18 @@ class BreadcrumbsComposer
     protected function parseSegments()
     {
         return collect($this->request->segments())->mapWithKeys(function ($segment, $key) {
+            // Replaces any segment ID in url with the objects name or title.
             if ($this->request->jobPoster && $this->request->jobPoster->id == $segment) {
                 $segment = $this->request->jobPoster->title;
+            }
+            if ($this->request->manager && $this->request->manager->id == $segment) {
+                $segment = $this->request->manager->user->full_name;
+            }
+            if ($this->request->applicant && $this->request->applicant->id == $segment) {
+                $segment = $this->request->applicant->user->full_name;
+            }
+            if ($this->request->application && $this->request->application->id == $segment) {
+                $segment = $this->request->application->user_name;
             }
             return [
                 $segment => implode('/', array_slice($this->request->segments(), 0, $key + 1)),
