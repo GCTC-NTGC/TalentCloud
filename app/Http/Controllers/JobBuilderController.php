60a657dfaaa43fc58281cecc66f9f6e56acaaa3b
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-04-17 11:36:23 -0400

    Dynamically update job title breadcrumb on job poster builder

--- app/Http/Controllers/JobBuilderController.php
+++ app/Http/Controllers/JobBuilderController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\JobPoster;
+use Facades\App\Services\WhichPortal;
 
 class JobBuilderController extends Controller
 {
@@ -13,20 +14,27 @@ class JobBuilderController extends Controller
      * Show the Job Builder mini SPA
      * @return \Illuminate\Http\Response
      */
-    public function show()
+    public function show(JobPoster $jobPoster)
     {
+        $custom_breadcrumbs = [
+            'home' => route(WhichPortal::prefixRoute('home')),
+            'jobs' => route(WhichPortal::prefixRoute('jobs.index')),
+            'job-builder' => '',
+        ];
+
         return view(
             'manager/job-builder-root'
         )->with([
             'title' => Lang::get('manager/job_builder.title'),
+            'custom_breadcrumbs' => $custom_breadcrumbs
         ]);
     }
 
-    public function hrReview(JobPoster $job)
+    public function hrReview(JobPoster $jobPoster)
     {
         return view('hr_advisor/job_review', [
             'title' => Lang::get('hr_advisor/job_review.title'),
-            'job' => $job
+            'job' => $jobPoster
         ]);
     }
 }
--- resources/assets/js/helpers/router.tsx
+++ resources/assets/js/helpers/router.tsx
@@ -2,6 +2,13 @@ import { createBrowserHistory, Location } from "history";
 import UniversalRouter, { Routes } from "universal-router";
 import React, { useState, useEffect, useMemo, ReactElement } from "react";
 import { IntlShape, MessageDescriptor } from "react-intl";
+import { useStore, useSelector } from "react-redux";
+import { getSelectedJob } from "../store/Job/jobSelector";
+import { localizeField, getLocale } from "./localize";
+import { managerJobSummary } from "./routes";
+import { RootState } from "../store/store";
+import { Job } from "../models/types";
+import { messages } from "../components/HRPortal/JobIndexHrPage";
 
 const HISTORY = createBrowserHistory();
 
@@ -45,20 +52,69 @@ export const useRouter = (
   routes: Routes<any, RouterResult>,
   intl: IntlShape,
 ): React.ReactElement | null => {
+  const locale = getLocale(intl.locale);
   const location = useLocation();
   const router = useMemo(() => new UniversalRouter(routes), [routes]);
   const [component, setComponent] = useState<React.ReactElement | null>(null);
+  const job = useSelector((state: RootState) => getSelectedJob(state));
+
+  const jobBreadcrumbId = "job-title-breadcrumb";
+  const addJobBreadcrumb = (jobBreadcrumb: Job | null): void => {
+    const breadcrumbs: HTMLOListElement | null = document.querySelector(
+      "#jpb-breadcrumbs",
+    );
+    const breadcrumb: HTMLLIElement = document.createElement("li");
+    const anchor = document.createElement("a");
+    const icon = document.createElement("i");
+
+    if (jobBreadcrumb) {
+      anchor.id = jobBreadcrumbId;
+      anchor.href = managerJobSummary(locale, jobBreadcrumb.id);
+      anchor.innerText =
+        localizeField(locale, jobBreadcrumb, "title") ||
+        `{ ${intl.formatMessage(messages.titleMissing)} }`;
+    } else {
+      anchor.id = jobBreadcrumbId;
+      anchor.innerText = intl.formatMessage(messages.loadingManager);
+    }
+
+    icon.classList.add("fas", "fa-caret-right");
+    breadcrumb.append(anchor);
+    breadcrumb.append(icon);
+
+    if (breadcrumbs) {
+      const lastBreadcrumb = breadcrumbs.lastElementChild;
+      breadcrumbs.insertBefore(breadcrumb, lastBreadcrumb);
+    }
+  };
 
   // Render the result of routing
   useEffect((): void => {
     router.resolve(location.pathname).then((result): void => {
+      // Dynamically update the page title and header on step changes
       const title = intl.formatMessage(result.title);
       document.title = title;
       const h1 = document.querySelector("h1");
       if (h1) h1.innerHTML = title;
+
+      // Dynamically update the breadcrumbs on step changes
+      const jobBreadcrumb: HTMLAnchorElement | null = document.querySelector(
+        `#${jobBreadcrumbId}`,
+      );
+      if (job && jobBreadcrumb) {
+        jobBreadcrumb.href = managerJobSummary(locale, job.id);
+        jobBreadcrumb.innerText =
+          localizeField(locale, job, "title") ||
+          `{ ${intl.formatMessage(messages.titleMissing)} }`;
+      } else if (job) {
+        addJobBreadcrumb(job);
+      } else {
+        addJobBreadcrumb(null);
+      }
+
       setComponent(result.component);
     });
-  }, [intl, location, router]);
+  }, [intl, location, router, job]);
 
   return component;
 };
