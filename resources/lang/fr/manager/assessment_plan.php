7e06c92ae0ed56728199cb9072b197f345cc6bf6
Grant Barnes <grant.d.barnes@gmail.com>
2020-04-23 16:09:21 -0400

    fix manager / hr portal page titles

--- resources/lang/fr/manager/assessment_plan.php
+++ resources/lang/fr/manager/assessment_plan.php
@@ -9,4 +9,6 @@ return [
     */
 
     'title' => 'Concepteur de plans d’évaluation',
+    'hr_title' => 'Nuage de talents | Portail de conseillers en RH : Concepteur de plans d’évaluation',
+    'manager_title' => 'Nuage de talents | Gestionnaire : Concepteur de plans d’évaluation'
 ];
