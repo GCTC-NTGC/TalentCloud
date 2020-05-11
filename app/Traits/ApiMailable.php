a583fd64e34196c57ff0a5a99afdbca485959511
tristan-orourke <tristan.m.orourke@gmail.com>
2020-05-11 12:43:25 -0400

    Emulate the built-in mailable class in how mailables are built

--- app/Traits/ApiMailable.php
+++ app/Traits/ApiMailable.php
@@ -2,6 +2,8 @@
 
 namespace App\Traits;
 
+use Illuminate\Container\Container;
+
 /**
  * This trait can be used on a Mailable class to get the mailable's plain text,
  * or for converting to an array that might be returned in an API request.
@@ -10,21 +12,26 @@ trait ApiMailable
 {
     public function getPlainText()
     {
-        $this->build();
-        return $this->buildView()['text']->toHtml();
+        return $this->withLocale($this->locale, function () {
+            $this->build();
+            return $this->buildView()['text']->toHtml();
+        });
     }
 
     public function toArray()
     {
-        $this->build();
-        $mail = $this;
-        return [
-            'from' => $mail->from,
-            'to' => $mail->to,
-            'cc' => $mail->cc,
-            'bcc' => $mail->bcc,
-            'subject' => $mail->subject,
-            'body' => $mail->getPlainText(),
-        ];
+        return $this->withLocale($this->locale, function () {
+            Container::getInstance()->call([$this, 'build']);
+
+            $mail = $this;
+            return [
+                'from' => $mail->from,
+                'to' => $mail->to,
+                'cc' => $mail->cc,
+                'bcc' => $mail->bcc,
+                'subject' => $mail->subject,
+                'body' => $mail->getPlainText(),
+            ];
+        });
     }
 }
