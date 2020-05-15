988b52aca3bd09f97e70134966a2ff5625252d2f
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-05-15 11:49:10 -0400

    Add missing route parameter for view_job_application

--- resources/views/vendor/backpack/crud/buttons/view_job_application.blade.php
+++ resources/views/vendor/backpack/crud/buttons/view_job_application.blade.php
@@ -1,2 +1,2 @@
-<a href="{{ route('manager.applications.show', $entry->id) }} " target="_blank" class="btn btn-sm btn-link"><i class="fa fa-edit"></i>View Application</a>
+<a href="{{ route('manager.applications.show', [$entry->job_poster_id, $entry->id]) }}" target="_blank" class="btn btn-sm btn-link"><i class="fa fa-edit"></i>View Application</a>
 
