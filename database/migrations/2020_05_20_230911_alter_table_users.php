c2d4d7c0fa7a287de583f7dceaffacc41369ebbf
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-05-24 18:26:41 -0400

    Add new fields to users model: work_phone, personal_email, personal_phone

---
+++ database/migrations/2020_05_20_230911_alter_table_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('work_phone')->unique();
+            $table->string('personal_email')->unique()->nullable();
+            $table->string('personal_phone')->unique()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('work_phone');
+            $table->dropColumn('personal_email');
+            $table->dropColumn('personal_phone');
+        });
+    }
+}
