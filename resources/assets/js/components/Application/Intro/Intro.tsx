150eedeef10d7465a39256d6c3d2b761f6033b46
Chris Wiseman <wiseman.christopher@gmail.com>
2020-06-23 16:08:08 -0300

    Rename component file and language keys for consistency

--- resources/assets/js/components/Application/Intro/Intro.tsx
+++ resources/assets/js/components/Application/Intro/Intro.tsx
@@ -11,21 +11,21 @@ const ApplicationIntro: React.FC<IntroProps> = ({ handleStart }) => {
       <div data-c-container="medium">
         <h2 data-c-heading="h2" data-c-margin="top(3) bottom(1)">
           <FormattedMessage
-            id="applicationTimeline.intro.heading"
+            id="application.intro.heading"
             defaultMessage="Start Your Application"
             description="Heading text on the intro step of the Application Timeline."
           />
         </h2>
         <p data-c-margin="bottom(1)">
           <FormattedMessage
-            id="applicationTimeline.intro.preamble"
+            id="application.intro.preamble"
             defaultMessage="Before you begin we'd like to provide you with some information about what you can expect from our application process."
             description="First section of text on the intro step of the Application Timeline."
           />
         </p>
         <p data-c-margin="bottom(1)">
           <FormattedMessage
-            id="applicationTimeline.intro.firstParagraph"
+            id="application.intro.firstParagraph"
             defaultMessage="Talent Cloud is a skills-based hiring platform, and <bold>this means that we're going to ask you about your experiences in a way that differs from a traditional resume format</bold>. The goal is to get a thorough understanding of the skills you have and how your past experiences have shaped them. This gives the manager reviewing your application a much stronger sense of your abilities."
             description="Paragraph explaining how TC differs from a traditional resumé."
             values={{
@@ -38,14 +38,14 @@ const ApplicationIntro: React.FC<IntroProps> = ({ handleStart }) => {
         <ul data-c-margin="bottom(1)">
           <li>
             <FormattedMessage
-              id="applicationTimeline.intro.firstBullet"
+              id="application.intro.firstBullet"
               defaultMessage="We'll begin by asking you about your experiences, varying across academia, work, community, etc."
               description="Bullet point about the beginning of the application process."
             />
           </li>
           <li>
             <FormattedMessage
-              id="applicationTimeline.intro.secondBullet"
+              id="application.intro.secondBullet"
               defaultMessage="Once we have an understanding of your history, we'll ask you to explain how those experiences relate to the skills needed for this position."
               description="Bullet point about the end of the application process."
             />
@@ -53,7 +53,7 @@ const ApplicationIntro: React.FC<IntroProps> = ({ handleStart }) => {
         </ul>
         <p data-c-margin="bottom(1)">
           <FormattedMessage
-            id="applicationTimeline.intro.secondParagraph"
+            id="application.intro.secondParagraph"
             defaultMessage="During your very first application you'll be starting with a blank slate, but the great thing about Talent Cloud is that <bold>all of your information will be saved to your profile</bold>. This allows us to automatically fill in parts of your future applications, saving you a ton of time."
             description="Paragraph explaining the time saving of the application and profile."
             values={{
@@ -65,7 +65,7 @@ const ApplicationIntro: React.FC<IntroProps> = ({ handleStart }) => {
         </p>
         <p>
           <FormattedMessage
-            id="applicationTimeline.intro.prompt"
+            id="application.intro.prompt"
             defaultMessage="Ready to get started?"
             description="Ending text to motivate the applicant to progress."
           />
@@ -90,7 +90,7 @@ const ApplicationIntro: React.FC<IntroProps> = ({ handleStart }) => {
             >
               <span>
                 <FormattedMessage
-                  id="applicationTimeline.intro.start"
+                  id="application.intro.start"
                   defaultMessage="Start My Application"
                   description="Button text for starting an application."
                 />
--- resources/assets/js/stories/Application/Intro.stories.tsx
+++ resources/assets/js/stories/Application/Intro.stories.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { withIntl } from "storybook-addon-intl";
-import ApplicationIntro from "../../components/Application/Intro/ApplicationIntro";
+import ApplicationIntro from "../../components/Application/Intro/Intro";
 
 const stories = storiesOf("Application|Intro", module).addDecorator(withIntl);
 
