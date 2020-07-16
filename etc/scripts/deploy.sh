3fee2ddc9e0804088cb77f7b5179bed0a9b02f4c
tristan-orourke <tristan.m.orourke@gmail.com>
2020-07-16 12:32:55 -0400

    Create a base deploy script that requires args, and specific scripts that call it.

--- etc/scripts/deploy.sh
+++ etc/scripts/deploy.sh
@@ -1,54 +1,60 @@
 #!/bin/bash
 
-# Extract files
-echo "Setting permissions of TalentCloud.zip...";
-sudo chmod 777 TalentCloud.zip;
+SRC=$1
+APP_DIR=$2
 
-echo "Unzipping contents of TalentCloud.zip...";
-sudo unzip -qq TalentCloud.zip;
+# If SRC includes ".zip", remove it
+SRC=${SRC//[.]zip/}
+
+# Extract files
+echo "Setting permissions of $SRC.zip...";
+sudo chmod 777 $SRC.zip;
+
+echo "Unzipping contents of $SRC.zip...";
+sudo unzip -qq $SRC.zip;
 
 echo "Ensure storage/framework/cache/data directory exists...";
-mkdir -p TalentCloud/storage/framework/cache/data;
+sudo mkdir -p $SRC/storage/framework/cache/data;
 
 echo "Ensure file upload directory exists...";
-mkdir -p TalentCloud/storage/app/public/resources;
+sudo mkdir -p $SRC/storage/app/public/resources;
 
 # Set permissions
-echo "Setting proper app permissions in TalentCloud:"
+echo "Setting proper app permissions in $SRC:"
 
 echo "Setting directory permissions...";
-sudo find TalentCloud -type d -exec chmod 775 {} \;
+sudo find $SRC -type d -exec chmod 775 {} \;
 
 echo "Setting file permissions...";
-sudo find TalentCloud -type f -exec chmod 664 {} \;
+sudo find $SRC -type f -exec chmod 664 {} \;
 
 echo "Setting SELinux security context...";
-sudo chcon -Rt httpd_sys_content_t TalentCloud;
+sudo chcon -Rt httpd_sys_content_t $SRC;
 
 echo "Setting SELinux security context on writable directories...";
-sudo chcon -Rt httpd_sys_rw_content_t TalentCloud/storage;
+sudo chcon -Rt httpd_sys_rw_content_t $SRC/storage;
 
 echo "Setting nginx as onwer of all files...";
-sudo chown -Rf nginx:nginx TalentCloud;
+sudo chown -Rf nginx:nginx $SRC;
 
 # Backup and copy to /var/www
-echo "Updating app with contents of TalentCloud:"
+echo "Updating app with contents of $SRC:"
 
-echo "Copying TalentCloud to backup...";
-sudo cp -R TalentCloud "TalentCloud_Backup";
+echo "Copying $SRC to backup...";
+sudo cp -a $SRC "$SRC_Backup";
 
 echo "Deleting contents of app directory...";
-sudo rm -Rf /var/www/*;
+sudo rm -Rf $APP_DIR/*;
 
-echo "Moving contents of TalentCloud to app directory...";
-sudo mv TalentCloud/* /var/www/;
+echo "Moving contents of $SRC to app directory...";
+sudo mv $SRC/* $APP_DIR/;
 
-echo "Deleting empty TalentCloud...";
-sudo rm -R TalentCloud;
+echo "Deleting empty $SRC...";
+sudo rm -R $SRC;
 
 # Migrations and optimizing Laravel
 echo "Moving to app directory...";
-cd /var/www;
+cd $APP_DIR;
 
 echo "Executing Laravel commands...";
 sudo php artisan route:clear;
@@ -57,7 +63,6 @@ sudo php artisan config:clear;
 
 echo "Database migrations...";
 sudo php artisan migrate -n --force;
-
 echo "Restarting email queue...";
 sudo php artisan queue:restart;
 
