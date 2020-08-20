9d11658eba2ce893eda2e685460e92b1d1fce0ab
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-08-20 10:58:09 -0400

    Add story for FinalSubmit component

---
+++ resources/assets/js/stories/Application/FinalSubmit.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withIntl } from "storybook-addon-intl";
+import FinalSubmit from "../../components/Application/FinalSubmit/FinalSubmit";
+import { fakeApplication } from "../../fakeData/fakeApplications";
+
+const stories = storiesOf("Application|Final Submit", module).addDecorator(
+  withIntl,
+);
+
+stories.add(
+  "Intro Page",
+  (): React.ReactElement => (
+    <FinalSubmit
+      application={fakeApplication()}
+      handleQuit={action("Save and Quit")}
+      handleReturn={action("Save and Return")}
+      submitApplication={async (x) => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        action("Confirmed")(x);
+      }}
+    />
+  ),
+);
