618b2728395b2c3a0190f5d33bb9923016af37be
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-08-24 09:14:16 -0400

    Add a controller for timeline with 'complete' method

---
+++ app/Http/Controllers/ApplicationTimelineController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\JobApplication;
+use App\Models\JobPoster;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Lang;
+
+class ApplicationTimelineController extends Controller
+{
+    /**
+     * Show the application submission information.
+     *
+     * @param  \App\Models\JobPoster $jobPoster Incoming Job Poster object.
+     * @return \Illuminate\Http\Response
+     */
+    public function complete(/* JobPoster $jobPoster */)
+    {
+        // Dummy Data
+        $applicant = Auth::user()->applicant;
+        $jobPoster = JobPoster::where('job_poster_status_id', '10')->first();
+        $application = JobApplication::where('job_poster_id', $jobPoster->id)->first();
+
+        return view(
+            'applicant/application/10-congrats',
+            [
+              'applicant' => $applicant,
+              'application' => $application,
+              'application_template' => Lang::get(
+                  'applicant/application_template',
+                  ['security_clearance' => $jobPoster->security_clearance->value ]
+              ),
+              'jobPoster' => $jobPoster,
+            ]
+        );
+    }
+}
