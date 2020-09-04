42aed86a993bb529e44fcef772f71e9c67d8bdff
Grant Barnes <grant.d.barnes@gmail.com>
2020-09-04 13:34:45 -0400

    edit proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -10,30 +10,18 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
-     * @var array
+     * @var string|array
      */
-
-    // Trust all proxies:
-    // protected $proxies = ['*'];
 
     // For the AppSvc in the Azure environment.
     protected $proxies = [
         '13.71.170.130',
-        '13.88.230.232',
-        '13.88.229.172',
-        '52.228.39.78',
-        '13.71.191.27',
-        '40.85.254.37',
-        '40.85.219.45',
-        '40.85.223.56',
-        '52.228.42.60',
-        '52.228.42.28',
     ];
 
     /**
      * The headers that should be used to detect proxies.
      *
-     * @var string
+     * @var int
      */
     protected $headers = Request::HEADER_X_FORWARDED_ALL; // Request::HEADER_FORWARDED
 }
