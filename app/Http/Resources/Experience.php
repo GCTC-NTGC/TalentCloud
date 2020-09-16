af7bb940ccade924085596baffd53d032891b21b
tristan-orourke <tristan.m.orourke@gmail.com>
2020-09-16 16:23:39 -0400

    Add text for Experience constants directly to API response, for simplicity on the frontend.

--- app/Http/Resources/Experience.php
+++ app/Http/Resources/Experience.php
@@ -14,9 +14,26 @@ class Experience extends JsonResource
      */
     public function toArray($request)
     {
-        return array_merge(parent::toArray($request), [
-            'type' => $this->experienceTypeName(),
-            'experience_skills' => JsonResource::collection($this->whenLoaded('experience_skills')),
-        ]);
+        $type = $this->experienceTypeName();
+        $translations = [];
+        if ($type === 'experience_education') {
+            $translations = [
+                'education_status' => $this->education_status->getTranslations('name'),
+                'eduation_type' => $this->education_type->getTranslations('name'),
+            ];
+        } elseif ($type === 'experience_award') {
+            $translations = [
+                'award_recipient_type' => $this->award_recipient_type->getTranslations('name'),
+                'award_recognition_type' => $this->award_recognition_type->getTranslations('name'),
+            ];
+        }
+        return array_merge(
+            parent::toArray($request),
+            $translations,
+            [
+                'type' => $type,
+                'experience_skills' => JsonResource::collection($this->whenLoaded('experience_skills')),
+            ]
+        );
     }
 }
