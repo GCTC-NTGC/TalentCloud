e2881934029fa22d73810006455cfdcb7355214e
tristan-orourke <tristan.m.orourke@gmail.com>
2020-09-17 15:08:26 -0400

    Creat snapshot test of Job Work Env ahead of trying to replace CheckboxGroup

---
+++ resources/assets/js/components/JobBuilder/WorkEnv/JobWorkEnv.test.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { shallow } from "enzyme";
+import JobWorkEnv from "./JobWorkEnv";
+import fakeJob from "../../../fakeData/fakeJob";
+import RootContainer from "../../RootContainer";
+
+const handleSubmit = async (values) => {
+  console.log("Handle Submit");
+  return values;
+};
+
+const action = (text: string) => (): void => {
+  console.log(text);
+};
+
+const promiseAction = (text: string) => async (): Promise<void> => {
+  console.log(text);
+};
+
+describe("Job Index Page", (): void => {
+  it("should render JobWorkEnv correctly", (): void => {
+    const wrapper = shallow(
+      <RootContainer>
+        <JobWorkEnv
+          job={fakeJob()}
+          handleSubmit={handleSubmit}
+          handleReturn={action("Save & Return")}
+          handleModalConfirm={action("Confirm")}
+          handleModalCancel={action("Cancel")}
+          jobIsComplete
+          handleSkipToReview={promiseAction("Skip to Review")}
+        />
+      </RootContainer>,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render JobWorkEnv without any a job correctly", (): void => {
+    const wrapper = shallow(
+      <RootContainer>
+        <JobWorkEnv
+          job={null}
+          handleSubmit={handleSubmit}
+          handleReturn={action("Save & Return")}
+          handleModalConfirm={action("Confirm")}
+          handleModalCancel={action("Cancel")}
+          jobIsComplete={false}
+          handleSkipToReview={promiseAction("Skip to Review")}
+        />
+      </RootContainer>,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
