7618d579ba57d73a1e5f2826b6d6da7dc79b566d
tristan-orourke <tristan.m.orourke@gmail.com>
2020-09-21 11:52:56 -0400

    Modify skillsHelpers test to account for COMPLETE and ERROR states set at initialization

--- resources/assets/js/components/Application/Skills/skillsHelpers.test.ts
+++ resources/assets/js/components/Application/Skills/skillsHelpers.test.ts
@@ -61,27 +61,27 @@ describe("Skills Helpers tests", (): void => {
           experiences: {
             [`${fakeExperienceEducation().type}_${
               fakeExperienceEducation().id
-            }`]: IconStatus.DEFAULT,
+            }`]: IconStatus.COMPLETE,
             [`${fakeExperienceWork().type}_${
               fakeExperienceWork().id
-            }`]: IconStatus.DEFAULT,
+            }`]: IconStatus.COMPLETE,
           },
         },
         [fakeSkills()[3].id]: {
           experiences: {
             [`${fakeExperiencePersonal().type}_${
               fakeExperiencePersonal().id
-            }`]: IconStatus.DEFAULT,
+            }`]: IconStatus.ERROR,
             [`${fakeExperienceCommunity().type}_${
               fakeExperienceCommunity().id
-            }`]: IconStatus.DEFAULT,
+            }`]: IconStatus.ERROR,
           },
         },
         [fakeSkills()[2].id]: {
           experiences: {
             [`${fakeExperienceAward().type}_${
               fakeExperienceAward().id
-            }`]: IconStatus.DEFAULT,
+            }`]: IconStatus.ERROR,
           },
         },
       };
