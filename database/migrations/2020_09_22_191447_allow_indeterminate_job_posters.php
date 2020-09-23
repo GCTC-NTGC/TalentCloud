226ae5e0729aff4e31579058072e1b107521c580
Jayson McIntosh <jayson.r.mcintosh@gmail.com>
2020-09-23 13:32:18 -0400

    adding column indeterminate and dropping on rollback

--- database/migrations/2020_09_22_191447_allow_indeterminate_job_posters.php
+++ database/migrations/2020_09_22_191447_allow_indeterminate_job_posters.php
@@ -14,7 +14,7 @@ class AllowIndeterminateJobPosters extends Migration
     public function up()
     {
         Schema::table('departments', function (Blueprint $table) {
-            //
+            $table->boolean('indeterminate')->default(false);;
         });
     }
 
@@ -26,7 +26,7 @@ class AllowIndeterminateJobPosters extends Migration
     public function down()
     {
         Schema::table('departments', function (Blueprint $table) {
-            //
+            $table->dropColumn('indeterminate');
         });
     }
 }
