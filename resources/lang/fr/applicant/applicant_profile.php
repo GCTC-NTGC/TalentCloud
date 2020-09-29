9bd08684a3d56a5ea2d095b6e41003e46134f08b
Jayson McIntosh <jayson.r.mcintosh@gmail.com>
2020-09-29 08:55:27 -0400

    Adding in attempted french translations

--- resources/lang/fr/applicant/applicant_profile.php
+++ resources/lang/fr/applicant/applicant_profile.php
@@ -15,9 +15,9 @@ return [
     'account_settings_label' => 'Paramètres du compte',
     'required_label' => 'Requis',
     'input_error' => 'Cette entrée contient une erreur.',
-    'page_going_away' => '',
-    'page_going_away_desc' => '',
-    'page_going_away_read_more' => '',
+    'page_going_away' => 'Cette page disparaît.',
+    'page_going_away_desc' => 'Cette page disparaît. Nous avons beaucoup appris au cours de notre première année d\'activité. Pour aider les gestionnaires à mieux comprendre que votre expérience correspond aux emplois auxquels vous postulez, cette section est en cours de remplacement.',
+    'page_going_away_read_more' => 'En savoir plus sur ce changement ici.',
     'about_section' => [
         'about_title' => 'Mes renseignements',
         'profile_title' => 'Nuage de talents | Candidat : Profil - À propos de moi',
