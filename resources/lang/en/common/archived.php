4b7ada3122b188b43b66d60e7c550483dc364374
Jayson McIntosh <jayson.r.mcintosh@gmail.com>
2020-09-29 10:56:26 -0400

    re-adding reuseable archived text

--- resources/lang/en/common/archived.php
+++ resources/lang/en/common/archived.php
@@ -1,14 +1,14 @@
 <?php
 /*
 | --------------------------------------------------------------------------
-| Applicant Archived Notification Lines
+| Applicant Profile Language Lines
 | --------------------------------------------------------------------------
 |
-| The following language lines are used in the notification that content has been archived.
+| The following language lines are used in the applicant profile.
 |
 */
 return [
     'page_going_away' => 'This page is going away',
     'page_going_away_desc' => 'We\'ve learned a lot in our first year of operation. To help managers better understand your experience fits with the jobs you are applying to, this section is being replaced.',
-    'page_going_away_read_more' => 'Read more about this change here'
+    'page_going_away_read_more' => 'Read more about this change here.'
 ];
--- resources/lang/fr/common/archived.php
+++ resources/lang/fr/common/archived.php
@@ -1,14 +1,14 @@
 <?php
 /*
 | --------------------------------------------------------------------------
-| Applicant Archived Notification Lines
+| Applicant Profile Language Lines
 | --------------------------------------------------------------------------
 |
-| The following language lines are used in the notification that content has been archived.
+| The following language lines are used in the applicant profile.
 |
 */
 return [
-    'page_going_away' => '',
-    'page_going_away_desc' => '',
-    'page_going_away_read_more' => ''
+    'page_going_away' => 'Cette page disparaît.',
+    'page_going_away_desc' => 'Cette page disparaît. Nous avons beaucoup appris au cours de notre première année d\'activité. Pour aider les gestionnaires à mieux comprendre que votre expérience correspond aux emplois auxquels vous postulez, cette section est en cours de remplacement.',
+    'page_going_away_read_more' => 'En savoir plus sur ce changement ici.'
 ];
