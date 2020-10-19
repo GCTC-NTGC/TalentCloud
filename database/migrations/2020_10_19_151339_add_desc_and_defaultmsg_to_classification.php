1f392a985446a2df398c2e36096cdc6a65ec1220
Jayson McIntosh <jayson.r.mcintosh@gmail.com>
2020-10-19 11:44:26 -0400

    Updating classification tables to have data that was stored in the classificationCodes

--- database/migrations/2020_10_19_151339_add_desc_and_defaultmsg_to_classification.php
+++ database/migrations/2020_10_19_151339_add_desc_and_defaultmsg_to_classification.php
@@ -14,8 +14,64 @@ class AddDescAndDefaultmsgToClassification extends Migration
     public function up()
     {
         Schema::table('classifications', function (Blueprint $table) {
-            $table->string('defaultMessage');
-            $table->string('description');
+            $table->string('defaultMessage')->nullable();;
+            #$table->string('defaultMessage')
+
+            $table->string('description')->nullable();
+            #$table->string('description')->nullable();
+        });
+
+        DB::table('classifications')->where('key', 'AS')->update(
+            ['defaultMessage' => 'AS - Administrative Services', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        DB::table('classifications')->where('key', 'BI')->update(
+            ['defaultMessage' => 'BI - Biological Sciences', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        DB::table('classifications')->where('key', 'CO')->update(
+            ['defaultMessage' => 'CO - Commerce', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        DB::table('classifications')->where('key', 'CR')->update(
+            ['defaultMessage' => 'CR - Clerical and Regulatory', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        DB::table('classifications')->where('key', 'CS')->update(
+            ['defaultMessage' => 'CS - Computer Systems', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        DB::table('classifications')->where('key', 'EC')->update(
+            ['defaultMessage' => 'EC - Economics and Social Science Services', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        DB::table('classifications')->where('key', 'EX')->update(
+            ['defaultMessage' => 'EX - Executive', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        DB::table('classifications')->where('key', 'FO')->update(
+            ['defaultMessage' => 'FO - Forestry', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        DB::table('classifications')->where('key', 'IS')->update(
+            ['defaultMessage' => 'IS - Information Services', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        DB::table('classifications')->where('key', 'PC')->update(
+            ['defaultMessage' => 'PC - Physical Sciences', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        DB::table('classifications')->where('key', 'PE')->update(
+            ['defaultMessage' => 'PE - Personnel Administration', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        DB::table('classifications')->where('key', 'PM')->update(
+            ['defaultMessage' => 'PM - Programme Administration', 'description' => 'Job Classification from list of Classifications']
+        );
+
+        Schema::table('classifications', function (Blueprint $table) {
+            $table->string('defaultMessage')->nullable(false)->change();
+            $table->string('description')->nullable(false)->change();
         });
     }
 
