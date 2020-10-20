5c283f2ee04492211e86db6a3349637724a93dfe
Grant Barnes <grant.d.barnes@gmail.com>
2020-10-20 15:07:27 -0400

    edit backpack user / aliases

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -55,26 +55,26 @@ return [
     'breadcrumbs' => true,
     // Horizontal navbar classes. Helps make the admin panel look similar to your project's design.
     'header_class' => 'app-header bg-danger border-0 navbar navbar-color',
-        // For background colors use: bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
-        // For links to be visible on different background colors use: "navbar-dark", "navbar-light", "navbar-color"
+    // For background colors use: bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
+    // For links to be visible on different background colors use: "navbar-dark", "navbar-light", "navbar-color"
     // ----
     // BODY
     // ----
     // Body element classes.
     'body_class' => 'app aside-menu-fixed sidebar-lg-show',
-        // Try sidebar-hidden, sidebar-fixed, sidebar-compact, sidebar-lg-show
+    // Try sidebar-hidden, sidebar-fixed, sidebar-compact, sidebar-lg-show
     // Sidebar element classes.
     'sidebar_class' => 'sidebar sidebar-pills bg-light',
-        // Remove "sidebar-transparent" for standard sidebar look
-        // Try "sidebar-light" or "sidebar-dark" for dark/light links
-        // You can also add a background class like bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan
+    // Remove "sidebar-transparent" for standard sidebar look
+    // Try "sidebar-light" or "sidebar-dark" for dark/light links
+    // You can also add a background class like bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan
     // ------
     // FOOTER
     // ------
     // Footer element classes.
     'footer_class' => 'app-footer',
-        // hide it with d-none
-        // change background color with bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
+    // hide it with d-none
+    // change background color with bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
     // Developer or company name. Shown in footer.
     'developer_name' => 'Talent Cloud Home',
     // Developer website. Link in footer. Type false if you want to hide it.
@@ -102,7 +102,7 @@ return [
         // 'https://unpkg.com/react-dom@16/umd/react-dom.production.min.js',
     ],
     // JS files that are loaded in all pages, using Laravel's mix() helper
-    'mix_scripts' => [// file_path => manifest_directory_path
+    'mix_scripts' => [ // file_path => manifest_directory_path
         // 'js/app.js' => '',
     ],
     // -------------
@@ -157,7 +157,7 @@ return [
     */
 
     // Fully qualified namespace of the User model
-    'user_model_fqn' => App\Models\User::class,
+    'user_model_fqn' => App\User::class,
 
     // The classes for the middleware to check if the visitor is an admin
     // Can be a single class or an array of clases
