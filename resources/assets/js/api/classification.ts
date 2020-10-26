fbd57da657d2e34ae334e0b29e203501a46b1979
Jayson McIntosh <jayson.r.mcintosh@gmail.com>
2020-10-26 08:39:45 -0400

    Updating classification to use api v2

--- resources/assets/js/api/classification.ts
+++ resources/assets/js/api/classification.ts
@@ -4,4 +4,4 @@ import { Classification } from "../models/types";
 // TODO: verify schema
 export const parseClassificationsResponse = (data: any): Classification[] => data;
 
-export const getClassificationsEndpoint = (): string => `${baseUrl()}/classifications`;
+export const getClassificationsEndpoint = (): string => `${baseUrl(2)}/classifications`;
