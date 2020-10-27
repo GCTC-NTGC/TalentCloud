f382c13f9e9f99ab394ce5b8f331e4a202561383
Grant Barnes <grant.d.barnes@gmail.com>
2020-10-27 12:10:53 -0400

    trust all proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -14,19 +14,7 @@ class TrustProxies extends Middleware
      */
 
     // For the AppSvc in the Azure environment.
-    protected $proxies = [
-        '13.71.170.130',
-        '205.193.174.5',
-        '13.88.230.232',
-        '13.88.229.172',
-        '52.228.39.78',
-        '13.71.191.27',
-        '40.85.254.37',
-        '40.85.219.45',
-        '40.85.223.56',
-        '52.228.42.60',
-        '52.228.42.28'
-    ];
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
