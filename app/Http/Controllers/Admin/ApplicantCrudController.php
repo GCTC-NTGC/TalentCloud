e0f5414190a42e5220fd55b910454f907da18f51
Chris Wiseman <wiseman.christopher@gmail.com>
2020-10-27 09:53:25 -0300

    Minor formatting changes in crud controllers

--- app/Http/Controllers/Admin/ApplicantCrudController.php
+++ app/Http/Controllers/Admin/ApplicantCrudController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use Backpack\CRUD\app\Http\Controllers\CrudController;
-use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 
 /**
  * Class ApplicantCrudController
