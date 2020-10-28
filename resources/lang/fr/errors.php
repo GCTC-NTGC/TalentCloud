3cba3a149eee56bd2628ec732cef4e55ffa99679
Matt Nigh <matt@mattnigh.com>
2020-10-28 13:07:05 -0400

    Add 404 error language copy

--- resources/lang/fr/errors.php
+++ resources/lang/fr/errors.php
@@ -15,4 +15,28 @@ return [
     'illegal_status_transition' => 'Le changement de statut de :from à :to n\'est pas autorisé.',
     'bad_url' => 'Le url est malformée ou utilise un paramètre d\'interrogation non pris en charge.',
     'email_missing_delivery_address' => 'Impossible d\'envoyer un courriel sans adresse de livraison.',
+    '404' => [
+        'title' => 'Page non trouvée',
+        'header' => '',
+        'paragraph1' => '',
+        'links' => [
+            '1' => [
+                'title' => 'Accueil',
+                'url' => '/',
+            ],
+            '2' => [
+                'title' => 'Parcourir les emplois',
+                'url' => '/jobs',
+            ],
+            '3' => [
+                'title' => 'FAQ',
+                'url' => '/faq',
+            ],
+            '4' => [
+                'title' => 'Portail des talents autochtones',
+                'url' => '/indigenous',
+            ],
+        ],
+        'paragraph2' => '',
+    ],
 ];
