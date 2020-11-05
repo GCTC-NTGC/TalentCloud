facbc9d4cf56d368905312146e8df7892e829344
tristan-orourke <tristan.m.orourke@gmail.com>
2020-11-05 12:39:06 -0500

    Add documentation to utility py file

--- etc/scripts/replace_fr_translations.py
+++ etc/scripts/replace_fr_translations.py
@@ -1,3 +1,28 @@
+"""Combines translated strings from a spreadsheet with a fr.json file.
+
+This utility assumes you have already run `npm run translations`, so that
+fr.json already contains keys for all the strings you want translated, but some
+of those keys still have english values.
+
+The spreadsheet must be a csv file with only two columns: the key (matching a
+key in fr.json), and the french string value.
+
+Before running this script, update the path variables to match the absolute
+paths in your local environment:
+  fr_json_path -> path to the current fr.json file.
+  fr_json_out_path -> where you want the updated fr.json file to be created
+  translations_path -> path to the csv file
+
+After updating the path variables, run this script with
+`py ./etc/scripts/replace_fr_translations.py`. The original fr.json file
+will not be edited, so if you are happy with the results, overwrite fr.json
+with the outputted file.
+
+Note: this script creates a copy of fr.json, overwriting any values whose key
+also appears in the csv file. It will not append new keys to the json file,
+ie keys which appear in the csv but not fr.json will be ignored.
+"""
+
 import csv
 import json
 
