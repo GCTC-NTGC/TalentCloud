f9caa2658bd6d39d256fa6753f7838086d885b81
Matt Nigh <matt@mattnigh.com>
2020-11-23 13:26:52 -0500

    Create Skill Category model

---
+++ app/Models/SkillCategory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Backpack\CRUD\app\Models\Traits\SpatieTranslatable\HasTranslations;
+
+/**
+ * Class SkillCategory
+ *
+ * @property int $id
+ * @property string $key
+ * @property string $name
+ * @property int $parent_category_id
+ * @property \Jenssegers\Date\Date $created_at
+ * @property \Jenssegers\Date\Date $updated_at
+ *
+ */
+class SkillCategory extends BaseModel
+{
+    use CrudTrait;
+    use HasTranslations;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'key',
+        'name',
+        'parent_category_id',
+    ];
+
+    /**
+     * @var string[]
+     */
+    public $translatable = [
+        'name',
+    ];
+}
