beece3c2e6d14484c4d9abacec0d410c2104e84c
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-11-23 20:35:45 -0500

    Add new controller methods for batch storing, updating, and deleting experience skills

--- app/Http/Controllers/Api/ExperienceSkillsController.php
+++ app/Http/Controllers/Api/ExperienceSkillsController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BatchStoreExperienceSkill;
 use App\Http\Requests\StoreExperienceSkill;
 use App\Models\ExperienceSkill;
 use Illuminate\Http\Request;
@@ -47,5 +48,62 @@ class ExperienceSkillsController extends Controller
     public function destroy(ExperienceSkill $experienceSkill)
     {
         $experienceSkill->delete();
+    }
+
+    public function batchStore(BatchStoreExperienceSkill $request)
+    {
+        $newExperienceSkills = $request->validated();
+        $response = [];
+        foreach ($newExperienceSkills as $newExperienceSkill) {
+            // Restore soft deleted experienceSkill if it exists, otherwise create a new one.
+            $softDeletedExperienceSkill = ExperienceSkill::onlyTrashed()
+                ->where([
+                ['skill_id', $newExperienceSkill['skill_id']],
+                ['experience_id', $newExperienceSkill['experience_id']],
+                ['experience_type', $newExperienceSkill['experience_type']]
+            ])->first();
+            if ($softDeletedExperienceSkill) {
+                $softDeletedExperienceSkill->restore();
+                $softDeletedExperienceSkill->save();
+                array_push($response, $softDeletedExperienceSkill);
+            } else {
+                $experienceSkill = new ExperienceSkill($newExperienceSkill);
+                $experienceSkill->skill_id = $newExperienceSkill['skill_id'];
+                $experienceSkill->experience_id = $newExperienceSkill['experience_id'];
+                $experienceSkill->experience_type = $newExperienceSkill['experience_type'];
+                $experienceSkill->justification = $newExperienceSkill['justification'];
+                $experienceSkill->save();
+                $experienceSkill->fresh();
+                array_push($response, $experienceSkill);
+            }
+        }
+
+        return new JsonResource($response);
+    }
+
+    public function batchUpdate(Request $request)
+    {
+        $newExperienceSkills = $request->validate([
+            '*.id' => 'required|exists:App\Models\ExperienceSkill,id',
+            '*.justification' => 'nullable|string',
+        ]);
+        $response = [];
+
+        foreach ($newExperienceSkills as $newExperienceSkill) {
+            $experienceSkill = ExperienceSkill::where('id', $newExperienceSkill['id'])->first();
+            $experienceSkill->fill($newExperienceSkill);
+            $experienceSkill->save();
+            array_push($response, $experienceSkill);
+        }
+        return new JsonResource($response);
+    }
+
+    public function batchDestroy(Request $request)
+    {
+        $experienceSkills = $request->validate([
+            '*.id' => 'required|exists:App\Models\ExperienceSkill,id',
+            ]);
+        ExperienceSkill::whereIn('id', $experienceSkills)->delete();
+        return response()->json(['success' => 'success'], 200);
     }
 }
