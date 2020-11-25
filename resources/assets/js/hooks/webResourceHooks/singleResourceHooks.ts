07c055269bbe3b71e21f263ea5a07b455f2794dd
tristan-orourke <tristan.m.orourke@gmail.com>
2020-11-25 14:00:55 -0500

    Add skipInitialFetch option to useResource hook.

--- resources/assets/js/hooks/webResourceHooks/singleResourceHooks.ts
+++ resources/assets/js/hooks/webResourceHooks/singleResourceHooks.ts
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { FetchError, useFetch } from "react-async";
 import { fetchParameters } from "../../helpers/httpRequests";
 import { identity } from "../../helpers/queries";
@@ -8,7 +8,8 @@ export function useResource<T>(
   endpoint: string,
   initialValue: T,
   overrides?: {
-    parseResponse?: (response: Json) => T;
+    parseResponse?: (response: Json) => T; // Defaults to the identity function.
+    skipInitialFetch?: boolean; // Defaults to false. Override if you want to keep the initialValue until refresh is called manually.
   },
 ): {
   value: T;
@@ -18,6 +19,7 @@ export function useResource<T>(
   refresh: () => void;
 } {
   const internalParseResponse = overrides?.parseResponse ?? identity;
+  const skipInitialFetch = overrides?.skipInitialFetch === true;
   const valueRef = useRef(initialValue);
   const { error, status, reload, run } = useFetch(
     endpoint,
@@ -26,10 +28,20 @@ export function useResource<T>(
       onResolve: (data) => {
         valueRef.current = internalParseResponse(data);
       },
+      initialValue: null, // Setting this prevents fetch from happening on first render. (We call it later if necessary.)
     },
   );
   const refresh = reload;
   const update = (newValue: T): void => run(fetchParameters("PUT", newValue));
+
+  // Despite the usual useEffect guidelines, this should only run on first render or when endpoint changes.
+  // Changing skipInitialFetch after the first render should not cause a refresh.
+  useEffect(() => {
+    if (!skipInitialFetch) {
+      refresh();
+    }
+  }, [endpoint]);
+
   return { value: valueRef.current, status, error, update, refresh };
 }
 
