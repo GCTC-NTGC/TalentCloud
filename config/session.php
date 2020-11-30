62effe1677f597f271a0587bd6947822e52fd583
Grant Barnes <grant.d.barnes@gmail.com>
2020-11-30 17:57:36 -0500

    set secure cookies, regenerate session ID

--- config/session.php
+++ config/session.php
@@ -164,7 +164,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', null),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
 
     /*
     |--------------------------------------------------------------------------
