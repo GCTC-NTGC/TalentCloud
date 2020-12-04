6af9d859cedfd06baaf92c3d72f43e70e1a2eee8
Jayson McIntosh <jayson.r.mcintosh@gmail.com>
2020-12-04 10:44:29 -0500

    stubbing in skeleton for ProfileBasicInformation page

---
+++ resources/assets/js/components/ApplicantProfile/ProfileBasicInformation.tsx
@@ -0,0 +1,17 @@
+import { useIntl } from "react-intl";
+
+export interface ProfileBasicInformationProps {
+
+  }
+
+export const ProfileBasicInformation: React.FC<ProfileBasicInformationProps> = ({
+
+  }) => {
+    const intl = useIntl();
+
+    return (
+
+    );
+  };
+
+  export default ProfileBasicInformation;
