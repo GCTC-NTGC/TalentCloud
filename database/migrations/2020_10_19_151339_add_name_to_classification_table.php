d6d15f6901483a6a24e212f16823032d824fcf1e
Chris Wiseman <wiseman.christopher@gmail.com>
2020-12-07 10:52:06 -0400

    Rename migration class to follow Laravel conventions

--- database/migrations/2020_10_19_151339_add_name_to_classification_table.php
+++ database/migrations/2020_10_19_151339_add_name_to_classification_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDescAndDefaultmsgToClassification extends Migration
+class AddNameToClassificationTable extends Migration
 {
     /**
      * Run the migrations.
