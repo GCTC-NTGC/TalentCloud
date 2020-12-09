bf1a644c6da05736980c5938d85a14782334efb0
Jayson McIntosh <jayson.r.mcintosh@gmail.com>
2020-12-09 11:45:34 -0500

    merging and adjusting the data format of fakeGoCClassification data

--- resources/assets/js/fakeData/fakeGocClassification.ts
+++ resources/assets/js/fakeData/fakeGocClassification.ts
@@ -3,51 +3,107 @@ import {GocClassification, Classification} from "../models/types";
 export const fakeGocClassifications = (
 ): GocClassification[] => ([
   {
-    classification : {"key": "CS"},
+    classification : {
+      id: 5,
+      key: "CS",
+      name:
+      {
+        en: "",
+        fr: "",
+      }
+    },
     level : 1,
     order : 0,
   },
   {
-    classification : {"key": "CS"},
+    classification : {
+      id: 5,
+      key: "CS",
+      name:
+      {
+        en: "",
+        fr: "",
+      }
+    },
     level : 2,
     order : 0,
   },
   {
-    classification : {"key": "CS"},
+    classification : {
+      id: 5,
+      key: "CS",
+      name:
+      {
+        en: "",
+        fr: "",
+      }
+    },
     level : 3,
     order : 0,
   },
   {
-    classification : {"key": "CS"},
+    classification : {
+      id: 5,
+      key: "CS",
+      name:
+      {
+        en: "",
+        fr: "",
+      }
+    },
     level : 4,
     order : 0,
   },
   {
-    classification : {"key": "CS"},
+    classification : {
+      id: 5,
+      key: "CS",
+      name:
+      {
+        en: "",
+        fr: "",
+      }
+    },
     level : 5,
     order : 0,
   },
   {
-    classification : {"key": "AS"},
+    classification : {
+      id: 1,
+      key: "AS",
+      name:
+      {
+        en: "",
+        fr: "",
+      }
+    },
     level : 1,
     order : 0,
   },
   {
-    classification : {"key": "AS"},
+    classification : {
+      id: 1,
+      key: "AS",
+      name:
+      {
+        en: "",
+        fr: "",
+      }
+    },
     level : 2,
     order : 0,
   },
   {
-    classification : {"key": "AS"},
+    classification : {
+      id: 1,
+      key: "AS",
+      name:
+      {
+        en: "",
+        fr: "",
+      }
+    },
     level : 3,
     order : 0,
-  }
+  },
 ]);
-
-/*
-export interface GocClassification {
-  classification : Classification;
-  level : Number;
-  order : Number; // Used to allow user to order the list of classifications
-}
-*/
