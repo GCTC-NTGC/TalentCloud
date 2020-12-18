4bf7f699388dc5d09a9a1dd4c8997e8f818d581e
jaysonmc <jayson.r.mcintosh@gmail.com>
2020-12-18 08:58:25 -0500

    Adding in formik and fastfield to reuse existing implementations/labels

--- resources/assets/js/components/ApplicantProfile/ProfileBasicInformation.tsx
+++ resources/assets/js/components/ApplicantProfile/ProfileBasicInformation.tsx
@@ -3,6 +3,17 @@ import { FormattedMessage, useIntl } from "react-intl";
 import { GocClassification, ProfileBasicInformation as ProfileBasicInformationProp } from "../../models/types";
 import { myBasicInformationMessages } from "../Application/applicationMessages";
 import { removeDuplicatesById } from "../../helpers/queries";
+import SelectInput from "../Form/SelectInput";
+import { FastField, Formik } from "formik";
+import messages, {
+  citizenshipDeclaration,
+} from "../Application/BasicInfo/basicInfoMessages";
+import {
+  basicInfoMessages,
+} from "../Application/applicationMessages";
+import {
+  CitizenshipId,
+} from "../../models/lookupConstants";
 
 export interface ProfileBasicInformationProps {
   gocClassifications : GocClassification[],
@@ -39,7 +50,12 @@ const GcExperience: FunctionComponent<GcExperienceProps> = ({
   }
 
   const addExperience = function(/*e : React.MouseEvent<HTMLAnchorElement, MouseEvent>*/) {
-    previousExperience.push({
+
+    console.dir(previousExperience)
+
+    let lPreviousExperience = previousExperience
+
+    lPreviousExperience.push({
       classification: {
         id: 0,
         key: "",
@@ -51,7 +67,11 @@ const GcExperience: FunctionComponent<GcExperienceProps> = ({
       level: 0,
       order: 0,
     })
-    setPreviousExperience(previousExperience)
+    setPreviousExperience(lPreviousExperience)
+
+    console.log("**************************************")
+
+    console.dir(previousExperience)
   }
 
   if (!wasGcEmployee)
@@ -193,91 +213,115 @@ export const ProfileBasicInformation: React.FC<ProfileBasicInformationProps> = (
 
   return (
     <>
-      <div>
-        <h2 data-c-heading="h2" data-c-margin="bottom(1)">
-          {intl.formatMessage(myBasicInformationMessages.heading)}
-        </h2>
-        <p data-c-margin="bottom(1)">
-          <FormattedMessage
-            id="profile.experience.preamble"
-            defaultMessage="This profile is also shared when you submit a job application."
-            description="First section of text on the 'My Basic Information' of the Application Timeline."
-          />
-        </p>
+      <Formik
+        initialValues={basicInformation}
+        onSubmit={(values): void => {
+          const basicInformationValues: ProfileBasicInformationProp = {
+            ...values,
+          };
+        }}
+      >
         <div>
-          <p>
-            Name: <b data-c-color="c1"> {name} </b>{" "}
+          <h2 data-c-heading="h2" data-c-margin="bottom(1)">
+            {intl.formatMessage(myBasicInformationMessages.heading)}
+          </h2>
+          <p data-c-margin="bottom(1)">
+            <FormattedMessage
+              id="profile.experience.preamble"
+              defaultMessage="This profile is also shared when you submit a job application."
+              description="First section of text on the 'My Basic Information' of the Application Timeline."
+            />
           </p>
-          <p>
-            Personal Email: <b data-c-color="c1"> {email} </b>{" "}
-          </p>
-          <p>
-            To change these go to:{" "}
-            <a data-c-color="c1" href="#">
-              Account Settings
-            </a>
-          </p>
-        </div>
-        <div data-c-input="select">
-          <label htmlFor="SEL2">{intl.formatMessage(myBasicInformationMessages.citizenStatus)}</label>
-          <span>Required</span>
-          <div data-c-grid-item="base(1of3)">
-
-            <i className="fas fa-caret-down" />
-            <select required id="SEL2">
-              <option disabled selected>
-                Select a status...
-              </option>
-              <option>Canadian Citizen</option>
-              <option>Not a citizen</option>
-            </select>
+          <div>
+            <p>
+              Name: <b data-c-color="c1"> {name} </b>{" "}
+            </p>
+            <p>
+              Personal Email: <b data-c-color="c1"> {email} </b>{" "}
+            </p>
+            <p>
+              To change these go to:{" "}
+              <a data-c-color="c1" href="#">
+                Account Settings
+              </a>
+            </p>
           </div>
-          <span>{intl.formatMessage(myBasicInformationMessages.inputEreror)}</span>
-        </div>
-        <div data-c-input="select">
-          <label htmlFor="SEL2">
-            {intl.formatMessage(myBasicInformationMessages.isVerteran)}
-          </label>
-          <span>Required</span>
-          <div data-c-grid-item="base(1of3)">
-            <i className="fas fa-caret-down" />
-            <select required id="SEL2">
-              <option>No</option>
-              <option>Yes</option>
-            </select>
-          </div>
-          <span>{intl.formatMessage(myBasicInformationMessages.inputEreror)}</span>
-        </div>
-        <h2 data-c-heading="h2" data-c-margin="bottom(1)">
-          {intl.formatMessage(myBasicInformationMessages.heading)}
-        </h2>
+          <FastField
+            id="citizenship"
+            name="citizenship"
+            component={SelectInput}
+            required
+            label={intl.formatMessage(basicInfoMessages.citizenshipLabel)}
+            grid="base(1of1)"
+            nullSelection={intl.formatMessage(messages.nullSelectOption)}
+            options={Object.values(CitizenshipId).map((id: number): {
+              value: number;
+              label: string;
+            } => ({
+              value: id,
+              label: intl.formatMessage(citizenshipDeclaration(id)),
+            }))}
+          />
+          <div data-c-input="select">
+            <label htmlFor="SEL2">{intl.formatMessage(myBasicInformationMessages.citizenStatus)}</label>
+            <span>Required</span>
+            <div data-c-grid-item="base(1of3)">
 
-        <div data-c-grid-item="base(1of3)" data-c-input="radio">
-          <label htmlFor="RG2">
-            {intl.formatMessage(myBasicInformationMessages.isGCEmployee)}
-          </label>
-          <span>Required</span>
-          <div id="RG2" role="radiogroup">
-            <label htmlFor="rB1">
-              <input id="rB1" required name="radioB" type="radio" defaultChecked={currentGcEmployee} onChange={() => setCurrentGcEmployee(true)} />
-              <span>Current GC Employee</span>
+              <i className="fas fa-caret-down" />
+              <select required id="SEL2">
+                <option disabled selected>
+                  Select a status...
+                </option>
+                <option>Canadian Citizen</option>
+                <option>Not a citizen</option>
+              </select>
+            </div>
+            <span>{intl.formatMessage(myBasicInformationMessages.inputEreror)}</span>
+          </div>
+          <div data-c-input="select">
+            <label htmlFor="SEL2">
+              {intl.formatMessage(myBasicInformationMessages.isVerteran)}
             </label>
-            <label htmlFor="rB2">
-              <input id="rB2" required name="radioB" type="radio" onChange={() => setCurrentGcEmployee(false)} />
-              <span>Not a GC employee</span>
-            </label>
+            <span>Required</span>
+            <div data-c-grid-item="base(1of3)">
+              <i className="fas fa-caret-down" />
+              <select required id="SEL2">
+                <option>No</option>
+                <option>Yes</option>
+              </select>
+            </div>
+            <span>{intl.formatMessage(myBasicInformationMessages.inputEreror)}</span>
           </div>
-          <span>{intl.formatMessage(myBasicInformationMessages.inputEreror)}</span>
+          <h2 data-c-heading="h2" data-c-margin="bottom(1)">
+            {intl.formatMessage(myBasicInformationMessages.heading)}
+          </h2>
+
+          <div data-c-grid-item="base(1of3)" data-c-input="radio">
+            <label htmlFor="RG2">
+              {intl.formatMessage(myBasicInformationMessages.isGCEmployee)}
+            </label>
+            <span>Required</span>
+            <div id="RG2" role="radiogroup">
+              <label htmlFor="rB1">
+                <input id="rB1" required name="radioB" type="radio" defaultChecked={currentGcEmployee} onChange={() => setCurrentGcEmployee(true)} />
+                <span>Current GC Employee</span>
+              </label>
+              <label htmlFor="rB2">
+                <input id="rB2" required name="radioB" type="radio" onChange={() => setCurrentGcEmployee(false)} />
+                <span>Not a GC employee</span>
+              </label>
+            </div>
+            <span>{intl.formatMessage(myBasicInformationMessages.inputEreror)}</span>
+          </div>
+
+          <GcExperience
+            gocClassifications={gocClassifications}
+            previousExperienceProp={basicInformation.previous_classifications}
+            currentGcClassification={basicInformation.current_classification}
+            wasGcEmployee={currentGcEmployee}
+          />
         </div>
-
-        <GcExperience
-          gocClassifications={gocClassifications}
-          previousExperienceProp={basicInformation.previous_classifications}
-          currentGcClassification={basicInformation.current_classification}
-          wasGcEmployee={currentGcEmployee}
-        />
-
-      </div>
+      </Formik>
     </>
   );
 };
