978be1a7e9f325e49bcca55aa6771822e69433b0
tristan-orourke <tristan.m.orourke@gmail.com>
2021-02-04 19:04:48 -0500

    When applicant skills are deleted, delete associated ExperienceSkills

--- app/Http/Controllers/Api/ApplicantSkillsController.php
+++ app/Http/Controllers/Api/ApplicantSkillsController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateApplicantSkills;
 use App\Models\Applicant;
+use App\Models\ExperienceSkill;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
 
 class ApplicantSkillsController extends Controller
 {
@@ -18,8 +21,15 @@ class ApplicantSkillsController extends Controller
     {
         $skillIds = $request->validated()['skill_ids'];
         $applicant->skills()->sync($skillIds);
+
+        // $deletedExperienceSkills = $applicant->experienceSkillsQuery()->whereNotIn('skill_id', $skillIds)->get();
+        // I'm leaving the above line commented out for now, but if we want to do something with the
+        // deleted ExperienceSkill objects, we can retrieve them before deleting and include
+        // them in the api response.
+        $applicant->experienceSkillsQuery()->whereNotIn('skill_id', $skillIds)->delete();
         return [
-            'skill_ids' => $skillIds
+            'skill_ids' => $skillIds,
+            // 'deleted_experience_skills' => JsonResource::collection($deletedExperienceSkills),
         ];
     }
 }
--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -40,6 +40,8 @@ use App\Traits\TalentCloudCrudTrait as CrudTrait;
  * @property \Illuminate\Database\Eloquent\Collection $experiences_education
  * @property \Illuminate\Database\Eloquent\Collection $experiences_award
  * @property \Illuminate\Database\Eloquent\Collection $experiences_community
+ *
+ * @method \Illuminate\Database\Query\Builder experienceSkillsQuery
  */
 class Applicant extends BaseModel
 {
@@ -169,5 +171,70 @@ class Applicant extends BaseModel
     {
         return $this->morphMany(\App\Models\ExperienceCommunity::class, 'experienceable')
             ->orderBy('end_date', 'desc');
+    }
+
+    /**
+     * Returns a Laravel QueryBuilder object which will retrieve all ExperienceSkills
+     * which are linked to this Applicant, through this Applicant's Experiences.
+     *
+     * It returns the query builder object instead of the results of the query, so that additional
+     * clauses can be added by other code.
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function experienceSkillsQuery()
+    {
+        $applicantId = $this->id;
+        return ExperienceSkill::where(function ($query) use ($applicantId) {
+            // A single where clause wraps 5 OR WHERE clauses.
+            // Each WHERE clause gets all the ExperienceSkills linked to a particular Experience type (and this Applicant).
+
+            $query->orWhere(function ($query) use ($applicantId) {
+                $query->whereIn('id', function ($query) use ($applicantId) {
+                    $query->select('experience_skills.id')->from('experience_skills')->join('experiences_work', function ($join) use ($applicantId) {
+                        $join->on('experience_skills.experience_id', '=', 'experiences_work.id')
+                            ->where('experience_skills.experience_type', 'experience_work')
+                            ->where('experiences_work.experienceable_type', 'applicant')
+                            ->where('experiences_work.experienceable_id', $applicantId);
+                    });
+                });
+            })->orWhere(function ($query) use ($applicantId) {
+                $query->whereIn('id', function ($query) use ($applicantId) {
+                    $query->select('experience_skills.id')->from('experience_skills')->join('experiences_personal', function ($join) use ($applicantId) {
+                        $join->on('experience_skills.experience_id', '=', 'experiences_personal.id')
+                            ->where('experience_skills.experience_type', 'experience_personal')
+                            ->where('experiences_personal.experienceable_type', 'applicant')
+                            ->where('experiences_personal.experienceable_id', $applicantId);
+                    });
+                });
+            })->orWhere(function ($query) use ($applicantId) {
+                $query->whereIn('id', function ($query) use ($applicantId) {
+                    $query->select('experience_skills.id')->from('experience_skills')->join('experiences_education', function ($join) use ($applicantId) {
+                        $join->on('experience_skills.experience_id', '=', 'experiences_education.id')
+                            ->where('experience_skills.experience_type', 'experience_education')
+                            ->where('experiences_education.experienceable_type', 'applicant')
+                            ->where('experiences_education.experienceable_id', $applicantId);
+                    });
+                });
+            })->orWhere(function ($query) use ($applicantId) {
+                $query->whereIn('id', function ($query) use ($applicantId) {
+                    $query->select('experience_skills.id')->from('experience_skills')->join('experiences_award', function ($join) use ($applicantId) {
+                        $join->on('experience_skills.experience_id', '=', 'experiences_award.id')
+                            ->where('experience_skills.experience_type', 'experience_award')
+                            ->where('experiences_award.experienceable_type', 'applicant')
+                            ->where('experiences_award.experienceable_id', $applicantId);
+                    });
+                });
+            })->orWhere(function ($query) use ($applicantId) {
+                $query->whereIn('id', function ($query) use ($applicantId) {
+                    $query->select('experience_skills.id')->from('experience_skills')->join('experiences_community', function ($join) use ($applicantId) {
+                        $join->on('experience_skills.experience_id', '=', 'experiences_community.id')
+                            ->where('experience_skills.experience_type', 'experience_community')
+                            ->where('experiences_community.experienceable_type', 'applicant')
+                            ->where('experiences_community.experienceable_id', $applicantId);
+                    });
+                });
+            });
+        });
     }
 }
