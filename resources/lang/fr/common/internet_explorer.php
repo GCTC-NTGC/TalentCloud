fd4275efc60342b6acc0598906e11b2a0c95e7c1
Matt Nigh <matt@mattnigh.com>
2021-02-08 16:57:02 -0500

    Update Internet Explorer Banner FR link_url

--- resources/lang/fr/common/internet_explorer.php
+++ resources/lang/fr/common/internet_explorer.php
@@ -16,7 +16,7 @@ return [
         ],
         'paragraph_2' => [
             'text_1' => 'Microsoft is ',
-            'link_url' => 'https://www.microsoft.com/en/microsoft-365/windows/end-of-ie-support',
+            'link_url' => 'https://www.microsoft.com/fr/microsoft-365/windows/end-of-ie-support',
             'link_title' => 'Visit Microsoft\'s website',
             'link_text' => 'phasing out support for Internet Explorer',
             'text_2' => ' in favour of Microsoft Edge. We\'re doing the same.',
