e341181419417ea15e69696bf33205a811f556d4
Matt Nigh <matt@mattnigh.com>
2021-02-08 15:25:38 -0500

    Update Internet Explorer Banner Language Lines

--- resources/lang/en/common/internet_explorer.php
+++ resources/lang/en/common/internet_explorer.php
@@ -12,18 +12,14 @@ return [
     'banner' => [
         'title' => 'We noticed you\'re using Internet Explorer.',
         'paragraph_1' => [
-            'text_1' => 'Talent Cloud is phasing out support for Internet Explorer in favour of Microsoft Edge',
-            'text_2' => 'If you find a bug',
-            'link_email' => 'talent.cloud-nuage.de.talents@tbs-sct.gc.ca',
-            'link_title' => 'Send an email to Talent Cloud',
-            'link_text' => 'please report it here',
+            'text_1' => 'As a result, you\'re likely to experience some issues with our site, like features not loading properly and information not saving.',
         ],
         'paragraph_2' => [
-            'text_1' => 'As Microsoft has begun',
+            'text_1' => 'Microsoft is ',
             'link_url' => 'https://www.microsoft.com/en/microsoft-365/windows/end-of-ie-support',
             'link_title' => 'Visit Microsoft\'s website',
             'link_text' => 'phasing out support for Internet Explorer',
-            'text_2' => 'we recommend using Talent Cloud from a modern web browser for the best experience',
+            'text_2' => ' in favour of Microsoft Edge. We\'re doing the same.',
         ],
         'dismiss_trigger' => 'Dismiss',
     ],
