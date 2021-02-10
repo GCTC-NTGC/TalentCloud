e4872b1700a268dc9759b090a9bf0e82bdab51cb
tristan-orourke <tristan.m.orourke@gmail.com>
2021-02-10 17:29:28 -0500

    Remove automatic timed dismissal of Error Toast Staying there until the user has a chance to read it and dismiss it manually is more accessible

--- resources/assets/js/components/ErrorToast.tsx
+++ resources/assets/js/components/ErrorToast.tsx
@@ -18,25 +18,6 @@ export const errorMessages = defineMessages({
   },
 });
 
-/**
- * Creates a closure which can be called in the future, to conditionally run a callback only if a certain value is unchanged.
- *
- * @param pastValue This value is fixed when the closure is initially created.
- * @param futureValueRef This allows access to a variable which may be changed by the time this closure runs.
- * @param callback This function will be called if pastValue === futureValueRef.current when this closure runs.
- */
-function runCallbackIfValueUnchanged<T>(
-  pastValue: T,
-  futureValueRef: React.MutableRefObject<T>,
-  callback: () => void,
-) {
-  return () => {
-    if (pastValue === futureValueRef.current) {
-      callback();
-    }
-  };
-}
-
 export const ErrorToast: React.FC = () => {
   const intl = useIntl();
   const { state, dispatch } = useContext(ErrorContext);
@@ -44,22 +25,6 @@ export const ErrorToast: React.FC = () => {
 
   // This toast will render the first error in the queue, if any.
   const currentError = state.errorQueue.length > 0 ? state.errorQueue[0] : null;
-
-  // This ref object makes the currently rendered error available to an async closure, dismissCurrentError
-  const currentErrorRef = useRef(currentError);
-  currentErrorRef.current = currentError;
-
-  useEffect(() => {
-    if (currentError !== null) {
-      // At the end of the timeout, if the current error hasn't been manually dismissed, dismiss it.
-      const dismissCurrentError = runCallbackIfValueUnchanged(
-        currentError,
-        currentErrorRef,
-        dismiss,
-      );
-      setTimeout(dismissCurrentError, TOAST_SELF_DISMISS_TIMER);
-    }
-  }, [currentError, dismiss]);
 
   return (
     <>
