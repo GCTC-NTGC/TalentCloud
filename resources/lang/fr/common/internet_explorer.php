5f3daaabaa2d8c68745ef74a865df6cae1aece60
Matt Nigh <matt@mattnigh.com>
2021-02-10 09:17:57 -0500

    Update Internet Explorer Banner Language Lines FR

--- resources/lang/fr/common/internet_explorer.php
+++ resources/lang/fr/common/internet_explorer.php
@@ -12,14 +12,14 @@ return [
     'banner' => [
         'title' => 'Nous avons remarqué que vous utilisez Internet Explorer.',
         'paragraph_1' => [
-            'text_1' => 'As a result, you\'re likely to experience some issues with our site, like features not loading properly and information not saving.',
+            'text_1' => 'Par conséquent, il est probable que vous éprouviez des problèmes avec notre site, comme des fonctions qui ne sont pas chargées correctement et des renseignements qui ne sont pas sauvegardés.',
         ],
         'paragraph_2' => [
-            'text_1' => 'Microsoft is ',
+            'text_1' => 'Microsoft ',
             'link_url' => 'https://www.microsoft.com/fr/microsoft-365/windows/end-of-ie-support',
-            'link_title' => 'Visit Microsoft\'s website',
-            'link_text' => 'phasing out support for Internet Explorer',
-            'text_2' => ' in favour of Microsoft Edge. We\'re doing the same.',
+            'link_title' => 'Visitez le site Web de Microsoft',
+            'link_text' => 'élimine progressivement la prise en charge d’Internet Explorer',
+            'text_2' => ' au profit de Microsoft Edge. Nous faisons la même chose.',
         ],
     ],
 ];
