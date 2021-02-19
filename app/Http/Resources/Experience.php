990c5dabfc805954ace920beabd0c8b2a6571408
Matt Nigh <matt@mattnigh.com>
2021-02-19 17:21:47 -0500

    Refactor Experience JsonResource class create dates array, replace if/elseif with switch statement

--- app/Http/Resources/Experience.php
+++ app/Http/Resources/Experience.php
@@ -17,16 +17,45 @@ class Experience extends JsonResource
     {
         $type = $this->experienceTypeName();
         $translations = [];
-        if ($type === 'experience_education') {
-            $translations = [
-                'education_status' => $this->education_status->getTranslations('name'),
-                'education_type' => $this->education_type->getTranslations('name'),
-            ];
-        } elseif ($type === 'experience_award') {
-            $translations = [
-                'award_recipient_type' => $this->award_recipient_type->getTranslations('name'),
-                'award_recognition_type' => $this->award_recognition_type->getTranslations('name'),
-            ];
+        $dates = [];
+        switch ($type) {
+            case 'experience_work':
+                $dates = [
+                    'start_date' => $this->when($this->start_date, $this->start_date->format('Y-m-d')),
+                    'end_date' => $this->when($this->end_date, $this->end_date->format('Y-m-d')),
+                ];
+                break;
+            case 'experience_award':
+                $dates = [
+                    'awarded_date' => $this->when($this->awarded_date, $this->awarded_date->format('Y-m-d')),
+                ];
+                $translations = [
+                    'award_recipient_type' => $this->award_recipient_type->getTranslations('name'),
+                    'award_recognition_type' => $this->award_recognition_type->getTranslations('name'),
+                ];
+                break;
+            case 'experience_community':
+                $dates = [
+                    'start_date' => $this->when($this->start_date, $this->start_date->format('Y-m-d')),
+                    'end_date' => $this->when($this->end_date, $this->end_date->format('Y-m-d')),
+                ];
+                break;
+            case 'experience_education':
+                $dates = [
+                    'start_date' => $this->when($this->start_date, $this->start_date->format('Y-m-d')),
+                    'end_date' => $this->when($this->end_date, $this->end_date->format('Y-m-d')),
+                ];
+                $translations = [
+                    'education_status' => $this->education_status->getTranslations('name'),
+                    'education_type' => $this->education_type->getTranslations('name'),
+                ];
+                break;
+            case 'experience_personal':
+                $dates = [
+                    'start_date' => $this->when($this->start_date, $this->start_date->format('Y-m-d')),
+                    'end_date' => $this->when($this->end_date, $this->end_date->format('Y-m-d')),
+                ];
+                break;
         }
         return array_merge(
             parent::toArray($request),
@@ -35,20 +64,7 @@ class Experience extends JsonResource
                 'type' => $type,
                 'experience_skills' => JsonResource::collection($this->whenLoaded('experience_skills')),
             ],
-            [
-                'start_date' => $this->when($this->start_date, function () {
-                    $start_date = new Date($this->start_date);
-                    return $start_date->format('Y-m-d');
-                }),
-                'end_date' => $this->when($this->end_date, function () {
-                    $end_date = new Date($this->end_date);
-                    return $end_date->format('Y-m-d');
-                }),
-                'awarded_date' => $this->when($this->awarded_date, function () {
-                    $awarded_date = new Date($this->awarded_date);
-                    return $awarded_date->format('Y-m-d');
-                }),
-            ]
+            $dates
         );
     }
 }
