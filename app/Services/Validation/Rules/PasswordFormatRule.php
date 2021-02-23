2ab0f3af187b3153a451cdd6ed36c29406a98753
Matt Nigh <matt@mattnigh.com>
2021-02-23 14:09:37 -0500

    Update PasswordFormatRule comments

--- app/Services/Validation/Rules/PasswordFormatRule.php
+++ app/Services/Validation/Rules/PasswordFormatRule.php
@@ -10,9 +10,10 @@ class PasswordFormatRule implements Rule
     /**
      * This Password Validation rule should be referenced wherever we need the user input their password on the site.
      * The rules used on their passwords are:
-     * 1) Password is required.
-     * 2) Must be at least 9 characters
-     * 3) The password must contain at least one character from the following categories:
+     * 1) The password is required.
+     * 2) The password must be at least 9 characters.
+     * 3) The password may not be greater than 100 characters.
+     * 4) The password must contain at least one character from the following categories:
      * lower-case characters (a-z), upper-case characters (A-Z),
      * digits (0-9), and non-alphanumeric symbols (%, $, !, etc.).
      * Guide used by Polivas Korop
