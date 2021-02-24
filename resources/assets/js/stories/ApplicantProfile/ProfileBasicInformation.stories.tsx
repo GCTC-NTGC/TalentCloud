039947254e3154cbd2dba68ec19253c7d68808b7
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2021-02-24 09:40:22 -0500

    Refactor ProfileBasicInfo story

--- resources/assets/js/stories/ApplicantProfile/ProfileBasicInformation.stories.tsx
+++ resources/assets/js/stories/ApplicantProfile/ProfileBasicInformation.stories.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withIntl } from "storybook-addon-intl";
-import { useState as useStorybookState } from "@storybook/addons";
-import { fakeBasicInformation } from "../../fakeData/fakeBasicInformation";
+import { text } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
 import ProfileBasicInfo from "../../components/ApplicantProfile/BasicInfo/ProfileBasicInfo";
-import { fakeGocClassifications } from "../../fakeData/fakeGocClassification";
+import fakeClassifications from "../../fakeData/fakeClassifications";
+import {
+  CitizenshipId,
+  GCEmployeeStatus,
+  VeteranId,
+} from "../../models/lookupConstants";
+import {
+  fakeApplicantClassification1,
+  fakeApplicantClassification2,
+  fakeApplicantClassifications,
+} from "../../fakeData/fakeApplicantClassifications";
 
 const stories = storiesOf(
   "Applicant Profile/Basic Information",
@@ -12,19 +22,73 @@ const stories = storiesOf(
 ).addDecorator(withIntl);
 
 stories.add(
-  "Current GC Employee",
+  "Not GC Employee",
   (): React.ReactElement => {
-    const [basicInformation, setBasicInformations] = useStorybookState(
-      fakeBasicInformation(),
-    );
-    const [gocClassifications] = useStorybookState(fakeGocClassifications());
-
     return (
       <ProfileBasicInfo
-        name="Gerardi Escandon"
-        email="jerbo@personal.com"
-        gocClassifications={gocClassifications}
-        basicInformation={basicInformation}
+        applicantId={1}
+        currentClassification={5}
+        currentLevel={3}
+        previousClassifications={[]}
+        citizenshipDeclaration={CitizenshipId.citizen}
+        classifications={fakeClassifications()}
+        email={text("Email", "jerbo@personal.com", "Basic Info")}
+        gcEmployeeStatus={GCEmployeeStatus.no}
+        name={text("Name", "Gerardi Escandon", "Basic Info")}
+        veteranStatus={VeteranId.current}
+        handleUpdateProfile={async (x) => {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          action("Update Applicant Profile")(x);
+        }}
+      />
+    );
+  },
+);
+stories.add(
+  "Previous GC Employee",
+  (): React.ReactElement => {
+    return (
+      <ProfileBasicInfo
+        applicantId={1}
+        currentClassification={null}
+        currentLevel={null}
+        previousClassifications={[
+          fakeApplicantClassification1(),
+          fakeApplicantClassification2(),
+        ]}
+        citizenshipDeclaration={CitizenshipId.citizen}
+        classifications={fakeClassifications()}
+        email={text("Email", "jerbo@personal.com", "Basic Info")}
+        gcEmployeeStatus={GCEmployeeStatus.past}
+        name={text("Name", "Gerardi Escandon", "Basic Info")}
+        veteranStatus={VeteranId.current}
+        handleUpdateProfile={async (x) => {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          action("Update Applicant Profile")(x);
+        }}
+      />
+    );
+  },
+);
+stories.add(
+  "Current GC Employee",
+  (): React.ReactElement => {
+    return (
+      <ProfileBasicInfo
+        applicantId={1}
+        currentClassification={5}
+        currentLevel={3}
+        previousClassifications={fakeApplicantClassifications()}
+        citizenshipDeclaration={CitizenshipId.citizen}
+        classifications={fakeClassifications()}
+        email={text("Email", "jerbo@personal.com", "Basic Info")}
+        gcEmployeeStatus={GCEmployeeStatus.current}
+        name={text("Name", "Gerardi Escandon", "Basic Info")}
+        veteranStatus={VeteranId.current}
+        handleUpdateProfile={async (x) => {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          action("Update Applicant Profile")(x);
+        }}
       />
     );
   },
