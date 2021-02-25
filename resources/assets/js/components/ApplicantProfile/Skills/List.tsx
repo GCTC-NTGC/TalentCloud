29f17ae30a66c9b046f82123bba4b77f39a949ac
tristan-orourke <tristan.m.orourke@gmail.com>
2021-02-25 16:23:38 -0500

    Add null state for Skill Categories without any user skills

--- resources/assets/js/components/ApplicantProfile/Skills/List.tsx
+++ resources/assets/js/components/ApplicantProfile/Skills/List.tsx
@@ -33,6 +33,11 @@ const messages = defineMessages({
     defaultMessage: "Sort By:",
     description: "Text for the label on the sort toggle UI.",
   },
+  categoryEmpty: {
+    id: "applicantProfile.skills.list.categoryEmpty",
+    defaultMessage: "You haven't yet added any skills in this group.",
+    description: "Text to show when user has no skills in a given category.",
+  },
   categoryOther: {
     id: "applicantProfile.skills.list.categoryOther",
     defaultMessage: "Other",
@@ -164,12 +169,14 @@ const List: React.FC<ListProps> = ({
   return (
     <div>
       <p data-h2-margin="b(bottom, .5)">
-        {intl.formatMessage(messages.sortBy)}{" "}
+        {intl.formatMessage(messages.sortBy)}
+{" "}
         <SortButton
           text={intl.formatMessage(messages[SortTypes.Group])}
           active={sortType === SortTypes.Group}
           handleClick={() => setSortType(SortTypes.Group)}
-        />{" "}
+        />
+{" "}
         <SortButton
           text={intl.formatMessage(messages[SortTypes.Alpha])}
           active={sortType === SortTypes.Alpha}
@@ -183,6 +190,9 @@ const List: React.FC<ListProps> = ({
               <h5 data-h2-margin="b(top, 1) b(bottom, .5)">
                 {localizeFieldNonNull(locale, group.category, "name")}
               </h5>
+              {group.skills.length === 0 && (
+                <p>{intl.formatMessage(messages.categoryEmpty)}</p>
+              )}
               {group.skills
                 .sort(sortByLocalizedName(locale))
                 .map((skill: Skill) => {
