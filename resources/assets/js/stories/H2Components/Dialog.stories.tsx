aefbfaaa8711e3bbf159ba20bea55d40b5c10eea
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2021-03-23 10:30:45 -0400

    Update Dialog story

--- resources/assets/js/stories/H2Components/Dialog.stories.tsx
+++ resources/assets/js/stories/H2Components/Dialog.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withIntl } from "storybook-addon-intl";
 import { text } from "@storybook/addon-knobs";
+import { useState } from "@storybook/client-api";
 import Dialog from "../../components/H2Components/Dialog";
 
 const stories = storiesOf("H2 Components/Dialog", module).addDecorator(
@@ -10,62 +11,74 @@ const stories = storiesOf("H2 Components/Dialog", module).addDecorator(
 
 stories.add(
   "Dialog",
-  (): React.ReactElement => (
-    <section>
-      <Dialog.Trigger
-        id="dialog"
-        data-h2-button="white, round, solid"
-        data-h2-shadow="b(medium)"
-        data-h2-padding="b(all, .5)"
-        data-h2-margin="b(right, 1)"
-      >
-        <p>{text("Trigger", "Open Dialog", "Text")}</p>
-      </Dialog.Trigger>
-      <Dialog isVisible id="dialog">
-        <Dialog.Header className="gradient-left-right">
-          <Dialog.Title
-            data-h2-padding="b(all, .5) b(left, 1)"
-            data-h2-font-color="b(white)"
-            data-h2-font-size="b(h4)"
-          >
-            {text(
-              "Title",
-              "Dialog Title but what if this were much longer?",
-              "Text",
-            )}
-          </Dialog.Title>
-          <Dialog.ActionBtn
-            data-h2-dialog-exit-trigger
-            data-h2-button="round, small, solid"
-            data-h2-align="b(right)"
-          >
-            <i
-              data-h2-font-size="b(normal)"
+  (): React.ReactElement => {
+    const [isDialogVisible, setIsDialogVisible] = useState(false);
+    const openDialog = () => setIsDialogVisible(true);
+    const closeDialog = () => {
+      setIsDialogVisible(false);
+    };
+    return (
+      <section>
+        <Dialog.Trigger
+          id="dialog"
+          data-h2-button="white, round, solid"
+          data-h2-shadow="b(medium)"
+          data-h2-padding="b(all, .5)"
+          data-h2-margin="b(right, 1)"
+          onClick={openDialog}
+        >
+          <p>{text("Trigger", "Open Dialog", "Text")}</p>
+        </Dialog.Trigger>
+        <Dialog
+          isVisible={isDialogVisible}
+          closeDialog={closeDialog}
+          id="dialog"
+        >
+          <Dialog.Header className="gradient-left-right">
+            <Dialog.Title
+              data-h2-padding="b(all, .5) b(left, 1)"
               data-h2-font-color="b(white)"
-              className="fas fa-times"
-              aria-hidden="true"
-            />
-          </Dialog.ActionBtn>
-        </Dialog.Header>
-        <Dialog.Content>
-          <p data-h2-padding="b(all, 3)">
-            {text(
-              "Content",
-              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officia reprehenderit labore ipsum et incidunt amet, dicta mollitia blanditiis minima!",
-              "Text",
-            )}
-          </p>
-        </Dialog.Content>
-        <Dialog.Actions data-h2-padding="b(all, .5)" data-h2-align="b(right)">
-          <Dialog.ActionBtn
-            buttonStyling="stop, round, solid"
-            data-h2-padding="b(rl, 2) b(tb, .5)"
-            data-h2-bg-color="b(white, 1)"
-          >
-            {text("Button Label", "Close", "Text")}
-          </Dialog.ActionBtn>
-        </Dialog.Actions>
-      </Dialog>
-    </section>
-  ),
+              data-h2-font-size="b(h4)"
+            >
+              {text(
+                "Title",
+                "Dialog Title but what if this were much longer?",
+                "Text",
+              )}
+            </Dialog.Title>
+            <Dialog.ActionBtn
+              data-h2-dialog-exit-trigger
+              data-h2-button="round, small, solid"
+              data-h2-align="b(right)"
+            >
+              <i
+                data-h2-font-size="b(normal)"
+                data-h2-font-color="b(white)"
+                className="fas fa-times"
+                aria-hidden="true"
+              />
+            </Dialog.ActionBtn>
+          </Dialog.Header>
+          <Dialog.Content>
+            <p data-h2-padding="b(all, 3)">
+              {text(
+                "Content",
+                " Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officia reprehenderit labore ipsum et incidunt amet, dicta mollitia blanditiis minima!",
+                "Text",
+              )}
+            </p>
+          </Dialog.Content>
+          <Dialog.Actions data-h2-padding="b(all, .5)" data-h2-align="b(right)">
+            <Dialog.ActionBtn
+              buttonStyling="stop, round, solid"
+              data-h2-padding="b(rl, 2) b(tb, .5)"
+              data-h2-bg-color="b(white, 1)"
+            >
+              {text("Button Label", "Close", "Text")}
+            </Dialog.ActionBtn>
+          </Dialog.Actions>
+        </Dialog>
+      </section>
+    );
+  },
 );
