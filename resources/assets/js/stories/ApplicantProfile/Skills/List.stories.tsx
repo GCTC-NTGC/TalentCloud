02be98319e06a618387c65fa8e6a1ec876e2a1e2
Matt Nigh <matt@mattnigh.com>
2021-04-12 08:15:29 -0400

    Update Profile Skills story use state

--- resources/assets/js/stories/ApplicantProfile/Skills/List.stories.tsx
+++ resources/assets/js/stories/ApplicantProfile/Skills/List.stories.tsx
@@ -1,37 +1,49 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withIntl } from "storybook-addon-intl";
-import { useState } from "@storybook/addons";
+import { action } from "@storybook/addon-actions";
+import { useState as useStorybookState } from "@storybook/addons";
 import List from "../../../components/ApplicantProfile/Skills/List";
 import fakeExperiences from "../../../fakeData/fakeExperience";
 import fakeExperienceSkills from "../../../fakeData/fakeExperienceSkills";
 import { fakeSkillCategories } from "../../../fakeData/fakeSkillCategories";
 import { fakeSkills } from "../../../fakeData/fakeSkills";
-import { promiseAction } from "../../helpers";
+import { sleep } from "../../helpers";
+import { Skill } from "../../../models/types";
 
 const stories = storiesOf("Applicant Profile/Skills", module).addDecorator(
   withIntl,
 );
 
+const handleDeleteSkill = (skills, setSkills, setUpdateInProgress) => async (
+  id: number,
+): Promise<void> => {
+  setUpdateInProgress(true);
+  await sleep(1000);
+  setSkills(skills.filter((skill: Skill) => !(skill.id === id)));
+  action("Skill Deleted")({ id });
+  setUpdateInProgress(false);
+};
+
 stories.add(
   "List",
   (): React.ReactElement => {
-    const [updateInProgress, setUpdateInProgress] = useState(false);
-    const handleDeleteSkill = async (...args) => {
-      setUpdateInProgress(true);
-      await promiseAction("Delete skill")(...args);
-      setUpdateInProgress(false);
-    };
+    const [skills, setSkills] = useStorybookState(fakeSkills());
+    const [updateInProgress, setUpdateInProgress] = useStorybookState(false);
+
     return (
       <section>
         <List
           experiences={fakeExperiences()}
           experienceSkills={fakeExperienceSkills()}
           skillCategories={fakeSkillCategories()}
-          skills={fakeSkills()}
+          skills={skills}
           applicantId={1}
-          handleDeleteSkill={handleDeleteSkill}
+          handleDeleteSkill={handleDeleteSkill(
+            skills,
+            setSkills,
+            setUpdateInProgress,
+          )}
           updateInProgress={updateInProgress}
         />
       </section>
